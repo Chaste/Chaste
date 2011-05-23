5bc7f53877701567eb181a47ea5bbd7c3d079d7d
Sara-Jane Dunn <sarajane.dunn@gmail.com>
2011-05-23 11:42:43 +0000

    New `BuskeCompressionForce` - see #1764
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk/notforrelease_cell_based@12746 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ src/population/mechanics/BuskeCompressionForce.cpp
@@ -0,0 +1,169 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "BuskeCompressionForce.hpp"
+
+template<unsigned DIM>
+BuskeCompressionForce<DIM>::BuskeCompressionForce()
+    : AbstractForce<DIM>(),
+    mCompressionEnergyParameter(1.0) // Denoted by K in Buske et al (2011) (doi:10.1371/journal.pcbi.1001045).
+{    
+}
+
+template<unsigned DIM>
+BuskeCompressionForce<DIM>::~BuskeCompressionForce()
+{
+}
+
+template<unsigned DIM>
+std::set<unsigned> BuskeCompressionForce<DIM>::GetNeighbouringNodeWithinInteractionDistance(unsigned index, AbstractCellPopulation<DIM>& rCellPopulation)
+{   
+    std::set<unsigned> neighbouring_node_indices;
+    
+    for (typename AbstractCellPopulation<DIM>::Iterator cell_iter = rCellPopulation.Begin();
+     cell_iter != rCellPopulation.End();
+     ++cell_iter)
+    { 
+        unsigned node_j_index = rCellPopulation.GetLocationIndexUsingCell(*cell_iter);
+        
+        // Get the node locations
+        c_vector<double, DIM> node_i_location = rCellPopulation.GetNode(index)->rGetLocation();
+        c_vector<double, DIM> node_j_location = rCellPopulation.GetNode(node_j_index)->rGetLocation();
+    
+        // Get the unit vector parallel to the line joining the two nodes (assuming no periodicities etc.)
+        c_vector<double, DIM> unit_vector = node_i_location - node_j_location;
+    
+        // Calculate the distance between the two nodes            
+        double distance_between_nodes = norm_2(unit_vector);
+        
+        // Determine cell radii
+        //    CellPtr p_cell_A = rCellPopulation.GetCellUsingLocationIndex(nodeAGlobalIndex);
+        //    CellPtr p_cell_B = rCellPopulation.GetCellUsingLocationIndex(nodeBGlobalIndex);
+
+        double radius_of_cell_i = 1.0;
+        double radius_of_cell_j = 1.0;      
+        
+        double max_interaction_distance = radius_of_cell_i + radius_of_cell_j;
+        
+        if (distance_between_nodes < max_interaction_distance)
+        {
+            neighbouring_node_indices.insert(node_j_index);
+        }
+    }
+
+    return neighbouring_node_indices;
+}
+
+template<unsigned DIM>
+void BuskeCompressionForce<DIM>::AddForceContribution(std::vector<c_vector<double, DIM> >& rForces,
+                                                 AbstractCellPopulation<DIM>& rCellPopulation)
+{
+    double radius_of_cell_i;
+    c_vector<double, DIM> unit_vector;
+    
+    for (typename AbstractCellPopulation<DIM>::Iterator cell_iter = rCellPopulation.Begin();
+         cell_iter != rCellPopulation.End();
+         ++cell_iter)
+    {        
+        unsigned node_global_index = rCellPopulation.GetLocationIndexUsingCell(*cell_iter);
+        
+        // Get all of the neighbouring cells
+        std::set<unsigned> neighbouring_node_indices = GetNeighbouringNodeWithinInteractionDistance(node_global_index, rCellPopulation);
+        
+        double delta_V_c = 0.0;
+        double dVAdd = 0.0;
+        
+        for (std::set<unsigned>::iterator iter = neighbouring_node_indices.begin();
+        iter != neighbouring_node_indices.end();
+        ++iter)
+        {                  
+            // Get the node locations
+            c_vector<double, DIM> node_i_location = rCellPopulation.GetNode(node_global_index)->rGetLocation();
+            c_vector<double, DIM> node_j_location = rCellPopulation.GetNode(*iter)->rGetLocation();
+        
+            // Get the unit vector parallel to the line joining the two nodes (assuming no periodicities etc.)
+            unit_vector = node_i_location - node_j_location;
+        
+            // Calculate the distance between the two nodes            
+            double dij = norm_2(unit_vector);
+            
+            unit_vector /= dij;
+                         
+            // Determine cell radii
+            //    CellPtr p_cell_A = rCellPopulation.GetCellUsingLocationIndex(nodeAGlobalIndex);
+            //    CellPtr p_cell_B = rCellPopulation.GetCellUsingLocationIndex(nodeBGlobalIndex);
+
+            radius_of_cell_i = 1.0;
+            double radius_of_cell_j = 1.0;                         
+                                                 
+            double xij = 0.5*(radius_of_cell_i*radius_of_cell_i - radius_of_cell_j*radius_of_cell_j + dij*dij)/dij;
+
+            delta_V_c += M_PI*pow(radius_of_cell_i - xij,2)*(2*radius_of_cell_i - xij)/3.0;
+            
+            
+            double dxijdd = 1.0 - 2.0*xij/dij;
+            
+            dVAdd += M_PI*dxijdd*(5.0*pow(radius_of_cell_i,2) + 3.0*pow(xij,2) - 8.0*radius_of_cell_i*xij)/3.0;
+            
+        }
+        
+        double VA = 4.0/3.0*M_PI*pow(radius_of_cell_i,3) - delta_V_c;
+        
+        /** Target volume of the cell 
+         *  \todo Doesn't say in the Buske paper how they calculate this, so 
+         * we need to look at this to be sure it's what we want
+         */ 
+        double VT = 5.0;
+      
+        double force_magnitude = -mCompressionEnergyParameter/VT*(VT - VA)*dVAdd;
+      
+        rForces[node_global_index] += force_magnitude*unit_vector;      
+    }
+}
+
+template<unsigned DIM>
+void BuskeCompressionForce<DIM>::OutputForceParameters(out_stream& rParamsFile)
+{
+    // No parameters to include
+
+    // Call method on direct parent class
+    AbstractForce<DIM>::OutputForceParameters(rParamsFile);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////////////
+
+template class BuskeCompressionForce<1>;
+template class BuskeCompressionForce<2>;
+template class BuskeCompressionForce<3>;
+
+// Serialization for Boost >= 1.36
+#include "SerializationExportWrapperForCpp.hpp"
+EXPORT_TEMPLATE_CLASS_SAME_DIMS(BuskeCompressionForce)
+
---
+++ src/population/mechanics/BuskeCompressionForce.hpp
@@ -0,0 +1,129 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef BUSKECOMPRESSIONFORCE_HPP_
+#define BUSKECOMPRESSIONFORCE_HPP_
+
+#include "ChasteSerialization.hpp"
+#include <boost/serialization/base_object.hpp>
+
+#include "AbstractForce.hpp"
+#include "TetrahedralMesh.hpp"
+
+// Needed here to avoid serialization errors (on Boost<1.37)
+#include "CellLabel.hpp"
+
+/**
+ * A force law employed by Buske et al (2011) in their overlapping spheres
+ * model of the intestinal crypt (doi:10.1371/journal.pcbi.1001045).
+ *
+ * Length is scaled by natural length. \todo does this mean natural radius of a cell? If so at what age? (#1764)
+ * Time is in hours.
+ * 
+ * This class specifically calculates the compression force which forms part of equation (A6) in the Buske paper.
+ */
+template<unsigned DIM>
+class BuskeCompressionForce  : public AbstractForce<DIM>
+{
+    
+    friend class TestForcesNotForRelease;
+
+private:
+
+    /**
+     * Get the magnitude of the chemotactic force.
+     *
+     * @param concentration the local nutrient concentration
+     * @param concentrationGradientMagnitude the magnitude of the local nutrient concentration gradient
+     */
+//    double GetChemotacticForceMagnitude(const double concentration, const double concentrationGradientMagnitude);
+
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & archive, const unsigned int version)
+    {
+        // If Archive is an output archive, then '&' resolves to '<<'
+        // If Archive is an input archive, then '&' resolves to '>>'
+        archive & boost::serialization::base_object<AbstractForce<DIM> >(*this);
+    }
+    
+    protected:
+    
+    /**
+     * Compression energy parameter
+     *
+     * Represented by the parameter K in the model by Buske et al (2011) in
+     * their off-lattice model of the intestinal crypt
+     * (doi:10.1371/journal.pcbi.1001045).
+     *
+     * Note: K is the bulk modulus of the spheres.
+     */
+    double mCompressionEnergyParameter;
+
+public:
+
+    /**
+     * Constructor.
+     */
+    BuskeCompressionForce();
+
+    /**
+     * Destructor.
+     */
+    ~BuskeCompressionForce();
+
+    /**
+     * Given a node index, returns the set of neighbouring node indices which
+     * lie within a specific interaction distance. 
+     *
+     * @param index the node index
+     *
+     * @return the set of neighbouring node indices.
+     */
+    std::set<unsigned> GetNeighbouringNodeWithinInteractionDistance(unsigned index, AbstractCellPopulation<DIM>& rCellPopulation);
+    
+    /**
+     * Overridden AddForceContribution() method.
+     *
+     */
+    void AddForceContribution(std::vector<c_vector<double, DIM> >& rForces,
+                              AbstractCellPopulation<DIM>& rCellPopulation);
+
+    /**
+     * Overridden OutputForceParameters() method.
+     *
+     * @param rParamsFile the file stream to which the parameters are output
+     */
+    void OutputForceParameters(out_stream& rParamsFile);
+};
+
+#include "SerializationExportWrapper.hpp"
+
+EXPORT_TEMPLATE_CLASS_SAME_DIMS(BuskeCompressionForce)
+
+#endif /*BUSKECOMPRESSIONFORCE_HPP_*/
