dba060f9e2ae33ed8c92f0c3beaa67f858235656
Joe Pitt-Francis <jmpf@cs.ox.ac.uk>
2012-02-20 17:55:49 +0000

    #2038 Making mesh generators more similar and removing "parallel" code
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@14968 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cell_based/src/mesh/HoneycombMeshGenerator.cpp
+++ cell_based/src/mesh/HoneycombMeshGenerator.cpp
@@ -39,7 +39,7 @@ HoneycombMeshGenerator::HoneycombMeshGenerator(unsigned numNodesAlongWidth, unsi
     mNumCellWidth(numNodesAlongWidth), //*1 because cells are considered to be size one
     mNumCellLength(numNodesAlongLength)
 {
-    // The getpid code below won't work in parallel
+    // The code below won't work in parallel
     assert(PetscTools::IsSequential());
 
     // An older version of the constructor might allow the wrong argument through to the scale factor
@@ -55,159 +55,154 @@ HoneycombMeshGenerator::HoneycombMeshGenerator(unsigned numNodesAlongWidth, unsi
     OutputFileHandler output_file_handler("");
     std::string output_dir = output_file_handler.GetOutputDirectoryFullPath();
 
-    if (PetscTools::AmMaster())
+    unsigned num_nodes_along_width = mNumCellWidth;
+    unsigned num_nodes_along_length = mNumCellLength;
+    double horizontal_spacing = mDomainWidth / (double)num_nodes_along_width;
+    double vertical_spacing = (sqrt(3)/2)*horizontal_spacing;
+
+    // This line is needed to define ghost nodes later
+    mDomainDepth = (double)(num_nodes_along_length) * vertical_spacing;
+
+    // Take account of ghost nodes
+    num_nodes_along_width +=  2*ghosts;
+    num_nodes_along_length += 2*ghosts;
+
+    unsigned num_nodes            = num_nodes_along_width*num_nodes_along_length;
+    unsigned num_elem_along_width = num_nodes_along_width-1;
+    unsigned num_elem_along_length = num_nodes_along_length-1;
+    unsigned num_elem             = 2*num_elem_along_width*num_elem_along_length;
+    unsigned num_edges            = 3*num_elem_along_width*num_elem_along_length + num_elem_along_width + num_elem_along_length;
+
+    double x0 = -horizontal_spacing*ghosts;
+    double y0 = -vertical_spacing*ghosts;
+
+    mBottom = -vertical_spacing*ghosts;
+    mTop = mBottom + vertical_spacing*(num_nodes_along_length-1);
+
+    // Write node file
+    out_stream p_node_file = output_file_handler.OpenOutputFile(mMeshFilename+".node");
+    (*p_node_file) << std::scientific;
+    (*p_node_file) << std::setprecision(20);
+    (*p_node_file) << num_nodes << "\t2\t0\t1" << std::endl;
+
+    unsigned node = 0;
+    for (unsigned i=0; i<num_nodes_along_length; i++)
     {
-        unsigned numNodesAlongWidth = mNumCellWidth;
-        unsigned numNodesAlongLength = mNumCellLength;
-        double horizontal_spacing = mDomainWidth / (double)numNodesAlongWidth;
-        double vertical_spacing = (sqrt(3)/2)*horizontal_spacing;
-
-        // This line is needed to define ghost nodes later
-        mDomainDepth = (double)(numNodesAlongLength) * vertical_spacing;
-
-        // Take account of ghost nodes
-        numNodesAlongWidth = numNodesAlongWidth + 2*ghosts;
-        numNodesAlongLength = numNodesAlongLength + 2*ghosts;
-
-        unsigned num_nodes            = numNodesAlongWidth*numNodesAlongLength;
-        unsigned num_elem_along_width = numNodesAlongWidth-1;
-        unsigned num_elem_along_length = numNodesAlongLength-1;
-        unsigned num_elem             = 2*num_elem_along_width*num_elem_along_length;
-        unsigned num_edges            = 3*num_elem_along_width*num_elem_along_length + num_elem_along_width + num_elem_along_length;
-
-        double x0 = -horizontal_spacing*ghosts;
-        double y0 = -vertical_spacing*ghosts;
-
-        mBottom = -vertical_spacing*ghosts;
-        mTop = mBottom + vertical_spacing*(numNodesAlongLength-1);
-
-        // Write node file
-        out_stream p_node_file = output_file_handler.OpenOutputFile(mMeshFilename+".node");
-        (*p_node_file) << std::scientific;
-        (*p_node_file) << setprecision(20);
-        (*p_node_file) << num_nodes << "\t2\t0\t1" << std::endl;
-
-        unsigned node = 0;
-        for (unsigned i=0; i<numNodesAlongLength; i++)
+        for (unsigned j=0; j<num_nodes_along_width; j++)
         {
-            for (unsigned j=0; j<numNodesAlongWidth; j++)
+            if (i<ghosts || i>=(ghosts+mNumCellLength))
             {
-                if (i<ghosts || i>=(ghosts+mNumCellLength))
-                {
-                    mGhostNodeIndices.insert(node);
-                }
-                else if (j < ghosts || j >= (ghosts+mNumCellWidth))
-                {
-                    mGhostNodeIndices.insert(node);
-                }
-                unsigned boundary = 0;
-                if (i==0 || i==numNodesAlongLength-1 || j==0 || j==numNodesAlongWidth-1)
-                {
-                    boundary = 1;
-                }
-
-                double x = x0 + horizontal_spacing*((double)j + 0.25*(1.0+ SmallPow(-1,i+1)));
-                double y = y0 + vertical_spacing*(double)i;
-
-                // Avoid floating point errors which upset OffLatticeSimulation
-                if ( (y<0.0) && (y>-1e-12) )
-                {
-                    // Difficult to cover - just corrects floating point errors that have occurred from time to time!
-                    #define COVERAGE_IGNORE
-                    y = 0.0;
-                    #undef COVERAGE_IGNORE
-                }
-
-                (*p_node_file) << node++ << "\t" << x << "\t" << y << "\t" << boundary << std::endl;
+                mGhostNodeIndices.insert(node);
             }
+            else if (j < ghosts || j >= (ghosts+mNumCellWidth))
+            {
+                mGhostNodeIndices.insert(node);
+            }
+            unsigned boundary = 0;
+            if (i==0 || i==num_nodes_along_length-1 || j==0 || j==num_nodes_along_width-1)
+            {
+                boundary = 1;
+            }
+
+            double x = x0 + horizontal_spacing*((double)j + 0.25*(1.0+ SmallPow(-1,i+1)));
+            double y = y0 + vertical_spacing*(double)i;
+
+            // Avoid floating point errors which upset OffLatticeSimulation
+            if ( (y<0.0) && (y>-1e-12) )
+            {
+                // Difficult to cover - just corrects floating point errors that have occurred from time to time!
+                #define COVERAGE_IGNORE
+                y = 0.0;
+                #undef COVERAGE_IGNORE
+            }
+
+            (*p_node_file) << node++ << "\t" << x << "\t" << y << "\t" << boundary << std::endl;
         }
-        p_node_file->close();
+    }
+    p_node_file->close();
 
-        // Write element file and edge file
-        out_stream p_elem_file = output_file_handler.OpenOutputFile(mMeshFilename+".ele");
-        (*p_elem_file) << std::scientific;
+    // Write element file and edge file
+    out_stream p_elem_file = output_file_handler.OpenOutputFile(mMeshFilename+".ele");
+    (*p_elem_file) << std::scientific;
 
-        out_stream p_edge_file = output_file_handler.OpenOutputFile(mMeshFilename+".edge");
-        (*p_node_file) << std::scientific;
+    out_stream p_edge_file = output_file_handler.OpenOutputFile(mMeshFilename+".edge");
+    (*p_node_file) << std::scientific;
 
-        (*p_elem_file) << num_elem << "\t3\t0" << std::endl;
-        (*p_edge_file) << num_edges << "\t1" << std::endl;
+    (*p_elem_file) << num_elem << "\t3\t0" << std::endl;
+    (*p_edge_file) << num_edges << "\t1" << std::endl;
 
-        unsigned elem = 0;
-        unsigned edge = 0;
-        for (unsigned i=0; i<num_elem_along_length; i++)
+    unsigned elem = 0;
+    unsigned edge = 0;
+    for (unsigned i=0; i<num_elem_along_length; i++)
+    {
+        for (unsigned j=0; j < num_elem_along_width; j++)
         {
-            for (unsigned j=0; j < num_elem_along_width; j++)
+            unsigned node0 =     i*num_nodes_along_width + j;
+            unsigned node1 =     i*num_nodes_along_width + j+1;
+            unsigned node2 = (i+1)*num_nodes_along_width + j;
+
+            if (i%2 != 0)
             {
-                unsigned node0 =     i*numNodesAlongWidth + j;
-                unsigned node1 =     i*numNodesAlongWidth + j+1;
-                unsigned node2 = (i+1)*numNodesAlongWidth + j;
-
-                if (i%2 != 0)
-                {
-                    node2 = node2 + 1;
-                }
-
-                (*p_elem_file) << elem++ << "\t" << node0 << "\t" << node1 << "\t" << node2 << std::endl;
-
-                unsigned horizontal_edge_is_boundary_edge = 0;
-                unsigned vertical_edge_is_boundary_edge = 0;
-                if (i==0)
-                {
-                    horizontal_edge_is_boundary_edge = 1;
-                }
-                if (j==0 && i%2==0)
-                {
-                    vertical_edge_is_boundary_edge = 1;
-                }
-
-                (*p_edge_file) << edge++ << "\t" << node0 << "\t" << node1 << "\t" << horizontal_edge_is_boundary_edge << std::endl;
-                (*p_edge_file) << edge++ << "\t" << node1 << "\t" << node2 << "\t" << 0 << std::endl;
-                (*p_edge_file) << edge++ << "\t" << node2 << "\t" << node0 << "\t" << vertical_edge_is_boundary_edge << std::endl;
-
-                node0 = i*numNodesAlongWidth + j + 1;
-
-                if (i%2 != 0)
-                {
-                    node0 = node0 - 1;
-                }
-                node1 = (i+1)*numNodesAlongWidth + j+1;
-                node2 = (i+1)*numNodesAlongWidth + j;
-
-                (*p_elem_file) << elem++ << "\t" << node0 << "\t" << node1 << "\t" << node2 << std::endl;
+                node2 = node2 + 1;
             }
-        }
 
-        for (unsigned i=0; i<num_elem_along_length; i++)
-        {
-            unsigned node0, node1;
+            (*p_elem_file) << elem++ << "\t" << node0 << "\t" << node1 << "\t" << node2 << std::endl;
 
-            if (i%2==0)
+            unsigned horizontal_edge_is_boundary_edge = 0;
+            unsigned vertical_edge_is_boundary_edge = 0;
+            if (i==0)
             {
-                 node0 = (i+1)*numNodesAlongWidth - 1;
-                 node1 = (i+2)*numNodesAlongWidth - 1;
+                horizontal_edge_is_boundary_edge = 1;
             }
-            else
+            if (j==0 && i%2==0)
             {
-                node0 = (i+1)*numNodesAlongWidth;
-                node1 = (i)*numNodesAlongWidth;
+                vertical_edge_is_boundary_edge = 1;
             }
-            (*p_edge_file) << edge++ << "\t" << node0 << "\t" << node1 << "\t" << 1 << std::endl;
-        }
 
-        for (unsigned j=0; j<num_elem_along_width; j++)
-        {
-            unsigned node0 = numNodesAlongWidth*(numNodesAlongLength-1) + j;
-            unsigned node1 = numNodesAlongWidth*(numNodesAlongLength-1) + j+1;
-            (*p_edge_file) << edge++ << "\t" << node1 << "\t" << node0 << "\t" << 1 << std::endl;
-        }
+            (*p_edge_file) << edge++ << "\t" << node0 << "\t" << node1 << "\t" << horizontal_edge_is_boundary_edge << std::endl;
+            (*p_edge_file) << edge++ << "\t" << node1 << "\t" << node2 << "\t" << 0 << std::endl;
+            (*p_edge_file) << edge++ << "\t" << node2 << "\t" << node0 << "\t" << vertical_edge_is_boundary_edge << std::endl;
 
-        p_elem_file->close();
-        p_edge_file->close();
+            node0 = i*num_nodes_along_width + j + 1;
+
+            if (i%2 != 0)
+            {
+                node0 = node0 - 1;
+            }
+            node1 = (i+1)*num_nodes_along_width + j+1;
+            node2 = (i+1)*num_nodes_along_width + j;
+
+            (*p_elem_file) << elem++ << "\t" << node0 << "\t" << node1 << "\t" << node2 << std::endl;
+        }
     }
 
-    // Wait for the new mesh to be available
-    PetscTools::Barrier();
+    for (unsigned i=0; i<num_elem_along_length; i++)
+    {
+        unsigned node0, node1;
+
+        if (i%2==0)
+        {
+             node0 = (i+1)*num_nodes_along_width - 1;
+             node1 = (i+2)*num_nodes_along_width - 1;
+        }
+        else
+        {
+            node0 = (i+1)*num_nodes_along_width;
+            node1 = (i)*num_nodes_along_width;
+        }
+        (*p_edge_file) << edge++ << "\t" << node0 << "\t" << node1 << "\t" << 1 << std::endl;
+    }
+
+    for (unsigned j=0; j<num_elem_along_width; j++)
+    {
+        unsigned node0 = num_nodes_along_width*(num_nodes_along_length-1) + j;
+        unsigned node1 = num_nodes_along_width*(num_nodes_along_length-1) + j+1;
+        (*p_edge_file) << edge++ << "\t" << node1 << "\t" << node0 << "\t" << 1 << std::endl;
+    }
+
+    p_elem_file->close();
+    p_edge_file->close();
+
 
     // Having written the mesh to file, now construct it using TrianglesMeshReader
     TrianglesMeshReader<2,2> mesh_reader(output_dir + mMeshFilename);
