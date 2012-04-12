b98a6439384d33302f4650478c036c7f99864600
Gary Mirams <gary.mirams@gmail.com>
2012-04-12 09:16:52 +0000

    Mend oneiric build for Pras.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@15337 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/src/solver/mechanics/AbstractCardiacMechanicsSolver.hpp
+++ heart/src/solver/mechanics/AbstractCardiacMechanicsSolver.hpp
@@ -36,6 +36,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef ABSTRACTCARDIACMECHANICSSOLVER_HPP_
 #define ABSTRACTCARDIACMECHANICSSOLVER_HPP_
 
+#include <map>
 #include "IncompressibleNonlinearElasticitySolver.hpp"
 #include "CompressibleNonlinearElasticitySolver.hpp"
 #include "QuadraticBasisFunction.hpp"
