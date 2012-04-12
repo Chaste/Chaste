Chaste/Chaste

commit 07e9b34de26f36570155ca8076bf09893c32b8ce

#2052 Todos: Boundary edge reading in 2D and cable radius reading
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@15296 b5f69653-2bf5-0310-8201-b0dafb8e1405

mesh/src/reader/VtkMeshReader.cpp
@@ -80,24 +80,30 @@ VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::VtkMeshReader(std::string pathBaseName) :
     vtk_xml_unstructured_grid_reader->SetFileName( pathBaseName.c_str() );
     vtk_xml_unstructured_grid_reader->Update();
     mpVtkUnstructuredGrid = vtk_xml_unstructured_grid_reader->GetOutput();
+    CommonConstructor();
+    vtk_xml_unstructured_grid_reader->Delete();
+}
 
-    mNumNodes = vtk_xml_unstructured_grid_reader->GetNumberOfPoints();
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::CommonConstructor()
+{
 
-    unsigned num_cells = vtk_xml_unstructured_grid_reader->GetNumberOfCells();
-
+    mNumNodes = mpVtkUnstructuredGrid->GetNumberOfPoints();
+    unsigned num_cells = mpVtkUnstructuredGrid->GetNumberOfCells();
 
     if (SPACE_DIM == 2)
     {
         mNodesPerElement = 3;
         mVtkCellType = VTK_TRIANGLE;
-        mNumFaces = 0; ///\todo Make the above filter work in 2D
     }
+
     //Determine if we have multiple cell types - such as cable elements in addition to tets/triangles
     vtkCellTypes* cell_types = vtkCellTypes::New();
     mpVtkUnstructuredGrid->GetCellTypes(cell_types);
 
     if(cell_types->GetNumberOfTypes() > 1)
     {
+        mNumCableElementAttributes = 1;
         for(unsigned cell_id = 0; cell_id < num_cells; ++cell_id)
         {
             if(mpVtkUnstructuredGrid->GetCellType(cell_id) == mVtkCellType)
@@ -125,19 +131,29 @@ VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::VtkMeshReader(std::string pathBaseName) :
 
 
     // Extract the surface faces
-    mpVtkGeometryFilter = vtkGeometryFilter::New();
-    mpVtkGeometryFilter->SetInput(mpVtkUnstructuredGrid);
-    mpVtkGeometryFilter->Update();
-    mNumFaces = mpVtkGeometryFilter->GetOutput()->GetNumberOfCells();
-    ///\todo #2052 The boundary face filter includes the cable elements
-    if (mNumCableElements > 0)
+    if (SPACE_DIM == 2)
     {
-        //Stop reading faces
-        mNumFaces =0;
+        vtkDataSetSurfaceFilter* p_surface = vtkDataSetSurfaceFilter::New();
+        p_surface->SetInput(mpVtkUnstructuredGrid);
+        mpVtkFilterEdges = vtkFeatureEdges::New();
+        mpVtkFilterEdges->SetInput(p_surface->GetOutput());
+        mpVtkFilterEdges->Update();
+        mNumFaces = mpVtkFilterEdges->GetOutput()->GetNumberOfCells();
+        p_surface->Delete();
     }
-
-
-    vtk_xml_unstructured_grid_reader->Delete();
+    else
+    {
+        mpVtkGeometryFilter = vtkGeometryFilter::New();
+        mpVtkGeometryFilter->SetInput(mpVtkUnstructuredGrid);
+        mpVtkGeometryFilter->Update();
+        mNumFaces = mpVtkGeometryFilter->GetOutput()->GetNumberOfCells();
+        ///\todo #2052 The boundary face filter includes the cable elements
+        if (mNumCableElements > 0)
+        {
+            //Stop reading faces
+            mNumFaces =0;
+        }
+    }
 }
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
@@ -160,29 +176,20 @@ VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::VtkMeshReader(vtkUnstructuredGrid* p_vtkUn
     mVtkCellType(VTK_TETRA)
 {
     mpVtkUnstructuredGrid = p_vtkUnstructuredGrid;
-
-    mNumNodes = mpVtkUnstructuredGrid->GetNumberOfPoints();
-    mNumElements = mpVtkUnstructuredGrid->GetNumberOfCells();
-
-    // Extract the surface faces
-    mpVtkGeometryFilter = vtkGeometryFilter::New();
-    mpVtkGeometryFilter->SetInput(mpVtkUnstructuredGrid);
-    mpVtkGeometryFilter->Update();
-
-    mNumFaces = mpVtkGeometryFilter->GetOutput()->GetNumberOfCells();
-    assert (SPACE_DIM == 3);
-//    if (SPACE_DIM == 2)
-//    {
-//        mNodesPerElement = 3;
-//        mVtkCellType = VTK_TRIANGLE;
-//        mNumFaces = 0; ///\todo Make the above filter work in 2D
-//    }
+    CommonConstructor();
 }
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::~VtkMeshReader()
 {
-    mpVtkGeometryFilter->Delete();
+    if (SPACE_DIM == 3)
+    {
+        mpVtkGeometryFilter->Delete();
+    }
+    else
+    {
+        mpVtkFilterEdges->Delete();
+    }
 }
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
@@ -313,8 +320,8 @@ ElementData VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::GetNextCableElementData()
         next_element_data.NodeIndices.push_back(mpVtkUnstructuredGrid->GetCell(next_index)->GetPointId(i));
     }
 
-    /// \todo #2052 Look at the radius data too
-    next_element_data.AttributeValue = 0;
+    vtkDataArray *p_scalars = mpVtkUnstructuredGrid->GetCellData()->GetArray( "Cable radius" );
+    next_element_data.AttributeValue = p_scalars->GetTuple(next_index)[0];
 
     mCableElementsRead++;
     return next_element_data;
@@ -331,11 +338,20 @@ ElementData VtkMeshReader<ELEMENT_DIM,SPACE_DIM>::GetNextFaceData()
 
     ElementData next_face_data;
 
-    for (unsigned i = 0; i < (mNodesPerElement-1); i++)
+    if (SPACE_DIM == 3)
     {
-        next_face_data.NodeIndices.push_back(mpVtkGeometryFilter->GetOutput()->GetCell(mBoundaryFacesRead)->GetPointId(i));
+        for (unsigned i = 0; i < (mNodesPerElement-1); i++)
+        {
+            next_face_data.NodeIndices.push_back(mpVtkGeometryFilter->GetOutput()->GetCell(mBoundaryFacesRead)->GetPointId(i));
+        }
     }
-
+    else
+    {
+        for (unsigned i = 0; i < (mNodesPerElement-1); i++)
+        {
+            next_face_data.NodeIndices.push_back(mpVtkFilterEdges->GetOutput()->GetCell(mBoundaryFacesRead)->GetPointId(i));
+        }
+    }
     mBoundaryFacesRead++;
     return next_face_data;
 }

mesh/src/reader/VtkMeshReader.hpp
@@ -63,6 +63,9 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vtkGeometryFilter.h>
 #include <vtkGenericGeometryFilter.h>
 #include <vtkDataCompressor.h>
+#include <vtkFeatureEdges.h>
+#include <vtkDataSetSurfaceFilter.h>
+
 
 #include "UblasVectorInclude.hpp"
 #include "AbstractMeshReader.hpp"
@@ -81,8 +84,11 @@ private:
     /** vtkUnstructuredGrid object: the rest of the class acts as an interface to this */
     vtkUnstructuredGrid* mpVtkUnstructuredGrid;
 
-    /** vtkGeometry filter object to extract the surface elements */
+    /** VTK geometry filter object to extract the surface elements */
     vtkGeometryFilter* mpVtkGeometryFilter;
+
+    /** VTK edge filter object to extract the boundary elements in 2D*/
+    vtkFeatureEdges* mpVtkFilterEdges;
 
     bool mIndexFromZero;             /**< True if input data is numbered from zero, false otherwise */
 
@@ -110,6 +116,14 @@ private:
     unsigned mNodesPerElement;      /**< Number of nodes per element */
 
     int mVtkCellType;               /**< Enumerated as VTK_TETRA in 3D and VTK_TRIANGLE in 2D.  The VTK method GetCellType() returns an int, but this is documented as being non-negative */
+
+    /**
+     * Common constructor which uses the vtkUnstructuredGrid object.  The main constructors
+     * need to make sure that mpVtkUnstructuredGrid is assigned properly.
+     *
+     */
+    void CommonConstructor();
+
 public:
 
     /**
@@ -126,6 +140,8 @@ public:
      * @param p_vtkUnstructuredGrid Pointer to a vtkUnstructuredGrid object
      */
     VtkMeshReader(vtkUnstructuredGrid* p_vtkUnstructuredGrid);
+
+
 
     /**
      * Destructor

mesh/test/reader/TestVtkMeshReader.hpp
@@ -422,15 +422,15 @@ public:
     {
 #ifdef CHASTE_VTK
     VtkMeshReader<2,2> mesh_reader("mesh/test/data/2D_0_to_1mm_200_elements.vtu");
+
+    TS_ASSERT_EQUALS(mesh_reader.GetNumFaces(), 40u);
+
     TetrahedralMesh<2,2> mesh;
     mesh.ConstructFromMeshReader(mesh_reader);
 
     TS_ASSERT_EQUALS(mesh.GetNumNodes(), 121u);
     TS_ASSERT_EQUALS(mesh.GetNumElements(), 200u);
-    // The 2D boundary element code is broken ---
-//    TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 40u);
-//    TS_ASSERT_EQUALS(mesh_reader.GetNumFaces(), 40u);
-    //mesh_reader.GetNextFaceData();
+    TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 40u);
 
     std::vector<c_vector<double, 2> > centroid;
     mesh_reader.GetCellData("Centroid", centroid);
@@ -448,6 +448,7 @@ public:
 #ifdef CHASTE_VTK
         VtkMeshReader<2,2> mesh_reader("mesh/test/data/mixed_dimension_meshes/mixed_mesh_2d.vtu");
         TS_ASSERT_EQUALS(mesh_reader.GetNumCableElements(), 10u);
+        TS_ASSERT_EQUALS(mesh_reader.GetNumCableElementAttributes(), 1u);
 
         MixedDimensionMesh<2,2> mesh(DistributedTetrahedralMeshPartitionType::DUMB);
         mesh.ConstructFromMeshReader(mesh_reader);
@@ -455,16 +456,14 @@ public:
         // Check we have the right number of nodes & elements
         TS_ASSERT_EQUALS(mesh.GetNumNodes(), 121u);
         TS_ASSERT_EQUALS(mesh.GetNumElements(), 200u);
-        ///\todo Fix face filtering in 2D        TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 312u);
-        TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 0u);
+        TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 40u);
         TS_ASSERT_EQUALS(mesh.GetNumCableElements(), 10u);
-
-        ///\todo Check radius data
 
         //Cover exception - note that ConstructFromMeshReader has reset the reader
         for (unsigned i=0; i<10; i++)
         {
-            mesh_reader.GetNextCableElementData();
+            ElementData element_data = mesh_reader.GetNextCableElementData();
+            TS_ASSERT_EQUALS(element_data.AttributeValue, i + 1.5);
         }
         TS_ASSERT_THROWS_THIS(mesh_reader.GetNextCableElementData(), "Trying to read data for a cable element that doesn't exist");
 
@@ -478,6 +477,7 @@ public:
 #ifdef CHASTE_VTK
         VtkMeshReader<3,3> mesh_reader("mesh/test/data/mixed_dimension_meshes/mixed_mesh_3d.vtu");
         TS_ASSERT_EQUALS(mesh_reader.GetNumCableElements(), 5u);
+        TS_ASSERT_EQUALS(mesh_reader.GetNumCableElementAttributes(), 1u);
 
         MixedDimensionMesh<3,3> mesh(DistributedTetrahedralMeshPartitionType::DUMB);
         mesh.ConstructFromMeshReader(mesh_reader);
@@ -488,7 +488,14 @@ public:
         ///\todo This should be 616. 621 includes 5 cables in the face filter. TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 621u);
         TS_ASSERT_EQUALS(mesh.GetNumBoundaryElements(), 0u);
         TS_ASSERT_EQUALS(mesh.GetNumCableElements(), 5u);
-        ///\todo Check radius data
+
+
+        for (MixedDimensionMesh<3,3>::CableElementIterator iter = mesh.GetCableElementIteratorBegin();
+                     iter != mesh.GetCableElementIteratorEnd();
+                     ++iter)
+        {
+            TS_ASSERT_EQUALS((*iter)->GetAttribute(), 2.0);
+        }
 #endif // CHASTE_VTK
     }
 

commit 6cbb7d9a170a9610d766b50bf137d8deb9542fd2

Interleave reading/writing to fix parallel problem caused by r15296/#2052
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@15341 b5f69653-2bf5-0310-8201-b0dafb8e1405

pde/test/utilities/TestHdf5Converters.hpp
@@ -47,6 +47,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "DistributedTetrahedralMesh.hpp"
 #include "TrianglesMeshReader.hpp"
 
+
 #ifdef CHASTE_VTK
 #define _BACKWARD_BACKWARD_WARNING_H 1 // Cut out the strstream deprecated warning for now (gcc4.3)
 #include <vtkVersion.h>
@@ -84,6 +85,7 @@ public:
     {
 #ifdef CHASTE_VTK // Requires  "sudo aptitude install libvtk5-dev" or similar
         std::string working_directory = "TestHdf5ToVtkConverter_bidomain";
+        std::string working_directory2 = "TestHdf5ToVtkConverter_bidomain2";
         OutputFileHandler handler(working_directory);
 
         /*
@@ -99,7 +101,21 @@ public:
 
         // Convert
         Hdf5ToVtkConverter<3,3> converter(working_directory, "cube_2mm_12_elements", &mesh, false, true);
+        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
 
+        // Now do something else before reading back...
+
+        {
+            //NOTE: Interleaved test
+            
+            // Show that trying to write .pvtu files from a TetrahedralMesh gives a warning (but writes anyway)
+            CopyToTestOutputDirectory("pde/test/data/cube_2mm_12_elements.h5",
+                                  working_directory2);
+            Hdf5ToVtkConverter<3,3> converter2(working_directory2, "cube_2mm_12_elements", &mesh, true, true);
+            
+            //The reading part of this test is below
+        }
+            
         /*
          * Note that VTK is not thread-safe. The master process has spawned
          * a child to write the mesh and may still be writing! This barrier
@@ -107,7 +123,6 @@ public:
          */
         PetscTools::Barrier();
 
-        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
 
         VtkMeshReader<3,3> vtk_mesh_reader(test_output_directory + working_directory
                                            + "/vtk_output/cube_2mm_12_elements.vtu");
@@ -135,11 +150,14 @@ public:
         TS_ASSERT_DELTA(phi_at_last[6],  0.0, 1e-3);
         TS_ASSERT_DELTA(phi_at_last[11], 0.0, 1e-3);
 
-        // Show that trying to write .pvtu files from a TetrahedralMesh gives a warning (but writes anyway)
-        Hdf5ToVtkConverter<3,3> converter2(working_directory, "cube_2mm_12_elements", &mesh, true, true);
-        VtkMeshReader<3,3> vtk_mesh_reader2(test_output_directory + working_directory
-                                            + "/vtk_output/cube_2mm_12_elements.vtu");
-        TS_ASSERT_EQUALS(vtk_mesh_reader2.GetNumNodes(), 12u);
+        {
+            //NOTE: Interleaved test
+            //The writing part of this test is above
+            VtkMeshReader<3,3> vtk_mesh_reader2(test_output_directory + working_directory2
+                                                + "/vtk_output/cube_2mm_12_elements.vtu");
+            TS_ASSERT_EQUALS(vtk_mesh_reader2.GetNumNodes(), 12u);
+        }
+
 #endif //CHASTE_VTK
     }
 