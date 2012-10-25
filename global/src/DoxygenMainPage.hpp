b526c1eae928b0ed6d0a7dd43628b08c60e58208
Jonathan Cooper <jonathan.cooper@cs.ox.ac.uk>
2012-10-25 11:25:28 +0000

    #1552 - Make the front Doxygen page prettier, with some helpful text.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@17182 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ global/src/DoxygenMainPage.hpp
@@ -0,0 +1,57 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef DOXYGENMAINPAGE_HPP_
+#define DOXYGENMAINPAGE_HPP_
+
+// Note: this header contains no code; it is purely here to contain the
+// front page content for the Doxygen generated documentation.
+
+/**
+ * \mainpage
+ *
+ * <a href="http://www.cs.ox.ac.uk/chaste/">Chaste</a>
+ * (<b>C</b>ancer, <b>H</b>eart <b>a</b>nd <b>S</b>oft <b>T</b>issue <b>E</b>nvironment) is a general
+ * purpose simulation package aimed at multi-scale, computationally demanding problems arising in biology and
+ * physiology. Current functionality includes tissue and cell level electrophysiology, discrete tissue modelling,
+ * and soft tissue modelling. The package is being developed by a team mainly based in the Computational Biology
+ * Group at the Department of Computer Science, University of Oxford, and development draws on expertise from
+ * software engineering, high performance computing, mathematical modelling and scientific computing.
+ *
+ * These pages contain documentation automatically generated from the Chaste source code, describing the
+ * programming interface to Chaste (the API).  They include details of all the classes, methods, functions, etc.
+ * available.
+ *
+ * For higher-level documentation, including
+ * <a href="https://chaste.cs.ox.ac.uk/cgi-bin/trac.cgi/wiki/DeveloperInstallGuide">installation guides</a>,
+ * <a href="https://chaste.cs.ox.ac.uk/cgi-bin/trac.cgi/wiki/UserTutorials">tutorials</a> and more,
+ * see the main Chaste website at
+ * <a href="http://www.cs.ox.ac.uk/chaste/documentation.html">http://www.cs.ox.ac.uk/chaste/documentation.html</a>.
+ */
+
+#endif // DOXYGENMAINPAGE_HPP_
