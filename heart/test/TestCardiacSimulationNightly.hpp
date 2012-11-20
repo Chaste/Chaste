fe9fc3b57a9aebfe0162d7102948e4e31c74f206
Joe Pitt-Francis <jmpf@cs.ox.ac.uk>
2012-11-20 16:51:45 +0000

    #2232/r17382 Tighten KSP tolerance to make this test portable between IntelProduction and GccOpt/GccOptNative
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@17420 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestCardiacSimulationNightly.hpp
+++ heart/test/TestCardiacSimulationNightly.hpp
@@ -78,8 +78,8 @@ public:
         TS_ASSERT( CompareFilesViaHdf5DataReader("heart/test/data/cardiac_simulations", "postprocess_monodomain_results", false,
                    foldername, "SimulationResults", true));
     }
-    ///#2232 This test is not longer portable between IntelProduction and GccOpt/GccOptNative
-    void doNotTest_TestCardiacSimulationSaveBidomain() throw(Exception)
+
+    void TestCardiacSimulationSaveBidomain() throw(Exception)
     {
         // run a bidomain simulation
         CardiacSimulation simulation("heart/test/data/xml/save_bidomain.xml");
@@ -87,8 +87,7 @@ public:
 
         // compare the files, using the CompareFilesViaHdf5DataReader() method
         TS_ASSERT( CompareFilesViaHdf5DataReader("heart/test/data/cardiac_simulations", "save_bidomain_results", false,
-                   foldername, "SimulationResults", true,
-                   1e-6 /*tol*/));
+                   foldername, "SimulationResults", true));
 
         FileFinder file(foldername + "_checkpoints/10ms/" + foldername + "_10ms/archive.arch.0",
                         RelativeTo::ChasteTestOutput);
