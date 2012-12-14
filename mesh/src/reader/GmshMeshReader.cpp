2863da901da13ee51234fe962d575f4a6aec1bf1
Lorenz Berger <lorenz.berger@dtc.ox.ac.uk>
2012-12-14 12:54:32 +0000

    #2312 Initial commit for the !GmshMeshReader.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@17651 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ mesh/src/reader/GmshMeshReader.cpp
@@ -0,0 +1,203 @@
+/*
+
+Copyright (c) 2005-2012, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#include <cassert>
+#include <sstream>
+#include <iostream>
+
+#include "GmshMeshReader.hpp"
+#include "Exception.hpp"
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GmshMeshReader(std::string pathBaseName) : mFileName(pathBaseName)
+{
+	// Open mesh file
+	mFile.open(mFileName.c_str());
+	if (!mFile.is_open())
+	{
+		EXCEPTION("Could not open data file: " + mFileName);
+	}
+
+	ReadHeader();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::~GmshMeshReader()
+{
+
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::ReadHeader()
+{
+    /*
+     * Read mesh format information from the file header
+     */
+    std::string actual_line;
+
+    mFile >> actual_line;
+    assert(actual_line == "$MeshFormat");
+
+    //Read the version no.
+    mFile >> mVersionNumber >> mFileType >> mDataSize;
+
+    if(mVersionNumber != 2.2)
+    {
+    	EXCEPTION("We can only read Gmsh version 2.2 files.");
+    }
+    assert(mFileType == 0);
+
+    //Check mesh format close string
+	mFile >> actual_line;
+	assert(actual_line == "$EndMeshFormat");
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumElements() const
+{
+	NEVER_REACHED;
+    //return mNumElements;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumNodes() const
+{
+	NEVER_REACHED;
+    //return mNumNodes;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumFaces() const
+{
+	NEVER_REACHED;
+    //return mNumFaces;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumCableElements() const
+{
+	NEVER_REACHED;
+    //return mNumCableElements;
+}
+
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumElementAttributes() const
+{
+	NEVER_REACHED;
+    //return mNumElementAttributes;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumFaceAttributes() const
+{
+	NEVER_REACHED;
+    //return mNumFaceAttributes;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNumCableElementAttributes() const
+{
+	NEVER_REACHED;
+    //return mNumCableElementAttributes;
+}
+
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::Reset()
+{
+	NEVER_REACHED;
+    //
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+std::vector<double> GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNextNode()
+{
+	NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+std::vector<double> GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNodeAttributes()
+{
+	NEVER_REACHED;
+    //return mNodeAttributes;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+ElementData GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNextElementData()
+{
+	NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+ElementData GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNextCableElementData()
+{
+	NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+ElementData GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNextFaceData()
+{
+	NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+std::vector<double> GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetNode(unsigned index)
+{
+	NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+ElementData GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetElementData(unsigned index)
+{
+	NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+ElementData GmshMeshReader<ELEMENT_DIM, SPACE_DIM>::GetFaceData(unsigned index)
+{
+	NEVER_REACHED;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////////////////////
+
+template class GmshMeshReader<0,1>;
+template class GmshMeshReader<1,1>;
+template class GmshMeshReader<1,2>;
+template class GmshMeshReader<1,3>;
+template class GmshMeshReader<2,2>;
+template class GmshMeshReader<2,3>;
+template class GmshMeshReader<3,3>;
---
+++ mesh/src/reader/GmshMeshReader.hpp
@@ -0,0 +1,143 @@
+/*
+
+Copyright (c) 2005-2012, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+#ifndef _GMSHMESHREADER_HPP_
+#define _GMSHMESHREADER_HPP_
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include "AbstractMeshReader.hpp"
+
+/**
+ * Class to enable reading of Gmsh format mesh files (see #2312).
+ */
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+class GmshMeshReader : public AbstractMeshReader<ELEMENT_DIM,SPACE_DIM>
+{
+public:
+
+    /**
+     * Constructor.
+     *
+     * @param pathBaseName  the base name of the files from which to read the mesh data
+     *    (either absolute, or relative to the current directory)
+     */
+    GmshMeshReader(std::string pathBaseName);
+
+    /**
+     * Destructor
+     */
+     ~GmshMeshReader();
+
+    /** Returns the number of elements in the mesh */
+    unsigned GetNumElements() const;
+
+    /** Returns the number of nodes in the mesh */
+    unsigned GetNumNodes() const;
+
+    /** Returns the number of faces in the mesh (synonym of GetNumEdges()) */
+    unsigned GetNumFaces() const;
+
+    /** Returns the number of cable elements in the mesh */
+    unsigned GetNumCableElements() const;
+
+    /** Returns the number of attributes in the mesh */
+    unsigned GetNumElementAttributes() const;
+
+    /** Returns the number of attributes in the mesh */
+    unsigned GetNumFaceAttributes() const;
+
+    /** Returns the number of cable element attributes in the mesh */
+    unsigned GetNumCableElementAttributes() const;
+
+    /** Resets pointers to beginning*/
+    void Reset();
+
+    /** Returns a vector of the coordinates of each node in turn */
+    std::vector<double> GetNextNode();
+
+    /** Returns a vector of the nodes of each element (and any attribute information, if there is any) in turn */
+    ElementData GetNextElementData();
+
+    /** Returns a vector of the nodes of each face in turn (synonym of GetNextEdgeData()) */
+    ElementData GetNextFaceData();
+
+    /** Returns a vector of the node indices of each cable element (and any attribute information, if there is any) in turn */
+    ElementData GetNextCableElementData();
+
+    /**
+     * @return the vector of node attributes
+     */
+    std::vector<double> GetNodeAttributes();
+
+    /**
+     *  Normally throws an exception.  Only implemented for tetrahedral mesh reader of binary files.
+     *
+     * @param index  The global node index
+     * @return a vector of the coordinates of the node
+     */
+    std::vector<double> GetNode(unsigned index);
+
+    /**
+     *  Normally throws an exception.  Only implemented for tetrahedral mesh reader of binary files.
+     *
+     * @param index  The global element index
+     * @return a vector of the node indices of the element (and any attribute information, if there is any)
+     */
+    ElementData GetElementData(unsigned index);
+
+    /**
+     *  Normally throws an exception.  Only implemented for tetrahedral mesh reader of binary files.
+     *
+     * @param index  The global face index
+     * @return a vector of the node indices of the face (and any attribute/containment information, if there is any)
+     */
+    ElementData GetFaceData(unsigned index);
+
+    /** Read the header from the mesh file. */
+    void ReadHeader();
+
+private:
+    std::string mFileName;     /**< The name of the mesh file. */
+    std::ifstream mFile;       /**< The file for the mesh. */
+    double mVersionNumber;     /**< The version number of the file. */
+    unsigned mFileType;  	   /**< The type of the mesh file being read (should always be 0) */
+    unsigned mDataSize;		   /**< The number of floating point numbers in the file */
+
+};
+
+#endif //_GMSHMESHREADER_HPP_
