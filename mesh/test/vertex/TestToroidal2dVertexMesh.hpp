5a7dea2d481bd4977bc4c325ce025524dbef9e53
Alex Fletcher <fletcher@maths.ox.ac.uk>
2013-05-07 09:30:19 +0000

    #2380 - attempt to fix memory leak
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@18805 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- mesh/test/vertex/TestToroidal2dVertexMesh.hpp
+++ mesh/test/vertex/TestToroidal2dVertexMesh.hpp
@@ -587,8 +587,8 @@ public:
 
         // A point outside the element due to periodicity
         c_vector<double, 2> test_point2;
-        test_point1[0] = 3.0;
-        test_point1[1] = 1.0;
+        test_point2[0] = 3.0;
+        test_point2[1] = 1.0;
         TS_ASSERT_EQUALS(mesh.ElementIncludesPoint(test_point2, 0), false);
 
         // Another point outside the element due to periodicity
