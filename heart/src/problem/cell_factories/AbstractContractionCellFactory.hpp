3bef80a902748d20c5b39c03e0190765ce452b22
Valentina Carapella <valentina.carapella@dtc.ox.ac.uk>
2013-05-16 15:32:22 +0000

    #2370 We added the !AbstractContractionCellFactory and tested it in !TestAbstractContractionCellFactory.hpp
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@18888 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ heart/src/problem/cell_factories/AbstractContractionCellFactory.hpp
@@ -0,0 +1,101 @@
+/*
+
+Copyright (c) 2005-2013, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef ABSTRACTCONTRACTIONCELLFACTORY_HPP_
+#define ABSTRACTCONTRACTIONCELLFACTORY_HPP_
+
+
+
+#include "AbstractCardiacCellFactory.hpp"
+#include "AbstractContractionModel.hpp"
+
+
+/**
+ * A factory to ease creating contraction cell models for use in a electro-mechanics simulations.
+ *
+ * The user should implement their own concrete class, in particular implementing
+ * CreateContractionCellForElement(unsigned), which should return the contraction model corresponding to a
+ * given element. The user should also implement GetNumberOfCells() if this isn't equal
+ * to the number of nodes. FinaliseCellCreation() can be used to (eg) add stimuli to
+ * certain cells after they have been created.
+ */
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM = ELEMENT_DIM>
+class AbstractContractionCellFactory : public AbstractCardiacCellFactory<ELEMENT_DIM, SPACE_DIM>
+{
+public:
+    /**
+     * @return a newly created contraction model for the given quadrature point.
+     *
+     * Note that there is no vector of all the quadrature points of the mesh;
+     * the quad point index is the index that would be obtained by looping over
+     * elements and then looping over quad points.
+     *
+     * @param quadIndex  Global element index.
+     */
+    virtual AbstractContractionModel* CreateContractionCellForQuadPoint(unsigned quadIndex) = 0;
+
+    //
+    // \todo 2370 The methods below are likely to be required for the final version of this class.
+    // Thus the definitions have been left in place commented out
+    //
+
+    /**
+     * May be overridden by subclasses to perform any necessary work after all cells
+     * have been created.
+     *
+     * @param pCellsDistributed  Pointer to a vector of cardiac cell pointers.
+     * @param lo  Lowest index owned by this process.
+     * @param hi  Highest index owned by this process.
+     */
+    //virtual void FinaliseCellCreation(std::vector< AbstractCardiacCellInterface* >* pCellsDistributed,
+    //                                  unsigned lo, unsigned hi);
+
+    /**
+     * Default constructor.
+     *
+     * @param pSolver  the ODE solver to use to simulate this cell.
+     */
+   // AbstractContractionCellFactory(boost::shared_ptr<AbstractIvpOdeSolver> pSolver = boost::shared_ptr<AbstractIvpOdeSolver>(new EulerIvpOdeSolver));
+
+    /**
+     * Destructor: free solver, zero stimulus and fake bath cell.
+     */
+    //virtual ~AbstractContractionCellFactory();
+
+
+};
+
+
+#endif /* ABSTRACTCONTRACTIONCELLFACTORY_HPP_ */
---
+++ heart/test/TestAbstractContractionCellFactory.hpp
@@ -0,0 +1,153 @@
+/*
+
+Copyright (c) 2005-2013, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef TESTABSTRACTCONTRACTIONCELLFACTORY_HPP_
+#define TESTABSTRACTCONTRACTIONCELLFACTORY_HPP_
+
+
+
+
+#include <cxxtest/TestSuite.h>
+#include "AbstractContractionCellFactory.hpp"
+#include "TrianglesMeshReader.hpp"
+#include "MixedDimensionMesh.hpp"
+#include "TetrahedralMesh.hpp"
+#include "FakeBathCell.hpp"
+#include "SimpleStimulus.hpp"
+#include "LuoRudy1991.hpp"
+#include "DiFrancescoNoble1985.hpp"
+#include "PetscSetupAndFinalize.hpp"
+#include "ContractionModelName.hpp"
+
+#include "NonPhysiologicalContractionModel.hpp"
+#include "Kerchoffs2003ContractionModel.hpp"
+#include "Nash2004ContractionModel.hpp"
+#include "ConstantActiveTension.hpp"
+
+class LabelBasedContractionCellFactory : public AbstractContractionCellFactory<2>
+{
+private:
+    ContractionModelName mContractionModelName;
+
+public:
+    LabelBasedContractionCellFactory(ContractionModelName contractionModelName)
+        : AbstractContractionCellFactory<2>(),
+          mContractionModelName(contractionModelName)
+    {
+    }
+
+    AbstractCardiacCellInterface* CreateCardiacCellForTissueNode(unsigned node)
+    {
+        return new CellLuoRudy1991FromCellML(mpSolver, mpZeroStimulus);
+    }
+
+    AbstractContractionModel* CreateContractionCellForQuadPoint(unsigned quadIndex)
+    {
+        switch(mContractionModelName)
+        {
+            case CONSTANT:
+            {
+                return new ConstantActiveTension;
+                break;
+            }
+            case NONPHYSIOL1:
+            case NONPHYSIOL2:
+            case NONPHYSIOL3:
+            {
+                unsigned option = (mContractionModelName==NONPHYSIOL1 ? 1 : (mContractionModelName==NONPHYSIOL2? 2 : 3));
+                return new NonPhysiologicalContractionModel(option);
+                break;
+            }
+            case NASH2004: //stretch dependent, will this work with explicit??
+            {
+                return new Nash2004ContractionModel;
+                break;
+            }
+            case KERCHOFFS2003: //stretch dependent, will this work with explicit? Answer: can be unstable
+            {
+                return new Kerchoffs2003ContractionModel;
+                break;
+            }
+            default:
+            {
+                EXCEPTION("Unknown or stretch-rate-dependent contraction model");
+            }
+        }
+    }
+};
+
+
+class TestAbstractContractionCellFactory : public CxxTest::TestSuite
+{
+public:
+    void TestContractionCellFactory() throw (Exception)
+    {
+        {
+            LabelBasedContractionCellFactory factory(CONSTANT);
+
+            ConstantActiveTension* p_model =
+                dynamic_cast<ConstantActiveTension*> (factory.CreateContractionCellForQuadPoint(0u));
+            TS_ASSERT(p_model);
+        }
+
+        {
+            LabelBasedContractionCellFactory factory(NONPHYSIOL1);
+
+            NonPhysiologicalContractionModel* p_model =
+                dynamic_cast<NonPhysiologicalContractionModel*> (factory.CreateContractionCellForQuadPoint(0u));
+            TS_ASSERT(p_model);
+        }
+
+        {
+            LabelBasedContractionCellFactory factory(NASH2004);
+
+            Nash2004ContractionModel* p_model =
+                dynamic_cast<Nash2004ContractionModel*> (factory.CreateContractionCellForQuadPoint(0u));
+            TS_ASSERT(p_model);
+        }
+
+        {
+            LabelBasedContractionCellFactory factory(KERCHOFFS2003);
+
+            Kerchoffs2003ContractionModel* p_model =
+                dynamic_cast<Kerchoffs2003ContractionModel*> (factory.CreateContractionCellForQuadPoint(0u));
+            TS_ASSERT(p_model);
+        }
+    }
+};
+
+
+
+#endif /* TESTABSTRACTCONTRACTIONCELLFACTORY_HPP_ */
