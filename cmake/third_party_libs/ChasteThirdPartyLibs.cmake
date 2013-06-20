130d70a816bfe0a20623ef6e85d217b6194e2af9
Jonathan Cooper <jonathan.cooper@cs.ox.ac.uk>
2013-06-20 10:43:14 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@19227 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ cmake/third_party_libs/ChasteThirdPartyLibs.cmake
@@ -0,0 +1,12 @@
+#URLs to Third party libraries needed by Chaste
+
+# Specify the urls of the libraries you want to build separated by spaces and/or newlines, or as separate strings.
+# Note that the URLS of PARMETIS, METIS, F2CBLAS, F2CLAPACK are all automatically obtained from the
+# PETSc distribution once it has been downloaded and unzipped. So, there is no need to manually
+# specify the URLs for these libraries 
+set(PETSC_URLS "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p6.tar.gz")
+set(HDF5_URLS "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.10-patch1/src/hdf5-1.8.10-patch1.tar.gz")
+set(SUNDIALS_URLS "https://computation.llnl.gov/casc/sundials/download/code/sundials-2.5.0.tar.gz")
+set(BOOST_URLS "http://kent.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.tar.gz" 
+			   #"http://kent.dl.sourceforge.net/project/boost/boost/1.52.0/boost_1_52_0.tar.gz" 
+			)
---
+++ cxx_flag_overrides.cmake
@@ -0,0 +1,8 @@
+#Link statically
+if(MSVC) # Applies only to MSVC
+	set(CMAKE_BUILD_TYPE Debug)
+	set(CMAKE_CXX_FLAGS_DEBUG "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
+	set(CMAKE_CXX_FLAGS_MINSIZEREL "/MT /O1 /Ob1 /D NDEBUG")
+	set(CMAKE_CXX_FLAGS_RELEASE "/MT /O2 /Ob2 /D NDEBUG")
+	set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MT /Zi /O2 /Ob1 /D NDEBUG")
+endif(MSVC)
