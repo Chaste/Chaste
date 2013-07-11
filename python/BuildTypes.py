Chaste/Chaste

commit 61ca9118f98bd2fa1aad4c414066da3bb849ba2f

git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@19160 b5f69653-2bf5-0310-8201-b0dafb8e1405

python/BuildTypes.py
@@ -562,12 +562,12 @@ class GoogleProfile(GccDebug):
         self._cc_flags.append('-O3')
         # Array bounds checking is available in -O2 optimization and above
         # Tetgen code produces a couple of issues, but g++ sometimes gives false negatives.
-        self._cc_flags.append('-Wno-array-bounds')
-        self._link_flags.append('-lprofiler')
+        self._cc_flags.extend(['-Wno-array-bounds', '-fno-omit-frame-pointer'])
+        #self._link_flags.append('-lprofiler')
         self._test_packs = ['Profile']
         self.build_dir = 'google_profile'
         self.is_profile = True
- 
+    
     def ParseGraphFilename(self, filename):
         "Remove the string 'Runner.gif' from the end of a filename, thus returning test_suite name"
         return filename[:-10]
@@ -575,15 +575,17 @@ class GoogleProfile(GccDebug):
     def GetTestRunnerCommand(self, exefile, exeflags=''):
         """Run the test with profiling on and analyse the results."""
         base = os.path.basename(exefile)
-        profile_file = '/tmp/' + base + '.prof'
+        profile_file = os.path.join(self.env['ENV']['CHASTE_TEST_OUTPUT'], base + '.prof')
         pprof_args = ' '.join(['--gif',
                                '--nodefraction=0.0001', '--edgefraction=0.0001',
                                exefile, profile_file,
                                '>', os.path.join(self.output_dir, base+'.gif')])
+        preload_hack = '' #'LD_PRELOAD=/usr/local/lib/libprofiler.so '
         commands = ['export HOME="."',
-                    'LD_PRELOAD=/usr/lib/libprofiler.so CPUPROFILE=' + profile_file + ' ' + exefile + ' ' + exeflags,
+                    'export CPUPROFILE="%s"' % profile_file,
+                    preload_hack + exefile + ' ' + exeflags,
                     self.tools['pprof'] + ' ' + pprof_args,
-                    self.tools['rm'] + ' ' + profile_file]
+                    self.tools['rm'] + ' -f ' + profile_file]
         return '; '.join(commands)
     
     def SetNumProcesses(self, np):

SConstruct
@@ -319,6 +319,7 @@ env.Replace(CPPPATH = cpppath)
 
 # Some state needed by our build system
 env['build'] = build
+build.env = env
 env['buildsig'] = build.GetSignature()
 env['CHASTE_COMPONENTS'] = components + ['projects']
 env['CHASTE_COMP_DEPS'] = comp_deps

commit 36fc77c596dd98e69150dbfbc30fc455d10e7d25

Avoid extra test re-running caused by r19160 adding the SCons environment as an attribute on build objects.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@19426 b5f69653-2bf5-0310-8201-b0dafb8e1405

python/BuildTypes.py
@@ -86,6 +86,16 @@ class BuildType(object):
                       'gprof': 'gprof', 'pprof': 'pprof',
                       'rm': 'rm', 'cat': 'cat'}
 
+    def __getstate__(self):
+        """Implementation of pickle protocol.
+
+        This is provided because SCons uses the pickle of objects to determine when to rebuild things built
+        with a Python function action.  We need to ignore the build environment or we'll always rebuild.
+        """
+        d = self.__dict__.copy()
+        d.pop('env', None)
+        return d
+
     def DumpDebugInfo(self):
         """Print out some useful build system debugging information."""
         print "Build class", self.__class__.__name__, "from build type", self.build_type