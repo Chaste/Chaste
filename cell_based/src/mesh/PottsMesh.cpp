Chaste/Chaste

commit bf851be455b304b59123efe7135b04820a5a083e

#2012 Added method to remove a node form a `PottsMesh`
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@20080 b5f69653-2bf5-0310-8201-b0dafb8e1405

cell_based/src/mesh/PottsMesh.cpp
@@ -251,6 +251,120 @@ void PottsMesh<DIM>::DeleteElement(unsigned index)
 }
 
 template<unsigned DIM>
+void PottsMesh<DIM>::DeleteNode(unsigned index)
+{
+	//Mark node as deleted so we don't consider it when iterating over nodes
+	this->mNodes[index]->MarkAsDeleted();
+
+    //Remove from Elements
+	std::set<unsigned> containing_element_indices = this->mNodes[index]->rGetContainingElementIndices();
+
+	for (std::set<unsigned>::iterator iter = containing_element_indices.begin();
+		 iter != containing_element_indices.end();
+		 iter++)
+	{
+		assert(mElements[*iter]->GetNumNodes() > 0);
+		if(mElements[*iter]->GetNumNodes() == 1)
+		{
+			DeleteElement(*iter);
+		}
+		else
+		{
+			this->mElements[*iter]->DeleteNode(this->mElements[*iter]->GetNodeLocalIndex(index));
+		}
+	}
+
+	//Remove from connectivity
+	mVonNeumannNeighbouringNodeIndices[index].clear();
+	mMooreNeighbouringNodeIndices[index].clear();
+
+	assert(mVonNeumannNeighbouringNodeIndices.size()==mMooreNeighbouringNodeIndices.size());
+	for( unsigned node_index = 0;
+	     node_index < mVonNeumannNeighbouringNodeIndices.size();
+         node_index++)
+	{
+		// Remove node "index" from the Von Neuman neighbourhood of node "node_index".
+		mVonNeumannNeighbouringNodeIndices[node_index].erase(index);
+		mMooreNeighbouringNodeIndices[node_index].erase(index);
+
+		// Check there's still connectivity for the other non-deleted nodes
+		if (!this->mNodes[index]->IsDeleted())
+		{
+			assert(mVonNeumannNeighbouringNodeIndices[index].size()>0);
+			assert(mMooreNeighbouringNodeIndices[index].size()>0);
+		}
+	}
+
+
+	// Remove node from mNodes and renumber all the elements and nodes
+
+	this->mNodes.erase(this->mNodes.begin()+index);
+	unsigned num_nodes = GetNumNodes();
+	mVonNeumannNeighbouringNodeIndices.erase(mVonNeumannNeighbouringNodeIndices.begin()+index);
+	mMooreNeighbouringNodeIndices.erase(mMooreNeighbouringNodeIndices.begin()+index);
+
+	assert(mVonNeumannNeighbouringNodeIndices.size()==num_nodes);
+    assert(mMooreNeighbouringNodeIndices.size()==num_nodes);
+
+    for( unsigned node_index = 0; node_index < num_nodes; node_index++)
+	{
+    	// Reduce the index of all nodes greater than  node "index"
+    	if (node_index >= index)
+    	{
+    		assert(this->mNodes[node_index]->GetIndex() == node_index+1);
+			this->mNodes[node_index]->SetIndex(node_index);
+    	}
+    	assert(this->mNodes[node_index]->GetIndex() == node_index);
+
+		// Reduce the index of all nodes greater than  node "index"
+    	// in the Moore and Von Neuman neighbourhoods.
+    	std::set<unsigned> von_neuman = mVonNeumannNeighbouringNodeIndices[node_index];
+    	mVonNeumannNeighbouringNodeIndices[node_index].clear();
+    	for (std::set<unsigned>::iterator iter = von_neuman.begin();
+    	      iter != von_neuman.end();
+    	      iter++)
+    	{
+    		if (*iter >= index)
+    		{
+    			mVonNeumannNeighbouringNodeIndices[node_index].insert(*iter-1);
+    		}
+    		else
+    		{
+    			mVonNeumannNeighbouringNodeIndices[node_index].insert(*iter);
+    		}
+    	}
+       	std::set<unsigned> moore = mMooreNeighbouringNodeIndices[node_index];
+       	mMooreNeighbouringNodeIndices[node_index].clear();
+		for (std::set<unsigned>::iterator iter = moore.begin();
+			  iter != moore.end();
+			  iter++)
+		{
+			if (*iter >= index)
+			{
+				mMooreNeighbouringNodeIndices[node_index].insert(*iter-1);
+			}
+			else
+			{
+				mMooreNeighbouringNodeIndices[node_index].insert(*iter);
+			}
+		}
+   	}
+    // Finally remove any elememts that have been removed
+    assert(mDeletedElementIndices.size() <= 1); // Should have at most one element to remove
+    if(mDeletedElementIndices.size() == 1)
+    {
+    	unsigned elem_index = mDeletedElementIndices[0];
+    	mElements.erase(mElements.begin()+elem_index);
+    	mDeletedElementIndices.clear();
+
+    	for (unsigned index=elem_index; index<GetNumElements(); index++)
+    	{
+    		mElements[index]->ResetIndex(index);
+    	}
+    }
+}
+
+template<unsigned DIM>
 unsigned PottsMesh<DIM>::DivideElement(PottsElement<DIM>* pElement,
                                   bool placeOriginalElementBelow)
 {

cell_based/src/mesh/PottsMesh.hpp
@@ -287,6 +287,13 @@ public:
     std::set<unsigned> GetVonNeumannNeighbouringNodeIndices(unsigned nodeIndex);
 
     /**
+     * Mark a node as deleted. Note that in a Potts mesh this requires the elements and connectivity to be updated accordingley.
+     *
+     * @param index  the global index of a specified node
+     */
+    void DeleteNode(unsigned index);
+
+    /**
      * Mark an element as deleted. Note that in a Potts mesh this does not
      * delete the nodes so no remeshing is required.
      *

cell_based/test/mesh/TestPottsMesh.hpp
@@ -1678,6 +1678,83 @@ public:
         //TS_ASSERT_EQUALS(basic_potts_mesh.GetElement(1)->GetNodeGlobalIndex(0), 3u);
     }
 
+    void TestDeleteNode() throw(Exception)
+	{
+    	// Create simle mesh with 2 pots elements and connectivities
+    	PottsMeshGenerator<2> generator(2, 2, 1, 2, 1, 2);
+        PottsMesh<2>* p_mesh = generator.GetMesh();
+
+		TS_ASSERT_EQUALS(p_mesh->GetNumElements(), 2u);
+		TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 4u);
+
+		// Delete node 0, note this decreases the index of all nodes greater than 0
+		p_mesh->DeleteNode(0);
+
+		TS_ASSERT_EQUALS(p_mesh->GetNumElements(), 2u);
+		TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 3u);
+
+
+		// Test Node Locations
+		TS_ASSERT_DELTA(p_mesh->GetNode(0)->rGetLocation()[0], 1.0, 1e-3);
+		TS_ASSERT_DELTA(p_mesh->GetNode(0)->rGetLocation()[1], 0.0, 1e-3);
+		TS_ASSERT_DELTA(p_mesh->GetNode(1)->rGetLocation()[0], 0.0, 1e-3);
+		TS_ASSERT_DELTA(p_mesh->GetNode(1)->rGetLocation()[1], 1.0, 1e-3);
+		TS_ASSERT_DELTA(p_mesh->GetNode(2)->rGetLocation()[0], 1.0, 1e-3);
+		TS_ASSERT_DELTA(p_mesh->GetNode(2)->rGetLocation()[1], 1.0, 1e-3);
+
+
+		// Test elements have correct nodes
+		TS_ASSERT_EQUALS(p_mesh->GetElement(0)->GetNumNodes(), 1u);
+		TS_ASSERT_EQUALS(p_mesh->GetElement(0)->GetNodeGlobalIndex(0), 1u);
+
+		TS_ASSERT_EQUALS(p_mesh->GetElement(1)->GetNumNodes(), 2u);
+		TS_ASSERT_EQUALS(p_mesh->GetElement(1)->GetNodeGlobalIndex(0), 0u);
+		TS_ASSERT_EQUALS(p_mesh->GetElement(1)->GetNodeGlobalIndex(1), 2u);
+
+		//Test Neighbourhoods
+		// Test Node 0
+		std::set<unsigned> neighbouring_sites = p_mesh->GetVonNeumannNeighbouringNodeIndices(0);
+		TS_ASSERT_EQUALS(neighbouring_sites.size(), 1u);
+		std::set<unsigned> expected_neighbouring_sites;
+		expected_neighbouring_sites.insert(2u);
+		TS_ASSERT_EQUALS(neighbouring_sites, expected_neighbouring_sites);
+		expected_neighbouring_sites.insert(1u);
+		neighbouring_sites = p_mesh->GetMooreNeighbouringNodeIndices(0);
+		TS_ASSERT_EQUALS(neighbouring_sites.size(), 2u);
+		TS_ASSERT_EQUALS(neighbouring_sites, expected_neighbouring_sites);
+
+		//Test Node 1
+		neighbouring_sites = p_mesh->GetVonNeumannNeighbouringNodeIndices(1);
+		TS_ASSERT_EQUALS(neighbouring_sites.size(), 1u);
+		expected_neighbouring_sites.clear();
+		expected_neighbouring_sites.insert(2);
+		TS_ASSERT_EQUALS(neighbouring_sites, expected_neighbouring_sites);
+		neighbouring_sites = p_mesh->GetMooreNeighbouringNodeIndices(1);
+		expected_neighbouring_sites.insert(0);
+		TS_ASSERT_EQUALS(neighbouring_sites.size(), 2u);
+		TS_ASSERT_EQUALS(neighbouring_sites, expected_neighbouring_sites);
+
+		// Test Node 3
+		neighbouring_sites = p_mesh->GetVonNeumannNeighbouringNodeIndices(2);
+		TS_ASSERT_EQUALS(neighbouring_sites.size(), 2u);
+		expected_neighbouring_sites.clear();
+		expected_neighbouring_sites.insert(0);
+		expected_neighbouring_sites.insert(1);
+		TS_ASSERT_EQUALS(neighbouring_sites, expected_neighbouring_sites);
+		neighbouring_sites = p_mesh->GetMooreNeighbouringNodeIndices(2);
+		TS_ASSERT_EQUALS(neighbouring_sites.size(), 2u);
+		TS_ASSERT_EQUALS(neighbouring_sites, expected_neighbouring_sites);
+
+		//For coverage delete all nodes in an element.
+		p_mesh->DeleteNode(1);
+
+		TS_ASSERT_EQUALS(p_mesh->GetNumElements(), 1u);
+		TS_ASSERT_EQUALS(p_mesh->GetNumAllElements(), 1u);
+		TS_ASSERT_EQUALS(p_mesh->GetElement(0)->GetIndex(), 0u);
+
+		TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 2u);
+	}
+
     void TestArchive2dPottsMesh()
     {
         FileFinder archive_dir("archive", RelativeTo::ChasteTestOutput);

commit 1f50b3e9df77c16601e9f4fdf7ba4e70f6faebfe

Fix Intel build broken by Ozzy in r20080 (#2012).
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@20107 b5f69653-2bf5-0310-8201-b0dafb8e1405

cell_based/src/mesh/PottsMesh.cpp
@@ -253,120 +253,120 @@ void PottsMesh<DIM>::DeleteElement(unsigned index)
 template<unsigned DIM>
 void PottsMesh<DIM>::DeleteNode(unsigned index)
 {
-	//Mark node as deleted so we don't consider it when iterating over nodes
-	this->mNodes[index]->MarkAsDeleted();
+    //Mark node as deleted so we don't consider it when iterating over nodes
+    this->mNodes[index]->MarkAsDeleted();
 
     //Remove from Elements
-	std::set<unsigned> containing_element_indices = this->mNodes[index]->rGetContainingElementIndices();
+    std::set<unsigned> containing_element_indices = this->mNodes[index]->rGetContainingElementIndices();
 
-	for (std::set<unsigned>::iterator iter = containing_element_indices.begin();
-		 iter != containing_element_indices.end();
-		 iter++)
-	{
-		assert(mElements[*iter]->GetNumNodes() > 0);
-		if(mElements[*iter]->GetNumNodes() == 1)
-		{
-			DeleteElement(*iter);
-		}
-		else
-		{
-			this->mElements[*iter]->DeleteNode(this->mElements[*iter]->GetNodeLocalIndex(index));
-		}
-	}
+    for (std::set<unsigned>::iterator iter = containing_element_indices.begin();
+         iter != containing_element_indices.end();
+         iter++)
+    {
+        assert(mElements[*iter]->GetNumNodes() > 0);
+        if(mElements[*iter]->GetNumNodes() == 1)
+        {
+            DeleteElement(*iter);
+        }
+        else
+        {
+            this->mElements[*iter]->DeleteNode(this->mElements[*iter]->GetNodeLocalIndex(index));
+        }
+    }
 
-	//Remove from connectivity
-	mVonNeumannNeighbouringNodeIndices[index].clear();
-	mMooreNeighbouringNodeIndices[index].clear();
+    //Remove from connectivity
+    mVonNeumannNeighbouringNodeIndices[index].clear();
+    mMooreNeighbouringNodeIndices[index].clear();
 
-	assert(mVonNeumannNeighbouringNodeIndices.size()==mMooreNeighbouringNodeIndices.size());
-	for( unsigned node_index = 0;
-	     node_index < mVonNeumannNeighbouringNodeIndices.size();
+    assert(mVonNeumannNeighbouringNodeIndices.size()==mMooreNeighbouringNodeIndices.size());
+    for( unsigned node_index = 0;
+         node_index < mVonNeumannNeighbouringNodeIndices.size();
          node_index++)
-	{
-		// Remove node "index" from the Von Neuman neighbourhood of node "node_index".
-		mVonNeumannNeighbouringNodeIndices[node_index].erase(index);
-		mMooreNeighbouringNodeIndices[node_index].erase(index);
+    {
+        // Remove node "index" from the Von Neuman neighbourhood of node "node_index".
+        mVonNeumannNeighbouringNodeIndices[node_index].erase(index);
+        mMooreNeighbouringNodeIndices[node_index].erase(index);
 
-		// Check there's still connectivity for the other non-deleted nodes
-		if (!this->mNodes[index]->IsDeleted())
-		{
-			assert(mVonNeumannNeighbouringNodeIndices[index].size()>0);
-			assert(mMooreNeighbouringNodeIndices[index].size()>0);
-		}
-	}
+        // Check there's still connectivity for the other non-deleted nodes
+        if (!this->mNodes[index]->IsDeleted())
+        {
+            assert(mVonNeumannNeighbouringNodeIndices[index].size()>0);
+            assert(mMooreNeighbouringNodeIndices[index].size()>0);
+        }
+    }
 
 
-	// Remove node from mNodes and renumber all the elements and nodes
+    // Remove node from mNodes and renumber all the elements and nodes
 
-	this->mNodes.erase(this->mNodes.begin()+index);
-	unsigned num_nodes = GetNumNodes();
-	mVonNeumannNeighbouringNodeIndices.erase(mVonNeumannNeighbouringNodeIndices.begin()+index);
-	mMooreNeighbouringNodeIndices.erase(mMooreNeighbouringNodeIndices.begin()+index);
+    this->mNodes.erase(this->mNodes.begin()+index);
+    unsigned num_nodes = GetNumNodes();
+    mVonNeumannNeighbouringNodeIndices.erase(mVonNeumannNeighbouringNodeIndices.begin()+index);
+    mMooreNeighbouringNodeIndices.erase(mMooreNeighbouringNodeIndices.begin()+index);
 
-	assert(mVonNeumannNeighbouringNodeIndices.size()==num_nodes);
+    assert(mVonNeumannNeighbouringNodeIndices.size()==num_nodes);
     assert(mMooreNeighbouringNodeIndices.size()==num_nodes);
 
     for( unsigned node_index = 0; node_index < num_nodes; node_index++)
-	{
-    	// Reduce the index of all nodes greater than  node "index"
-    	if (node_index >= index)
-    	{
-    		assert(this->mNodes[node_index]->GetIndex() == node_index+1);
-			this->mNodes[node_index]->SetIndex(node_index);
-    	}
-    	assert(this->mNodes[node_index]->GetIndex() == node_index);
+    {
+        // Reduce the index of all nodes greater than  node "index"
+        if (node_index >= index)
+        {
+            assert(this->mNodes[node_index]->GetIndex() == node_index+1);
+            this->mNodes[node_index]->SetIndex(node_index);
+        }
+        assert(this->mNodes[node_index]->GetIndex() == node_index);
 
-		// Reduce the index of all nodes greater than  node "index"
-    	// in the Moore and Von Neuman neighbourhoods.
-    	std::set<unsigned> von_neuman = mVonNeumannNeighbouringNodeIndices[node_index];
-    	mVonNeumannNeighbouringNodeIndices[node_index].clear();
-    	for (std::set<unsigned>::iterator iter = von_neuman.begin();
-    	      iter != von_neuman.end();
-    	      iter++)
-    	{
-    		if (*iter >= index)
-    		{
-    			mVonNeumannNeighbouringNodeIndices[node_index].insert(*iter-1);
-    		}
-    		else
-    		{
-    			mVonNeumannNeighbouringNodeIndices[node_index].insert(*iter);
-    		}
-    	}
-       	std::set<unsigned> moore = mMooreNeighbouringNodeIndices[node_index];
-       	mMooreNeighbouringNodeIndices[node_index].clear();
-		for (std::set<unsigned>::iterator iter = moore.begin();
-			  iter != moore.end();
-			  iter++)
-		{
-			if (*iter >= index)
-			{
-				mMooreNeighbouringNodeIndices[node_index].insert(*iter-1);
-			}
-			else
-			{
-				mMooreNeighbouringNodeIndices[node_index].insert(*iter);
-			}
-		}
-   	}
+        // Reduce the index of all nodes greater than  node "index"
+        // in the Moore and Von Neuman neighbourhoods.
+        std::set<unsigned> von_neuman = mVonNeumannNeighbouringNodeIndices[node_index];
+        mVonNeumannNeighbouringNodeIndices[node_index].clear();
+        for (std::set<unsigned>::iterator iter = von_neuman.begin();
+             iter != von_neuman.end();
+             iter++)
+        {
+            if (*iter >= index)
+            {
+                mVonNeumannNeighbouringNodeIndices[node_index].insert(*iter-1);
+            }
+            else
+            {
+                mVonNeumannNeighbouringNodeIndices[node_index].insert(*iter);
+            }
+        }
+        std::set<unsigned> moore = mMooreNeighbouringNodeIndices[node_index];
+        mMooreNeighbouringNodeIndices[node_index].clear();
+        for (std::set<unsigned>::iterator iter = moore.begin();
+             iter != moore.end();
+             iter++)
+        {
+            if (*iter >= index)
+            {
+                mMooreNeighbouringNodeIndices[node_index].insert(*iter-1);
+            }
+            else
+            {
+                mMooreNeighbouringNodeIndices[node_index].insert(*iter);
+            }
+        }
+    }
     // Finally remove any elememts that have been removed
     assert(mDeletedElementIndices.size() <= 1); // Should have at most one element to remove
-    if(mDeletedElementIndices.size() == 1)
+    if (mDeletedElementIndices.size() == 1)
     {
-    	unsigned elem_index = mDeletedElementIndices[0];
-    	mElements.erase(mElements.begin()+elem_index);
-    	mDeletedElementIndices.clear();
+        unsigned deleted_elem_index = mDeletedElementIndices[0];
+        mElements.erase(mElements.begin()+deleted_elem_index);
+        mDeletedElementIndices.clear();
 
-    	for (unsigned index=elem_index; index<GetNumElements(); index++)
-    	{
-    		mElements[index]->ResetIndex(index);
-    	}
+        for (unsigned elem_index=deleted_elem_index; elem_index<GetNumElements(); elem_index++)
+        {
+            mElements[elem_index]->ResetIndex(elem_index);
+        }
     }
 }
 
 template<unsigned DIM>
 unsigned PottsMesh<DIM>::DivideElement(PottsElement<DIM>* pElement,
-                                  bool placeOriginalElementBelow)
+                                       bool placeOriginalElementBelow)
 {
     /// Not implemented in 1d
     assert(DIM==2 || DIM==3);