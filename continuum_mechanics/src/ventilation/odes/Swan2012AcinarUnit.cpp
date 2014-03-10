beeeaf31437451f32e0c5d90cff8092c05989094
Rafel Bordas <rafb@cs.ox.ac.uk>
2014-03-10 11:20:48 +0000

    #2477 Forgot to commit Swan model with updated tissue properties
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21205 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- continuum_mechanics/src/ventilation/odes/Swan2012AcinarUnit.cpp
+++ continuum_mechanics/src/ventilation/odes/Swan2012AcinarUnit.cpp
@@ -48,7 +48,7 @@ Swan2012AcinarUnit::Swan2012AcinarUnit() : mQ(0.0),
                                            mRaw(0.0),
                                            mA(0.433),
                                            mB(0.611),
-                                           mXi(2500),
+                                           mXi(2.5),
                                            mV0(1.0)
 {
 
