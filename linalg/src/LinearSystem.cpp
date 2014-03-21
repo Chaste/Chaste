Chaste/Chaste

commit 904a6549084ac8d769d97f7738f0e994f9d02af1

Fix minor bug in `LinearSystem`: LHS guesses were acted on if it was the first call to the solver and the KSP was being set up.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21254 b5f69653-2bf5-0310-8201-b0dafb8e1405

linalg/src/LinearSystem.cpp
@@ -792,12 +792,6 @@ Vec LinearSystem::Solve(Vec lhsGuess)
             PETSCEXCEPT( KSPSetNullSpace(mKspSolver, mMatNullSpace) );
         }
 
-        if (lhsGuess)
-        {
-            // Assume that the user of this method will always be kind enough to give us a reasonable guess.
-            KSPSetInitialGuessNonzero(mKspSolver,PETSC_TRUE);
-        }
-
         KSPSetFromOptions(mKspSolver);
 
         /*
@@ -940,6 +934,8 @@ Vec LinearSystem::Solve(Vec lhsGuess)
     if (lhsGuess)
     {
         VecCopy(lhsGuess, lhs_vector);
+        // Assume that the user of this method will always be kind enough to give us a reasonable guess.
+        KSPSetInitialGuessNonzero(mKspSolver, PETSC_TRUE);
     }
 
     // Check if the right hand side is small (but non-zero), PETSc can diverge immediately

linalg/test/TestLinearSystem.hpp
@@ -621,6 +621,17 @@ public:
         ls.SetRhsVectorElement(2, 50.0);
         ls.AssembleFinalLinearSystem();
 
+        ls.Solve();
+        unsigned num_iters_from_default = ls.GetNumIterations();
+        Vec zero_guess=PetscTools::CreateVec(3);
+        PetscVecTools::SetElement(zero_guess, 0, 0.0);
+        PetscVecTools::SetElement(zero_guess, 1, 0.0);
+        PetscVecTools::SetElement(zero_guess, 2, 0.0);
+        PetscVecTools::Finalise(zero_guess);
+        ls.Solve(zero_guess);
+        unsigned num_iters_from_zero = ls.GetNumIterations();
+        TS_ASSERT_EQUALS(num_iters_from_default, num_iters_from_zero);
+
         // Set the correct answer for the intial guess
         Vec good_guess=PetscTools::CreateVec(3);
         PetscVecTools::SetElement(good_guess, 0, 1.0);
@@ -628,8 +639,13 @@ public:
         PetscVecTools::SetElement(good_guess, 2, 3.0);
         PetscVecTools::Finalise(good_guess);
 
+
         Vec solution_vector;
         solution_vector = ls.Solve(good_guess);
+        unsigned num_iters_from_perfect = ls.GetNumIterations();
+        TS_ASSERT_EQUALS(num_iters_from_perfect, 0u);
+        TS_ASSERT_LESS_THAN(num_iters_from_perfect, num_iters_from_zero);
+
         int lo, hi;
         VecGetOwnershipRange(solution_vector,&lo,&hi);
         PetscScalar* p_solution_elements_array;

commit 3c7e1b0faae59b5419b90e6bcff2d94faf78b3be

Attempt to fix Windows parallel broken by r21254
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21284 b5f69653-2bf5-0310-8201-b0dafb8e1405

linalg/src/LinearSystem.cpp
@@ -793,7 +793,11 @@ Vec LinearSystem::Solve(Vec lhsGuess)
         }
 
         KSPSetFromOptions(mKspSolver);
-
+        if (lhsGuess)
+        {
+            // Assume that the user of this method will always be kind enough to give us a reasonable guess.
+            KSPSetInitialGuessNonzero(mKspSolver,PETSC_TRUE);
+        }
         /*
          * Non-adaptive Chebyshev: the required spectrum approximation is computed just once
          * at the beginning of the simulation. This is done with two extra CG solves.
@@ -934,8 +938,9 @@ Vec LinearSystem::Solve(Vec lhsGuess)
     if (lhsGuess)
     {
         VecCopy(lhsGuess, lhs_vector);
-        // Assume that the user of this method will always be kind enough to give us a reasonable guess.
-        KSPSetInitialGuessNonzero(mKspSolver, PETSC_TRUE);
+        // If this wasn't done at construction time then it may be too late for this:
+        // KSPSetInitialGuessNonzero(mKspSolver, PETSC_TRUE);
+        // Is it possible to warn the user?
     }
 
     // Check if the right hand side is small (but non-zero), PETSc can diverge immediately

linalg/test/TestLinearSystem.hpp
@@ -620,10 +620,11 @@ public:
         ls.SetRhsVectorElement(1, 32.0);
         ls.SetRhsVectorElement(2, 50.0);
         ls.AssembleFinalLinearSystem();
+//      /* This will mess up the following because the *first call* to Solve has to contain an initial guess. */
+//        Vec sol1 = ls.Solve();
+//        PetscTools::Destroy(sol1);
+//        unsigned num_iters_from_default = ls.GetNumIterations();
 
-	Vec sol1 = ls.Solve();
-	PetscTools::Destroy(sol1);
-        unsigned num_iters_from_default = ls.GetNumIterations();
         Vec zero_guess=PetscTools::CreateVec(3);
         PetscVecTools::SetElement(zero_guess, 0, 0.0);
         PetscVecTools::SetElement(zero_guess, 1, 0.0);
@@ -632,9 +633,8 @@ public:
         Vec sol2 = ls.Solve(zero_guess);
         PetscTools::Destroy(zero_guess);
         PetscTools::Destroy(sol2);
-      
+
         unsigned num_iters_from_zero = ls.GetNumIterations();
-        TS_ASSERT_EQUALS(num_iters_from_default, num_iters_from_zero);
 
         // Set the correct answer for the intial guess
         Vec good_guess=PetscTools::CreateVec(3);