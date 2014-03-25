a953cec9d6e5660e7897c89084a78d0913c37371
Gary Mirams <gary.mirams@gmail.com>
2014-03-25 16:06:33 +0000

    A bit more explanation of what is what in metadata list
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21316 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/pycml/cellml_metadata.py
+++ python/pycml/cellml_metadata.py
@@ -203,7 +203,9 @@ METADATA_NAMES = frozenset(
      # ========================================================================
      # Calcium subsystem parameters - needs tidying up.
      # ========================================================================
-     'SR_release_current', 'SR_uptake_current', 'SR_leak_current', 
+     'SR_release_current', # a.k.a. Jrel or RyR channel current 
+     'SR_uptake_current',# a.k.a. Jup or SERCA current
+     'SR_leak_current', 
      'SR_leak_current_max', 'SR_release_current_max', 'SR_uptake_current_max', 
      'SR_release_kmcacyt', 'SR_release_kmcads', 
      'calcium_dynamics_release_current_maximum', 'calcium_dynamics_leak_current_maximum', 
