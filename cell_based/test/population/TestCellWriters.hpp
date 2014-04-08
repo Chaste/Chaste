966f913caffe912db34842acedfc6dc327c7f1e5
Alex Fletcher <fletcher@maths.ox.ac.uk>
2014-04-08 17:24:29 +0000

    #2441 - sigh
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21462 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cell_based/test/population/TestCellWriters.hpp
+++ cell_based/test/population/TestCellWriters.hpp
@@ -782,11 +782,11 @@ public:
         CellProliferativeTypesWriter<2,2> cell_writer;
         cell_writer.OpenOutputFile(output_directory);
         cell_writer.WriteTimeStamp();
-		for (AbstractCellPopulation<2>::Iterator cell_iter = cell_population.Begin();
-			 cell_iter != cell_population.End();
-			 ++cell_iter)
+		for (AbstractCellPopulation<2>::Iterator other_cell_iter = cell_population.Begin();
+			 other_cell_iter != cell_population.End();
+			 ++other_cell_iter)
 		{
-		    cell_writer.VisitCell(*cell_iter, &cell_population);
+		    cell_writer.VisitCell(*other_cell_iter, &cell_population);
 		}
         cell_writer.CloseFile();
 
