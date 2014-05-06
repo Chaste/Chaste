95ea52b035cfbb43f121d492f422889cd49f7ced
Joe Pitt-Francis <jmpf@cs.ox.ac.uk>
2014-05-06 08:17:11 +0000

    #2525 Apply Shameer's patch
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21643 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/extended_bidomain/TestFibreReaderAbilityExtendedBidomainProblem.hpp
+++ heart/test/extended_bidomain/TestFibreReaderAbilityExtendedBidomainProblem.hpp
@@ -36,6 +36,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <cxxtest/TestSuite.h>
 #include "ExtendedBidomainProblem.hpp"
 #include "CorriasBuistSMCModified.hpp"
+#include "BidomainProblem.hpp"
 #include "PetscSetupAndFinalize.hpp"
 
 #ifndef TESTFIBREREADERABILITYEXTENDEDBIDOMAINPROBLEM_HPP_
@@ -45,7 +46,7 @@ class UnStimulated3DCellFactory: public AbstractCardiacCellFactory<3>
 {
 
 public:
-	UnStimulated3DCellFactory() : AbstractCardiacCellFactory<3>()
+    UnStimulated3DCellFactory() : AbstractCardiacCellFactory<3>()
     {
     }
 
@@ -95,7 +96,7 @@ public:
         HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(3, 4));
         extended_problem.SetIntracellularConductivitiesForSecondCell(Create_c_vector(5, 6));
 
-        // This mesh has parallel to x axis fibre for x < 0.5 mm and aligned 45 degrees for x > 45. However,
+        // This mesh has parallel to x axis fibre for x < 0.5 mm and aligned 45 degrees for x > 0.5 mm. However,
         // in this case the fibre is not read.
         HeartConfig::Instance()->SetMeshFileName("mesh/test/data/2D_0_to_1mm_800_elements");
 
@@ -104,51 +105,33 @@ public:
         ExtendedBidomainTissue<2>* extended_tissue = extended_problem.GetExtendedBidomainTissue();
 
         c_matrix<double, 2, 2> modified_extracellular;
-        // Get effective conductivity tensor for element < 0.5 mm eg. 35
-        modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(35);
-        double tol = 1e-5;
-        TS_ASSERT_DELTA(modified_extracellular(0,0), 3, tol);
-        TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,1), 4, tol);
-
-        // Get effective conductivity tensor for element > 0.5 mm eg. 700
-        modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(700);
-        TS_ASSERT_DELTA(modified_extracellular(0,0), 3, tol);
-        TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,1), 4, tol);
-
         c_matrix<double, 2, 2> modified_intracellular;
-        // Get effective conductivity tensor first cell  for element < 0.5 mm eg. 35
-        modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(35);
-        TS_ASSERT_DELTA(modified_intracellular(0,0), 1, tol);
-        TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,1), 2, tol);
+        c_matrix<double, 2, 2> modified_intracellular_second_cell;
+        double tol = 1e-5;
 
-		// Get effective conductivity tensor first cell for element > 0.5 mm eg. 700
-		modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(700);
-        TS_ASSERT_DELTA(modified_intracellular(0,0), 1, tol);
-        TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,1), 2, tol);
+        // We should expect unchanged tensors for all elements.
+        for (AbstractTetrahedralMesh<2,2>::ElementIterator it = extended_problem.rGetMesh().GetElementIteratorBegin();
+                it != extended_problem.rGetMesh().GetElementIteratorBegin();
+                ++it)
+        {
+            modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(it->GetIndex());
+            TS_ASSERT_DELTA(modified_extracellular(0,0), 3, tol);
+            TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(1,1), 4, tol);
 
-		c_matrix<double, 2, 2> modified_intracellular_second_cell;
-		// Get effective conductivity tensor second cell for element < 0.5 mm eg. 35
-		modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(35);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 6, tol);
+            modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(it->GetIndex());
+            TS_ASSERT_DELTA(modified_intracellular(0,0), 1, tol);
+            TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(1,1), 2, tol);
 
-		// Get effective conductivity tensor second cell for element > 0.5 mm eg. 700
-		modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(700);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 6, tol);
-
+            modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(it->GetIndex());
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 6, tol);
+        }
     }
 
     void TestOrthoFibreAbilityExtendedProblem() throw (Exception)
@@ -159,128 +142,143 @@ public:
         HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(1, 2));
         HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(3, 4));
 
-        // This mesh has parallel to x axis fibre for x < 0.5 mm and aligned 45 degrees for x > 45.
+        // This mesh has parallel to x axis fibre for x < 0.5 mm and aligned 45 degrees for x > 0.5 mm.
         HeartConfig::Instance()->SetMeshFileName("mesh/test/data/2D_0_to_1mm_800_elements", cp::media_type::Orthotropic);
 
         UnStimulatedCellFactory un_stimulated_cell_factory;
 
         ExtendedBidomainProblem<2> extended_problem( &un_stimulated_cell_factory , &un_stimulated_cell_factory);
+
         extended_problem.SetExtendedBidomainParameters(1, 2, 3, 4, 5, 6);
+
         extended_problem.SetIntracellularConductivitiesForSecondCell(Create_c_vector(5, 6));
+
         extended_problem.Initialise();
 
         ExtendedBidomainTissue<2>* extended_tissue = extended_problem.GetExtendedBidomainTissue();
+
         c_matrix<double, 2, 2> modified_extracellular;
-
-        // Get effective conductivity tensor first cell  for element < 0.5 mm eg. 35
-        modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(35);
-
-        double tol = 1e-5;
-        TS_ASSERT_DELTA(modified_extracellular(0,0), 3, tol);
-        TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,1), 4, tol);
-
-        // Get effective conductivity tensor first cell  for element > 0.5 mm eg. 700
-        modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(700);
-        TS_ASSERT_DELTA(modified_extracellular(0,0), 3.5, tol);
-        TS_ASSERT_DELTA(modified_extracellular(0,1), -0.5, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,0), -0.5, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,1), 3.5, tol);
-
         c_matrix<double, 2, 2> modified_intracellular;
-        // Get effective conductivity tensor first cell  for element < 0.5 mm eg. 35
-        modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(35);
-        TS_ASSERT_DELTA(modified_intracellular(0,0), 1, tol);
-        TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,1), 2, tol);
+        c_matrix<double, 2, 2> modified_intracellular_second_cell;
+        double tol = 1e-5;
 
-        // Get effective conductivity tensor first cell  for element > 0.5 mm eg. 700
-		modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(700);
-        TS_ASSERT_DELTA(modified_intracellular(0,0), 1.5, tol);
-        TS_ASSERT_DELTA(modified_intracellular(0,1), -0.5, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,0), -0.5, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,1), 1.5, tol);
+        // We should expect unchanged tensors for elements with x < 0.5 mm and modified tensors for x > 0.5
+        for (AbstractTetrahedralMesh<2,2>::ElementIterator it = extended_problem.rGetMesh().GetElementIteratorBegin();
+                     it != extended_problem.rGetMesh().GetElementIteratorBegin();
+                     ++it)
+        {
+            // Get effective conductivity tensors for element < 0.5 mm
+            if(it->CalculateCentroid()[0] < 0.05)
+            {
+                modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(it->GetIndex());
+                TS_ASSERT_DELTA(modified_extracellular(0,0), 3, tol);
+                TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
+                TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
+                TS_ASSERT_DELTA(modified_extracellular(1,1), 4, tol);
 
-		c_matrix<double, 2, 2> modified_intracellular_second_cell;
-		// Get effective conductivity tensor second cell for element < 0.5 mm eg. 35
-		modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(35);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 6, tol);
+                modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(it->GetIndex());
+                TS_ASSERT_DELTA(modified_intracellular(0,0), 1, tol);
+                TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
+                TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
+                TS_ASSERT_DELTA(modified_intracellular(1,1), 2, tol);
 
-        // Get effective conductivity tensor second cell for element > 0.5 mm eg. 700
-		modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(700);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5.5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), -0.5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), -0.5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 5.5, tol);
+                modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(it->GetIndex());
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5, tol);
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 6, tol);
+            }
+            else
+            {
+                modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(it->GetIndex());
+                TS_ASSERT_DELTA(modified_extracellular(0,0), 3.5, tol);
+                TS_ASSERT_DELTA(modified_extracellular(0,1), -0.5, tol);
+                TS_ASSERT_DELTA(modified_extracellular(1,0), -0.5, tol);
+                TS_ASSERT_DELTA(modified_extracellular(1,1), 3.5, tol);
+
+                modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(it->GetIndex());
+                TS_ASSERT_DELTA(modified_intracellular(0,0), 1.5, tol);
+                TS_ASSERT_DELTA(modified_intracellular(0,1), -0.5, tol);
+                TS_ASSERT_DELTA(modified_intracellular(1,0), -0.5, tol);
+                TS_ASSERT_DELTA(modified_intracellular(1,1), 1.5, tol);
+
+                modified_intracellular_second_cell =  extended_tissue->rGetIntracellularConductivityTensorSecondCell(it->GetIndex());
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 5.5, tol);
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), -0.5, tol);
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), -0.5, tol);
+                TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 5.5, tol);
+            }
+        }
     }
 
     void Test3DAxiFibreAbilityExtendedProblem() throw (Exception)
     {
         HeartConfig::Instance()->Reset();
         HeartEventHandler::Instance()->Reset();
-
         HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(1, 2, 2));
         HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(3, 4, 4));
 
         // This mesh defines a single tetrahdera with fibre oriented along y axis
-        HeartConfig::Instance()->SetMeshFileName("mesh/test/data/3D_Single_tetrahedron_element",
-                                                 cp::media_type::Axisymmetric);
+        HeartConfig::Instance()->SetMeshFileName("mesh/test/data/3D_Single_tetrahedron_element", cp::media_type::Axisymmetric);
+
+        // We ignore mesh partitioning as this mesh has only one element.
+        HeartConfig::Instance()->SetMeshPartitioning("dumb");
 
         UnStimulated3DCellFactory un_stimulated_3Dcell_factory;
 
-        ExtendedBidomainProblem<3> extended_problem( &un_stimulated_3Dcell_factory,
-                                                     &un_stimulated_3Dcell_factory);
-
+        ExtendedBidomainProblem<3> extended_problem( &un_stimulated_3Dcell_factory , &un_stimulated_3Dcell_factory);
         extended_problem.SetExtendedBidomainParameters(1, 2, 3, 4, 5, 6);
         extended_problem.SetIntracellularConductivitiesForSecondCell(Create_c_vector(5, 6, 6));
         extended_problem.Initialise();
 
         ExtendedBidomainTissue<3>* extended_tissue = extended_problem.GetExtendedBidomainTissue();
 
-        double tol =1e-5;
         c_matrix<double, 3, 3> modified_extracellular;
-        modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(0);
-        TS_ASSERT_DELTA(modified_extracellular(0,0), 4, tol);
-        TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(0,2), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,1), 3, tol);
-        TS_ASSERT_DELTA(modified_extracellular(1,2), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(2,0), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(2,1), 0, tol);
-        TS_ASSERT_DELTA(modified_extracellular(2,2), 4, tol);
-
         c_matrix<double, 3, 3> modified_intracellular;
-        modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(0);
-        TS_ASSERT_DELTA(modified_intracellular(0,0), 2, tol);
-        TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(0,2), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,1), 1, tol);
-        TS_ASSERT_DELTA(modified_intracellular(1,2), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(2,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(2,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular(2,2), 2, tol);
+        c_matrix<double, 3, 3> modified_intracellular_second_cell;
 
- 		c_matrix<double, 3, 3> modified_intracellular_second_cell;
- 		modified_intracellular_second_cell = extended_tissue->rGetIntracellularConductivityTensorSecondCell(0);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 6, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(0,2), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 5, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(1,2), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(2,0), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(2,1), 0, tol);
-        TS_ASSERT_DELTA(modified_intracellular_second_cell(2,2), 6, tol);
-   }
+        double tol = 1e-5;
 
+        // We should expect modified tensors for the mesh.
+        for (AbstractTetrahedralMesh<3,3>::ElementIterator it = extended_problem.rGetMesh().GetElementIteratorBegin();
+                it != extended_problem.rGetMesh().GetElementIteratorBegin();
+                ++it)
+        {
+            modified_extracellular =  extended_tissue->rGetExtracellularConductivityTensor(it->GetIndex());
+            TS_ASSERT_DELTA(modified_extracellular(0,0), 4, tol);
+            TS_ASSERT_DELTA(modified_extracellular(0,1), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(0,2), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(1,0), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(1,1), 3, tol);
+            TS_ASSERT_DELTA(modified_extracellular(1,2), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(2,0), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(2,1), 0, tol);
+            TS_ASSERT_DELTA(modified_extracellular(2,2), 4, tol);
 
+            modified_intracellular =  extended_tissue->rGetIntracellularConductivityTensor(it->GetIndex());
+            TS_ASSERT_DELTA(modified_intracellular(0,0), 2, tol);
+            TS_ASSERT_DELTA(modified_intracellular(0,1), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(0,2), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(1,0), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(1,1), 1, tol);
+            TS_ASSERT_DELTA(modified_intracellular(1,2), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(2,0), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(2,1), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular(2,2), 2, tol);
+
+            modified_intracellular_second_cell = extended_tissue->rGetIntracellularConductivityTensorSecondCell(it->GetIndex());
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(0,0), 6, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(0,1), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(0,2), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(1,0), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(1,1), 5, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(1,2), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(2,0), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(2,1), 0, tol);
+            TS_ASSERT_DELTA(modified_intracellular_second_cell(2,2), 6, tol);
+        }
+
+    }
 };
 
 #endif /* TESTFIBREREADERABILITYEXTENDEDBIDOMAINPROBLEM_HPP_ */
