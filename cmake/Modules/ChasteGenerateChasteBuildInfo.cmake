67741c32f25e782d36124cc72ae79b3335e05874
Martin Robinson <martin.robinson@maths.ox.ac.uk>
2015-04-15 14:09:47 +0000

    additional variable for ChasteBuildInfo.cpp not set in Windows, fixed
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@24037 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cmake/Modules/ChasteGenerateChasteBuildInfo.cmake
+++ cmake/Modules/ChasteGenerateChasteBuildInfo.cmake
@@ -1,3 +1,11 @@
+if(MSVC)
+    set(additional
+    "
+    //This has been put here to satisfy an MSVC linker issue 
+    int __cdecl _purecall(void){return 0;}
+    ")
+endif()
+
 configure_file (
   "${Chaste_SOURCE_DIR}/global/src/ChasteBuildInfo_cmake.cpp.in"
   ChasteBuildInfo.cpp
