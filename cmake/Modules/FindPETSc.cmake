b0edf7188e32e1d141075d61b9935a288babc051
Martin Robinson <martin.robinson@maths.ox.ac.uk>
2015-08-21 11:49:26 +0000

    #2656: fix PETSc 3.6 dir layout in FindPETSc.cmake (https://github.com/jedbrown/cmake-modules/commit/6d822839eea1dde4f1c51bf6b2b75b30070671e9)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@25084 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cmake/Modules/FindPETSc.cmake
+++ cmake/Modules/FindPETSc.cmake
@@ -133,9 +133,9 @@ endif()
 
 # Determine whether the PETSc layout is old-style (through 2.3.3) or
 # new-style (>= 3.0.0)
-if (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/lib/petsc-conf/petscvariables") # > 3.5
-  set (petsc_conf_rules "${PETSC_DIR}/lib/petsc-conf/rules")
-  set (petsc_conf_variables "${PETSC_DIR}/lib/petsc-conf/variables")
+if (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/lib/petsc/conf/petscvariables") # > 3.5
+  set (petsc_conf_rules "${PETSC_DIR}/lib/petsc/conf/rules")
+  set (petsc_conf_variables "${PETSC_DIR}/lib/petsc/conf/variables")
 elseif (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/include/petscconf.h")   # > 2.3.3
   set (petsc_conf_rules "${PETSC_DIR}/conf/rules")
   set (petsc_conf_variables "${PETSC_DIR}/conf/variables")
