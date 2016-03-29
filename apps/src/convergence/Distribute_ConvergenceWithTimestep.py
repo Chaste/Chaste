439d6996f4bea6c77cf6511bde55de8f00be7808
fcooper <fcooper@b5f69653-2bf5-0310-8201-b0dafb8e1405>
2016-03-29 18:49:29 +0000

    [ImmersedBoundary] added executables for convergence analysis
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/projects/ImmersedBoundary@26353 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ apps/src/convergence/Distribute_ConvergenceWithTimestep.py
@@ -0,0 +1,157 @@
+import multiprocessing
+import os
+import subprocess
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import immersed_boundary as ib
+
+# Globally accessible directory paths and names
+path_to_exec   = os.environ.get('CHASTE_BUILD_DIR') + '/projects/ImmersedBoundary/apps/Exe_ConvergenceWithTimestep'
+path_to_output = os.environ.get('CHASTE_TEST_OUTPUT') + '/convergence/time_step/'
+
+# Range for simulation parameters
+num_sims = 12
+
+def main():
+    # run_simulations()
+    # combine_output()
+    plot_results()
+
+
+# Create a list of commands and pass them to separate processes
+def run_simulations():
+
+    # Make a list of calls to a Chaste executable
+    command_list = []
+    for sim_id in range(num_sims):
+
+        time_step = 2**(-1-sim_id)
+
+        command = 'nice -n 19 ' + path_to_exec \
+                  + ' --ID ' + str(sim_id) \
+                  + ' --DT ' + str(time_step)
+        command_list.append(command)
+
+    # Use processes equal to the number of cpus available
+    count = multiprocessing.cpu_count()
+
+    print("Py: Starting simulations with " + str(count) + " processes")
+
+    # Generate a pool of workers
+    pool = multiprocessing.Pool(processes=count)
+
+    # Pass the list of bash commands to the pool
+
+    pool.map(execute_command, command_list)
+
+
+# This is a helper function for run_simulation that runs bash commands in separate processes
+def execute_command(cmd):
+    return subprocess.call(cmd, shell=True)
+
+
+# Gather the output from all simulations and put it in the same file
+def combine_output():
+
+    print("Py: Combining output")
+
+    combined_results = open(path_to_output + '/combined_results.dat', 'w')
+    combined_results.write("simulation_id,time_step,esf_at_end\n")
+
+    for sim_id in range(num_sims):
+        file_name = path_to_output + '/sim/' + str(sim_id) + '/results.dat'
+
+        if os.path.isfile(file_name):
+            local_results = open(file_name, 'r')
+            combined_results.write(local_results.readline())
+            local_results.close()
+            combined_results.write("\n")
+
+    combined_results.close()
+
+
+def plot_results():
+
+    print("Py: Plotting results")
+
+    ################################################################
+    # Global options to ensure consistency between different plots #
+    ################################################################
+
+    bg_gray = '0.75'
+    bg_line_width = 0.5
+    font_size = 8
+
+    mycol_gr = '#006532' #Green
+    mycol_or = '#F39200' #Orange
+    mycol_bl = '#2C2E83' #Blue
+    mycol_lb = '#0072bd' #Light blue
+
+    # Useful to convert picas to inches or cm
+    # SIAM max fig width = 31pi = 13.12cm = 5.17in
+    pica_to_cm = 127.0/300.0
+    pica_to_in = 1.0/6.0
+    golden_rat = 1.618
+
+    fig_width = 14 * pica_to_in # num picas, but need it in inches
+
+    # Set LaTeX font rendering
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif', serif='computer modern roman')
+    plt.rc('figure', figsize=[fig_width, fig_width / golden_rat])
+    plt.rc('axes', linewidth=bg_line_width, edgecolor=bg_gray, axisbelow=True)
+    plt.rc('xtick', labelsize=font_size)
+    plt.rc('ytick', labelsize=font_size)
+    plt.rc('xtick.major', size=0, pad=4)
+    plt.rc('ytick.major', size=0, pad=4)
+
+
+    # Data for esf vs time plot
+    esf_data = np.genfromtxt(path_to_output + '/combined_results.dat',
+                             delimiter=',',
+                             skip_header=1)
+
+    time_step = -1.0 * np.log2(esf_data[:, 1])
+    esf_at_end = esf_data[:, 2]
+
+    # Calculate limits to give a comfy margin
+    x_min, x_max = min(time_step), max(time_step)
+    y_min, y_max = min(esf_at_end), max(esf_at_end)
+
+    x_range, y_range = x_max - x_min, y_max - y_min
+    margin = 0.05
+
+    x_lims = [x_min - margin * x_range, x_max + margin * x_range]
+    y_lims = [y_min - margin * y_range, y_max + margin * y_range]
+
+    # Plot the two continuous lines
+    plt.plot(time_step, esf_at_end, marker='o',
+             linestyle='-',
+             linewidth=0.75,
+             color=mycol_or,
+             markerfacecolor='#FFFFFF',
+             markeredgecolor=mycol_or,
+             markersize=4.0,
+             markeredgewidth=0.75)
+
+    # plt.plot(time_step, esf_at_end, color=mycol_or, linewidth=0.5)
+
+    # Set the axis limits
+    plt.xlim(x_lims)
+    plt.ylim(y_lims)
+
+    # Label the axes
+    plt.xlabel(r'$-\log_2(\Delta t)$', fontsize=font_size, labelpad=1.0*font_size)
+    plt.ylabel(r'ESF', fontsize=font_size, labelpad=1.0*font_size)
+
+    # Customise axes
+    ax = plt.gca()
+    ax.grid(b=True, which='major', color=bg_gray, linestyle='dotted', dash_capstyle='round')
+
+    # Export figure
+    plt.savefig(path_to_output + 'ConvergenceWithTimeStep.pdf', bbox_inches='tight', pad_inches=0.0)
+
+if __name__ == "__main__":
+    main()
---
+++ apps/src/convergence/Exe_ConvergenceWithTimestep.cpp
@@ -0,0 +1,191 @@
+/*
+
+Copyright (c) 2005-2015, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+#include <cxxtest/TestSuite.h>
+#include "CheckpointArchiveTypes.hpp"
+
+#include "SimulationTime.hpp"
+#include "RandomNumberGenerator.hpp"
+#include "ExecutableSupport.hpp"
+
+#include "Debug.hpp"
+
+/*
+ * These headers handle passing parameters to the executable.
+ */
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+
+#include "OffLatticeSimulation.hpp"
+#include "StochasticDurationCellCycleModel.hpp"
+#include "CellsGenerator.hpp"
+#include "ImmersedBoundaryMesh.hpp"
+#include "ImmersedBoundaryCellPopulation.hpp"
+#include "ImmersedBoundarySimulationModifier.hpp"
+#include "ImmersedBoundaryPalisadeMeshGenerator.hpp"
+#include "ImmersedBoundaryMembraneElasticityForce.hpp"
+#include "ImmersedBoundaryCellCellInteractionForce.hpp"
+
+/*
+ * Prototype functions
+ */
+void SetupSingletons();
+void DestroySingletons();
+void SetupAndRunSimulation(unsigned simulationId, double timeStep);
+void OutputOnCompletion(unsigned simulationId, double timeStep);
+
+int main(int argc, char *argv[])
+{
+    // This sets up PETSc and prints out copyright information, etc.
+    ExecutableSupport::StandardStartup(&argc, &argv);
+
+    // Define command line options
+    boost::program_options::options_description general_options("This is a Chaste Immersed Boundary executable.\n");
+    general_options.add_options()
+                    ("help", "produce help message")
+                    ("ID", boost::program_options::value<unsigned>()->default_value(0),"Index of the simulation")
+                    ("DT", boost::program_options::value<double>()->default_value(0.1),"Time step for the simulation");
+
+    // Parse command line into variables_map
+    boost::program_options::variables_map variables_map;
+    boost::program_options::store(parse_command_line(argc, argv, general_options), variables_map);
+
+    // Print help message if wanted
+    if (variables_map.count("help"))
+    {
+        std::cout << setprecision(3) << general_options << "\n";
+        std::cout << general_options << "\n";
+        return 1;
+    }
+
+    // get id and name from command line
+    unsigned simulation_id = variables_map["ID"].as<unsigned>();
+    double time_step = variables_map["DT"].as<double>();
+
+    SetupSingletons();
+    SetupAndRunSimulation(simulation_id, time_step);
+    DestroySingletons();
+    OutputOnCompletion(simulation_id, time_step);
+}
+
+void SetupSingletons()
+{
+    // Set up what the test suite would do
+    SimulationTime::Instance()->SetStartTime(0.0);
+
+    // Reseed with 0 for same random numbers each time, or time(NULL) or simulation_id to change each realisation
+    RandomNumberGenerator::Instance()->Reseed(0);
+    CellPropertyRegistry::Instance()->Clear();
+    CellId::ResetMaxCellId();
+}
+
+void DestroySingletons()
+{
+    // this is from the tearDown method of the test suite
+    SimulationTime::Destroy();
+    RandomNumberGenerator::Destroy();
+    CellPropertyRegistry::Instance()->Clear();
+}
+
+void OutputOnCompletion(unsigned simulationId, double timeStep)
+{
+    // Compose the message
+    std::stringstream message;
+    message << "Completed simulation with ID " << simulationId << " and time step " << timeStep << std::endl;
+
+    // Send it to the console
+    std::cout << message.str() << std::flush;
+}
+void SetupAndRunSimulation(unsigned simulationId, double timeStep)
+{
+    /*
+     * 1: Num cells
+     * 2: Num nodes per cell
+     * 3: Superellipse exponent
+     * 4: Superellipse aspect ratio
+     * 5: Random y-variation
+     * 6: Include membrane
+     */
+    ImmersedBoundaryPalisadeMeshGenerator gen(1, 128, 1.0, 2.0, 0.0, false);
+    ImmersedBoundaryMesh<2, 2>* p_mesh = gen.GetMesh();
+
+    p_mesh->SetNumGridPtsXAndY(128);
+
+    std::vector<CellPtr> cells;
+    MAKE_PTR(DifferentiatedCellProliferativeType, p_diff_type);
+    CellsGenerator<StochasticDurationCellCycleModel, 2> cells_generator;
+    cells_generator.GenerateBasicRandom(cells, p_mesh->GetNumElements(), p_diff_type);
+
+    ImmersedBoundaryCellPopulation<2> cell_population(*p_mesh, cells);
+    cell_population.SetIfPopulationHasActiveSources(false);
+
+    OffLatticeSimulation<2> simulator(cell_population);
+
+    // Add main immersed boundary simulation modifier
+    MAKE_PTR(ImmersedBoundarySimulationModifier<2>, p_main_modifier);
+    simulator.AddSimulationModifier(p_main_modifier);
+
+    // Add force laws
+    MAKE_PTR_ARGS(ImmersedBoundaryMembraneElasticityForce<2>, p_boundary_force, (cell_population));
+    p_main_modifier->AddImmersedBoundaryForce(p_boundary_force);
+    p_boundary_force->SetSpringConstant(1e7);
+
+    // Create and set an output directory that is different for each simulation
+    std::stringstream output_directory;
+    output_directory << "convergence/time_step/sim/" << simulationId;
+    simulator.SetOutputDirectory(output_directory.str());
+
+    double end_time = 10.0;
+    unsigned num_time_steps = (unsigned) (end_time / timeStep);
+
+    // Set simulation properties and solve
+    simulator.SetDt(timeStep);
+    simulator.SetSamplingTimestepMultiple(num_time_steps);
+    simulator.SetEndTime(end_time);
+    simulator.Solve();
+
+    // Prepare results file to record simulation statistics
+    OutputFileHandler results_handler(output_directory.str(), false);
+    out_stream results_file = results_handler.OpenOutputFile("results.dat");
+
+    double esf_at_end = p_mesh->GetElongationShapeFactorOfElement(0);
+
+    // Output summary statistics to results file
+    (*results_file) << simulationId << "," << timeStep << "," << esf_at_end;
+
+    // Tidy up
+    results_file->close();
+}
