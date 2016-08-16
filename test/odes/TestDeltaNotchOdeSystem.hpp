Chaste/Chaste

commit f7aec5e7005acf45f7765c82d34e745f5e52f6fb

Adding 'DeltaNotchOdeSystem' to the notforrelease_cell_based trunk.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk/notforrelease_cell_based@13532 b5f69653-2bf5-0310-8201-b0dafb8e1405

src/odes/DeltaNotchOdeSystem.cpp
@@ -1,0 +1,71 @@
+#include "DeltaNotchOdeSystem.hpp"
+#include "CellwiseOdeSystemInformation.hpp"
+#include "Debug.hpp"
+
+DeltaNotchOdeSystem::DeltaNotchOdeSystem(double meanDelta, std::vector<double> stateVariables)
+    : AbstractOdeSystem(3)
+{
+    mpSystemInfo.reset(new CellwiseOdeSystemInformation<DeltaNotchOdeSystem>);
+
+    /**
+     * The state variables are as follows:
+     * 
+     * 0 - Notch concentration for this cell
+     * 1 - Delta concentration for this cell
+     * 2 - average Delta concentration for this cell's immediate neighbours
+     * 
+     * We store the last state variable so that it can be written
+     * to file at each time step alongside the others, and visualized.
+     */
+
+    SetDefaultInitialCondition(0, 1.0); // soon overwritten
+    SetDefaultInitialCondition(1, 1.0); // soon overwritten
+    SetDefaultInitialCondition(2, 0.5);
+
+    if (stateVariables != std::vector<double>())
+    {
+        SetStateVariables(stateVariables);
+    }
+}
+
+DeltaNotchOdeSystem::~DeltaNotchOdeSystem()
+{
+}
+
+void DeltaNotchOdeSystem::EvaluateYDerivatives(double time, const std::vector<double>& rY, std::vector<double>& rDY)
+{
+    double notch = rY[0];
+    double delta = rY[1];
+    double mean_delta = rY[2];
+
+
+    // The next two lines define the ODE system by Collier et al. (1996)
+    double dx1 = mean_delta*mean_delta/(0.01 + mean_delta*mean_delta) - notch;
+    double dx2 = 1/(1 + 100*notch*notch) - delta;
+
+    rDY[0] = dx1;
+    rDY[1] = dx2;
+    rDY[2] = 0.0; // don't change the mean Delta level over the course of the mechanics time step
+}
+
+template<>
+void CellwiseOdeSystemInformation<DeltaNotchOdeSystem>::Initialise()
+{
+    this->mVariableNames.push_back("Notch");
+    this->mVariableUnits.push_back("non-dim");
+    this->mInitialConditions.push_back(0.0); // will be filled in later
+
+    this->mVariableNames.push_back("Delta");
+    this->mVariableUnits.push_back("non-dim");
+    this->mInitialConditions.push_back(0.0); // will be filled in later
+
+    this->mVariableNames.push_back("Mean Delta");
+    this->mVariableUnits.push_back("non-dim");
+    this->mInitialConditions.push_back(0.0); // will be filled in later
+
+    this->mInitialised = true;
+}
+
+// Serialization for Boost >= 1.36
+#include "SerializationExportWrapperForCpp.hpp"
+CHASTE_CLASS_EXPORT(DeltaNotchOdeSystem)

src/odes/DeltaNotchOdeSystem.hpp
@@ -1,0 +1,101 @@
+#ifndef DELTANOTCHODESYSTEM_HPP_
+#define DELTANOTCHODESYSTEM_HPP_
+
+#include "ChasteSerialization.hpp"
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+
+#include <cmath>
+#include <iostream>
+
+#include "AbstractOdeSystem.hpp"
+
+/**
+ * Represents the Delta-Notch ODE system described by Collier et al,
+ * "Pattern formation by lateral inhibition with feedback: a mathematical
+ * model of delta-notch intercellular signalling" (Journal of Theoretical
+ * Biology 183:429-446, 1996).
+ */
+class DeltaNotchOdeSystem : public AbstractOdeSystem
+{
+private:
+
+    friend class boost::serialization::access;
+    /**
+     * Serialize the object and its member variables.
+     * 
+     * @param archive the archive
+     * @param version the current version of this class
+     */
+    template<class Archive>
+    void serialize(Archive & archive, const unsigned int version)
+    {
+        archive & boost::serialization::base_object<AbstractOdeSystem>(*this);
+    }
+
+    ///\todo extract model parameters as member variables
+
+public:
+
+    /**
+     * Default constructor.
+     * 
+     * @param meanDelta the average of the levels of Delta in the surrounding cells (defaults to zero)
+     * @param stateVariables optional initial conditions for state variables (only used in archiving)
+     */
+    DeltaNotchOdeSystem(double meanDelta=0.0, std::vector<double> stateVariables=std::vector<double>());
+
+    /**
+     * Destructor.
+     */
+    ~DeltaNotchOdeSystem();
+    
+    /**
+     * Compute the RHS of the Alarcon et al. (2004) system of ODEs.
+     *
+     * Returns a vector representing the RHS of the ODEs at each time step, y' = [y1' ... yn'].
+     * An ODE solver will call this function repeatedly to solve for y = [y1 ... yn].
+     *
+     * @param time used to evaluate the RHS.
+     * @param rY value of the solution vector used to evaluate the RHS.
+     * @param rDY filled in with the resulting derivatives (using Alarcons et al. (2004) system of equations).
+     */
+    void EvaluateYDerivatives(double time, const std::vector<double>& rY, std::vector<double>& rDY);
+};
+
+// Declare identifier for the serializer
+#include "SerializationExportWrapper.hpp"
+CHASTE_CLASS_EXPORT(DeltaNotchOdeSystem)
+
+namespace boost
+{
+namespace serialization
+{
+/**
+ * Serialize information required to construct a DeltaNotchOdeSystem.
+ */
+template<class Archive>
+inline void save_construct_data(
+    Archive & ar, const DeltaNotchOdeSystem * t, const BOOST_PFTO unsigned int file_version)
+{
+    const std::vector<double> state_variables = t->rGetConstStateVariables();
+    ar & state_variables;
+}
+
+/**
+ * De-serialize constructor parameters and initialise a DeltaNotchOdeSystem.
+ */
+template<class Archive>
+inline void load_construct_data(
+    Archive & ar, DeltaNotchOdeSystem * t, const unsigned int file_version)
+{
+    std::vector<double> state_variables;
+    ar & state_variables;
+
+    // Invoke inplace constructor to initialise instance
+    ::new(t)DeltaNotchOdeSystem(0.0, state_variables);
+}
+}
+} // namespace ...
+
+#endif /*DELTANOTCHODESYSTEM_HPP_*/

test/odes/TestDeltaNotchOdeSystem.hpp
@@ -1,0 +1,122 @@
+#ifndef TESTDELTANOTCHODESYSTEM_HPP_
+#define TESTDELTANOTCHODESYSTEM_HPP_
+
+#include <cxxtest/TestSuite.h>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <ctime>
+#include <vector>
+#include <iostream>
+
+#include "OutputFileHandler.hpp"
+#include "DeltaNotchOdeSystem.hpp"
+#include "RungeKutta4IvpOdeSolver.hpp"
+#include "RungeKuttaFehlbergIvpOdeSolver.hpp"
+#include "BackwardEulerIvpOdeSolver.hpp"
+#include "CvodeAdaptor.hpp"
+
+class TestDeltaNotchOdeSystem : public CxxTest::TestSuite
+{
+public:
+
+    void TestDeltaNotchOdeSystemSetup() throw(Exception)
+    {
+    	EXIT_IF_PARALLEL;
+#ifdef CHASTE_CVODE
+        double mean_delta = 0.5;
+        DeltaNotchOdeSystem ode_system(mean_delta);
+
+        double h_value = 0.0001;
+        CvodeAdaptor cvode_solver;
+        OdeSolution solutions;
+
+        std::vector<double> initial_conditions = ode_system.GetInitialConditions();
+
+        double start_time = std::clock();
+        solutions = cvode_solver.Solve(&ode_system, initial_conditions, 0.0, 100.0, h_value, h_value);
+        double end_time = std::clock();
+        double elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
+        std::cout <<  "1. Cvode Elapsed time = " << elapsed_time << " secs for 100 hours\n";
+
+        // Test solutions are OK for a small time increase...
+        int end = solutions.rGetSolutions().size() - 1;
+        // Tests the simulation is ending at the right time...(going into S phase at 7.8 hours)
+        TS_ASSERT_DELTA(solutions.rGetTimes()[end], 100, 1e-2);
+
+        // Decent results
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0], 0.9615, 1e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1], 0.0107, 1e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2], mean_delta, 1e-4);
+#endif //CHASTE_CVODE
+    }
+
+    void TestArchiving()
+    {
+    	EXIT_IF_PARALLEL;
+#ifdef CHASTE_CVODE
+        OutputFileHandler handler("archive", false);
+        std::string archive_filename = handler.GetOutputDirectoryFullPath() + "delta_notch_ode.arch";
+
+        {
+            DeltaNotchOdeSystem ode_system;
+
+            std::vector<double> initial_conditions = ode_system.GetInitialConditions();
+            TS_ASSERT_EQUALS(initial_conditions.size(), 3u);
+            TS_ASSERT_DELTA(initial_conditions[0], 1.0, 1e-6);
+            TS_ASSERT_DELTA(initial_conditions[1], 1.0, 1e-6);
+            TS_ASSERT_DELTA(initial_conditions[2], 0.5, 1e-6);
+
+            // Create an output archive
+            std::ofstream ofs(archive_filename.c_str());
+            boost::archive::text_oarchive output_arch(ofs);
+
+            // Archive ODE system
+            AbstractOdeSystem* const p_const_ode_system = &ode_system;
+            output_arch << p_const_ode_system;
+        }
+
+        {
+            AbstractOdeSystem* p_ode_system;
+
+            // Create an input archive
+            std::ifstream ifs(archive_filename.c_str(), std::ios::binary);
+            boost::archive::text_iarchive input_arch(ifs);
+
+            // Restore from the archive
+            input_arch >> p_ode_system;
+
+            // Check that archiving worked correctly
+            std::vector<double> initial_conditions = p_ode_system->GetInitialConditions();
+            TS_ASSERT_EQUALS(initial_conditions.size(), 3u);
+            TS_ASSERT_DELTA(initial_conditions[0], 1.0, 1e-6);
+            TS_ASSERT_DELTA(initial_conditions[1], 1.0, 1e-6);
+            TS_ASSERT_DELTA(initial_conditions[2], 0.5, 1e-6);
+
+            // Tidy up
+            delete p_ode_system;
+        }
+#endif //CHASTE_CVODE
+    }
+
+    void TestSetStateVariables()
+    {
+    	EXIT_IF_PARALLEL;
+#ifdef CHASTE_CVODE
+
+    	std::vector<double> state_vars;
+    	state_vars.push_back(0.0);
+    	state_vars.push_back(1.0);
+    	state_vars.push_back(2.0);
+    	DeltaNotchOdeSystem ode_system(0.5,state_vars);
+
+    	TS_ASSERT_EQUALS(ode_system.GetStateVariable(0),0.0);
+    	TS_ASSERT_EQUALS(ode_system.GetStateVariable(1),1.0);
+    	TS_ASSERT_EQUALS(ode_system.GetStateVariable(2),2.0);
+
+#endif //CHASTE_CVODE
+   }
+};
+
+#endif /*TESTDELTANOTCHODESYSTEM_HPP_*/

commit 7afa0357a935f546609a6f8c3cfd2300e5b37d72

Fix build (and copyrights) broken by r13532
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk/notforrelease_cell_based@13540 b5f69653-2bf5-0310-8201-b0dafb8e1405

src/odes/DeltaNotchOdeSystem.cpp
@@ -1,3 +1,31 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include "DeltaNotchOdeSystem.hpp"
 #include "CellwiseOdeSystemInformation.hpp"
 #include "Debug.hpp"

src/odes/DeltaNotchOdeSystem.hpp
@@ -1,3 +1,31 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef DELTANOTCHODESYSTEM_HPP_
 #define DELTANOTCHODESYSTEM_HPP_
 

test/odes/TestDeltaNotchOdeSystem.hpp
@@ -1,3 +1,31 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef TESTDELTANOTCHODESYSTEM_HPP_
 #define TESTDELTANOTCHODESYSTEM_HPP_
 
@@ -23,7 +51,7 @@ public:
 
     void TestDeltaNotchOdeSystemSetup() throw(Exception)
     {
-    	EXIT_IF_PARALLEL;
+//    	EXIT_IF_PARALLEL;
 #ifdef CHASTE_CVODE
         double mean_delta = 0.5;
         DeltaNotchOdeSystem ode_system(mean_delta);
@@ -54,7 +82,7 @@ public:
 
     void TestArchiving()
     {
-    	EXIT_IF_PARALLEL;
+//    	EXIT_IF_PARALLEL;
 #ifdef CHASTE_CVODE
         OutputFileHandler handler("archive", false);
         std::string archive_filename = handler.GetOutputDirectoryFullPath() + "delta_notch_ode.arch";
@@ -102,7 +130,7 @@ public:
 
     void TestSetStateVariables()
     {
-    	EXIT_IF_PARALLEL;
+ //   	EXIT_IF_PARALLEL;
 #ifdef CHASTE_CVODE
 
     	std::vector<double> state_vars;