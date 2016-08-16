Chaste/Chaste

commit cdf8622cc0048c37040c6cce2b98090192b69086

#1112 added some cell generators for vertex crypts and tidied up `TestVertexCryptSimulation2D`.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk/notforrelease_cell_based@8009 b5f69653-2bf5-0310-8201-b0dafb8e1405

src/crypt/Cell/SimpleWntCellCycleModelCellsGeneratorForVertex.cpp
@@ -1,0 +1,175 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include "SimpleWntCellCycleModelCellsGeneratorForVertex.hpp"
+
+
+template<unsigned DIM>
+AbstractCellCycleModel* SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::CreateCellCycleModel()
+{
+    return new SimpleWntCellCycleModel(DIM);
+}
+
+
+template<unsigned DIM>
+double SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GetTypicalTransitCellCycleTime()
+{
+    return TissueConfig::Instance()->GetTransitCellG1Duration()
+            + TissueConfig::Instance()->GetSG2MDuration();
+}
+
+
+template<unsigned DIM>
+double SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GetTypicalStemCellCycleTime()
+{
+    return TissueConfig::Instance()->GetStemCellG1Duration()
+            + TissueConfig::Instance()->GetSG2MDuration();
+}
+
+// \TODO merge this with the GenreateForCrypt method in AbsractCellsGenerator
+template<unsigned DIM>
+void SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
+                                 VertexMesh<2,2>& rMesh,
+                                 const std::vector<unsigned> locationIndices,
+                                 bool randomBirthTimes,
+                                 double y0,
+                                 double y1,
+                                 double y2,
+                                 double y3,
+                                 bool initialiseCells)
+{
+    #define COVERAGE_IGNORE
+    assert(DIM==2);
+    #undef COVERAGE_IGNORE
+
+    RandomNumberGenerator* p_random_num_gen = RandomNumberGenerator::Instance();
+
+    unsigned num_cells = locationIndices.empty() ? rMesh.GetNumElements() : locationIndices.size();
+    
+
+    AbstractCellCycleModel* p_cell_cycle_model = NULL;
+    double typical_transit_cycle_time;
+    double typical_stem_cycle_time;
+
+    rCells.clear();
+    rCells.reserve(num_cells);
+
+    for (unsigned i=0; i<rMesh.GetNumElements(); i++)
+    {
+        CellProliferativeType cell_type;
+        unsigned generation;
+
+        double y = 0.0;
+        if (!locationIndices.empty())
+        {
+            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
+            {
+                y = rMesh.GetCentroidOfElement(i)[1];
+            }
+        }
+        else
+        {
+            y = rMesh.GetCentroidOfElement(i)[1];
+        }
+
+        p_cell_cycle_model = CreateCellCycleModel();
+        typical_transit_cycle_time = this->GetTypicalTransitCellCycleTime();
+        typical_stem_cycle_time = GetTypicalStemCellCycleTime();
+
+        double birth_time = 0.0;
+        if (randomBirthTimes)
+        {
+            birth_time = -p_random_num_gen->ranf();
+        }
+
+        if (y <= y0)
+        {
+            cell_type = STEM;
+            generation = 0;
+            birth_time *= typical_stem_cycle_time; // hours
+        }
+        else if (y < y1)
+        {
+            cell_type = TRANSIT;
+            generation = 1;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+        else if (y < y2)
+        {
+            cell_type = TRANSIT;
+            generation = 2;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+        else if (y < y3)
+        {
+            cell_type = TRANSIT;
+            generation = 3;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+        else
+        {
+            cell_type = AbstractCellsGenerator<DIM>::CellsCanDifferentiate() ? DIFFERENTIATED : TRANSIT;
+            generation = 4;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+
+        if (dynamic_cast<AbstractSimpleGenerationBasedCellCycleModel*>(p_cell_cycle_model))
+        {
+            static_cast<AbstractSimpleGenerationBasedCellCycleModel*>(p_cell_cycle_model)->SetGeneration(generation);
+        }
+
+        TissueCell cell(cell_type, HEALTHY, p_cell_cycle_model);
+        if (initialiseCells)
+        {
+            cell.InitialiseCellCycleModel();
+        }
+
+        cell.SetBirthTime(birth_time);
+
+        if (!locationIndices.empty())
+        {
+            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
+            {
+                rCells.push_back(cell);
+            }
+        }
+        else
+        {
+            rCells.push_back(cell);
+        }
+    }
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////////////
+
+//template class SimpleWntCellCycleModelCellsGenerator<1>;
+template class SimpleWntCellCycleModelCellsGeneratorForVertex<2>;
+//template class SimpleWntCellCycleModelCellsGenerator<3>;

src/crypt/Cell/SimpleWntCellCycleModelCellsGeneratorForVertex.hpp
@@ -1,0 +1,92 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef SIMPLEWNTCELLCYCLEMODELCELLSGENERATORFORVERTEX_HPP_
+#define SIMPLEWNTCELLCYCLEMODELCELLSGENERATORFORVERTEX_HPP_
+
+#include "AbstractCellsGenerator.hpp"
+#include "SimpleWntCellCycleModel.hpp"
+#include "VertexMesh.hpp"
+
+/**
+ * A helper class for generating a vector of cells with
+ * SimpleWntCellCycleModels for a given vertex mesh.
+ * 
+ * \todo this should be added to the AbstractCellsGenerator class when 
+ * vertex models are added to the main code.
+ */
+template<unsigned DIM>
+class SimpleWntCellCycleModelCellsGeneratorForVertex : public AbstractCellsGenerator<DIM>
+{
+public:
+
+    /**
+     * @return a pointer to a new SimpleWntCellCycleModel.
+     */
+    AbstractCellCycleModel* CreateCellCycleModel();
+
+    /**
+     * @return default cell cycle time for a transit cell.
+     */
+    double GetTypicalTransitCellCycleTime();
+
+    /**
+     * @return default cell cycle time for a transit cell.
+     */
+    double GetTypicalStemCellCycleTime();
+
+    /**
+     * Generates cells of a specified cell cycle type under the correct
+     * crypt conditions and gives random ages if required,
+     * or gives them an age of 0.0 - creates least work for solver startup.
+     *
+     * @param rCells  An empty cells vector for this function to fill up
+     * @param rMesh  The crypt mesh (A VERTEX MESH)
+     * @param locationIndices the node indices corresponding to real cells
+     * @param randomBirthTimes  Whether to assign the cells random birth times
+     *    (this can be expensive computationally with ODE models)
+     * @param y0  below this line cells are generation 0 (defaults to 0.3)
+     * @param y1  below this line cells are generation 1 (defaults to 2.0)
+     * @param y2  below this line cells are generation 2 (defaults to 3.0)
+     * @param y3  below this line cells are generation 3 (defaults to 4.0)
+     * @param initialiseCells  whether to initialise the cell cycle models as each
+     *   cell is created
+     * 
+     * \todo this should be refactored with GenerateForCrypt in AbstractCellsGenerator when Vertex code is released
+     */
+    void GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
+                                  VertexMesh<2,2>& rMesh,
+                                  const std::vector<unsigned> locationIndices,
+                                  bool randomBirthTimes,
+                                  double y0 = 0.3,
+                                  double y1 = 2.0,
+                                  double y2 = 3.0,
+                                  double y3 = 4.0,
+                                  bool initialiseCells = false);
+};
+
+#endif /*SIMPLEWNTCELLCYCLEMODELCELLSGENERATORFORVERTEX_HPP_*/

commit d461e34ab156d4cd90d8501bea508a694840196a

Fixing coverage broken in r8009
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk/notforrelease_cell_based@8047 b5f69653-2bf5-0310-8201-b0dafb8e1405

src/crypt/Cell/SimpleWntCellCycleModelCellsGeneratorForVertex.cpp
@@ -54,7 +54,6 @@ double SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GetTypicalStemCellCy
 template<unsigned DIM>
 void SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
                                  VertexMesh<2,2>& rMesh,
-                                 const std::vector<unsigned> locationIndices,
                                  bool randomBirthTimes,
                                  double y0,
                                  double y1,
@@ -68,7 +67,7 @@ void SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt
 
     RandomNumberGenerator* p_random_num_gen = RandomNumberGenerator::Instance();
 
-    unsigned num_cells = locationIndices.empty() ? rMesh.GetNumElements() : locationIndices.size();
+    unsigned num_cells = rMesh.GetNumElements();
     
 
     AbstractCellCycleModel* p_cell_cycle_model = NULL;
@@ -83,19 +82,8 @@ void SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt
         CellProliferativeType cell_type;
         unsigned generation;
 
-        double y = 0.0;
-        if (!locationIndices.empty())
-        {
-            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
-            {
-                y = rMesh.GetCentroidOfElement(i)[1];
-            }
-        }
-        else
-        {
-            y = rMesh.GetCentroidOfElement(i)[1];
-        }
-
+        double y = rMesh.GetCentroidOfElement(i)[1];
+        
         p_cell_cycle_model = CreateCellCycleModel();
         typical_transit_cycle_time = this->GetTypicalTransitCellCycleTime();
         typical_stem_cycle_time = GetTypicalStemCellCycleTime();
@@ -150,17 +138,7 @@ void SimpleWntCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt
 
         cell.SetBirthTime(birth_time);
 
-        if (!locationIndices.empty())
-        {
-            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
-            {
-                rCells.push_back(cell);
-            }
-        }
-        else
-        {
-            rCells.push_back(cell);
-        }
+        rCells.push_back(cell);
     }
 }
 

src/crypt/Cell/SimpleWntCellCycleModelCellsGeneratorForVertex.hpp
@@ -66,7 +66,6 @@ public:
      *
      * @param rCells  An empty cells vector for this function to fill up
      * @param rMesh  The crypt mesh (A VERTEX MESH)
-     * @param locationIndices the node indices corresponding to real cells
      * @param randomBirthTimes  Whether to assign the cells random birth times
      *    (this can be expensive computationally with ODE models)
      * @param y0  below this line cells are generation 0 (defaults to 0.3)
@@ -80,7 +79,6 @@ public:
      */
     void GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
                                   VertexMesh<2,2>& rMesh,
-                                  const std::vector<unsigned> locationIndices,
                                   bool randomBirthTimes,
                                   double y0 = 0.3,
                                   double y1 = 2.0,

src/tissue/cell/FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex.cpp
@@ -62,7 +62,6 @@ bool FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>::Cel
 template<unsigned DIM>
 void FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
                                  VertexMesh<2,2>& rMesh,
-                                 const std::vector<unsigned> locationIndices,
                                  bool randomBirthTimes,
                                  double y0,
                                  double y1,
@@ -76,7 +75,7 @@ void FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>::Gen
 
     RandomNumberGenerator* p_random_num_gen = RandomNumberGenerator::Instance();
 
-    unsigned num_cells = locationIndices.empty() ? rMesh.GetNumElements() : locationIndices.size();
+    unsigned num_cells = rMesh.GetNumElements();
     
 
     AbstractCellCycleModel* p_cell_cycle_model = NULL;
@@ -91,19 +90,8 @@ void FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>::Gen
         CellProliferativeType cell_type;
         unsigned generation;
 
-        double y = 0.0;
-        if (!locationIndices.empty())
-        {
-            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
-            {
-                y = rMesh.GetCentroidOfElement(i)[1];
-            }
-        }
-        else
-        {
-            y = rMesh.GetCentroidOfElement(i)[1];
-        }
-
+        double y = rMesh.GetCentroidOfElement(i)[1];
+        
         p_cell_cycle_model = CreateCellCycleModel();
         typical_transit_cycle_time = this->GetTypicalTransitCellCycleTime();
         typical_stem_cycle_time = GetTypicalStemCellCycleTime();
@@ -158,17 +146,7 @@ void FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>::Gen
 
         cell.SetBirthTime(birth_time);
 
-        if (!locationIndices.empty())
-        {
-            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
-            {
-                rCells.push_back(cell);
-            }
-        }
-        else
-        {
-            rCells.push_back(cell);
-        }
+        rCells.push_back(cell);
     }
 }
 

src/tissue/cell/FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex.hpp
@@ -71,7 +71,6 @@ public:
      *
      * @param rCells  An empty cells vector for this function to fill up
      * @param rMesh  The crypt mesh (A VERTEX MESH)
-     * @param locationIndices the node indices corresponding to real cells
      * @param randomBirthTimes  Whether to assign the cells random birth times
      *    (this can be expensive computationally with ODE models)
      * @param y0  below this line cells are generation 0 (defaults to 0.8)
@@ -85,7 +84,6 @@ public:
      */
     void GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
                                   VertexMesh<2,2>& rMesh,
-                                  const std::vector<unsigned> locationIndices,
                                   bool randomBirthTimes,
                                   double y0 = 0.8,
                                   double y1 = 2.0,

src/tissue/cell/StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex.cpp
@@ -61,7 +61,6 @@ bool StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>
 template<unsigned DIM>
 void StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>::GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
                                  VertexMesh<2,2>& rMesh,
-                                 const std::vector<unsigned> locationIndices,
                                  bool randomBirthTimes,
                                  double y0,
                                  double y1,
@@ -75,7 +74,7 @@ void StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>
 
     RandomNumberGenerator* p_random_num_gen = RandomNumberGenerator::Instance();
 
-    unsigned num_cells = locationIndices.empty() ? rMesh.GetNumElements() : locationIndices.size();
+    unsigned num_cells = rMesh.GetNumElements();
     
 
     AbstractCellCycleModel* p_cell_cycle_model = NULL;
@@ -90,19 +89,8 @@ void StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>
         CellProliferativeType cell_type;
         unsigned generation;
 
-        double y = 0.0;
-        if (!locationIndices.empty())
-        {
-            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
-            {
-                y = rMesh.GetCentroidOfElement(i)[1];
-            }
-        }
-        else
-        {
-            y = rMesh.GetCentroidOfElement(i)[1];
-        }
-
+        double y = rMesh.GetCentroidOfElement(i)[1];
+        
         p_cell_cycle_model = CreateCellCycleModel();
         typical_transit_cycle_time = this->GetTypicalTransitCellCycleTime();
         typical_stem_cycle_time = GetTypicalStemCellCycleTime();
@@ -157,17 +145,7 @@ void StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<DIM>
 
         cell.SetBirthTime(birth_time);
 
-        if (!locationIndices.empty())
-        {
-            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
-            {
-                rCells.push_back(cell);
-            }
-        }
-        else
-        {
-            rCells.push_back(cell);
-        }
+        rCells.push_back(cell);
     }
 }
 

src/tissue/cell/StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex.hpp
@@ -71,7 +71,6 @@ public :
      *
      * @param rCells  An empty cells vector for this function to fill up
      * @param rMesh  The crypt mesh (A VERTEX MESH)
-     * @param locationIndices the node indices corresponding to real cells
      * @param randomBirthTimes  Whether to assign the cells random birth times
      *    (this can be expensive computationally with ODE models)
      * @param y0  below this line cells are generation 0 (defaults to 0.8)
@@ -85,7 +84,6 @@ public :
      */
     void GenerateForVertexCrypt(std::vector<TissueCell>& rCells,
                                   VertexMesh<2,2>& rMesh,
-                                  const std::vector<unsigned> locationIndices,
                                   bool randomBirthTimes,
                                   double y0 = 0.8,
                                   double y1 = 2.0,

test/crypt/TestCellsGeneratorForVertex.hpp
@@ -63,10 +63,10 @@ public:
 		// Create cells
         std::vector<TissueCell> fixed_cells, stochastic_cells;
         FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> fixed_cells_generator;
-        fixed_cells_generator.GenerateForVertexCrypt(fixed_cells, *p_mesh, std::vector<unsigned>(), true, y0, y1, y2,y3 );
+        fixed_cells_generator.GenerateForVertexCrypt(fixed_cells, *p_mesh, true, y0, y1, y2,y3 );
 
 		StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> stochastic_cells_generator;
-        stochastic_cells_generator.GenerateForVertexCrypt(stochastic_cells, *p_mesh, std::vector<unsigned>(), true, y0, y1, y2,y3 );
+        stochastic_cells_generator.GenerateForVertexCrypt(stochastic_cells, *p_mesh, true, y0, y1, y2,y3 );
 
 
         TS_ASSERT_EQUALS(fixed_cells.size(), p_mesh->GetNumElements());
@@ -119,7 +119,7 @@ public:
         // Create cells
         std::vector<TissueCell> cells;
         SimpleWntCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), false);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, false);
 
         // Test that cells were generated correctly
         TS_ASSERT_EQUALS(cells.size(), p_mesh->GetNumElements());

test/crypt/TestVertexCryptSimulation2d.hpp
@@ -125,7 +125,7 @@ public:
         // Set up cells
         std::vector<TissueCell> cells;
         FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true);
         
         // Create tissue
         VertexBasedTissue<2> tissue(*p_mesh, cells);
@@ -182,7 +182,7 @@ public:
         // Create cells
         std::vector<TissueCell> cells;
         FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true);
         
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);
@@ -232,7 +232,7 @@ public:
         // Create cells, all differentiated
         std::vector<TissueCell> cells;
         FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true, 0.0, 0.0, 0.0, 0.0);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true, 0.0, 0.0, 0.0, 0.0);
 
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);
@@ -268,7 +268,7 @@ public:
         // Create cells, bottom row are STEM rest are DIFFERENTIATED. 
         std::vector<TissueCell> cells;
         FixedDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true, 0.8,0.8,0.8,0.8);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true, 0.8,0.8,0.8,0.8);
         
         // Cell 1 should divide at time t=0.05
         cells[0].SetBirthTime(-23.95);
@@ -315,7 +315,7 @@ public:
         // Create cells
         std::vector<TissueCell> cells;
         StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true);
 
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);
@@ -356,7 +356,7 @@ public:
         // Create cells
         std::vector<TissueCell> cells;
         SimpleWntCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true);
         
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);
@@ -401,7 +401,7 @@ public:
         // Create cells
         std::vector<TissueCell> cells;
         SimpleWntCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true);   
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true);   
         
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);
@@ -448,7 +448,7 @@ public:
         // Create cells, bottom row are STEM rest are DIFFERENTIATED. 
         std::vector<TissueCell> cells;
 		StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true, 0.8,0.8,0.8,0.8);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true, 0.8,0.8,0.8,0.8);
 
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);
@@ -499,7 +499,7 @@ public:
         // Create cells
         std::vector<TissueCell> cells;
         StochasticDurationGenerationBasedCellCycleModelCellsGeneratorForVertex<2> cells_generator;
-        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, std::vector<unsigned>(), true);
+        cells_generator.GenerateForVertexCrypt(cells, *p_mesh, true);
         
         // Create tissue
         VertexBasedTissue<2> crypt(*p_mesh, cells);

test/simulation/TestTissueSimulationWithVertexBasedTissue.hpp
@@ -277,7 +277,7 @@ public:
         // Set up tissue simulation
         TissueSimulation<2> simulator(tissue, force_collection);
         simulator.SetOutputDirectory("TestVertexMonolayerWithCellBirth");
-        simulator.SetSamplingTimestepMultiple(100);
+        simulator.SetSamplingTimestepMultiple(50);
         simulator.SetEndTime(0.1);
 
         // Run simulation
@@ -293,7 +293,7 @@ public:
         TS_ASSERT_EQUALS(new_num_cells, old_num_cells+1);
     }
 
-    void noTestVertexMonolayerLong() throw (Exception)
+    void TestVertexMonolayerLong() throw (Exception)
     {
         // Create a simple 2D VertexMesh
         HoneycombVertexMeshGenerator generator(3, 3);
@@ -303,7 +303,7 @@ public:
         std::vector<TissueCell> cells;
         for (unsigned elem_index=0; elem_index<p_mesh->GetNumElements(); elem_index++)
         {
-            CellProliferativeType cell_type = TRANSIT;
+            CellProliferativeType cell_type = STEM;
 
             double birth_time = - RandomNumberGenerator::Instance()->ranf()*
                                  ( TissueConfig::Instance()->GetTransitCellG1Duration()
@@ -326,11 +326,11 @@ public:
         // Set up tissue simulation
         TissueSimulation<2> simulator(tissue, force_collection);
         simulator.SetOutputDirectory("TestVertexMonolayerLong");
-        simulator.SetSamplingTimestepMultiple(100);
-        simulator.SetEndTime(30.0); // at 35.6 a void forms and need to deal with this!!
+        simulator.SetSamplingTimestepMultiple(50);
+        simulator.SetEndTime(50.0); // at 35.6 a void forms and need to deal with this!!
 
         // Adjust Max Generations so cells keep proliferating
-        TissueConfig::Instance()->SetMaxTransitGenerations(8u);
+        TissueConfig::Instance()->SetMaxTransitGenerations(5u);
 
         // Run simulation
         simulator.Solve();