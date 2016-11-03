5d173e1e1d3475339c210355c0076d0b49b450cc
WeijieT <tanweijie_94@hotmail.com>
2016-11-03 17:55:41 +0000

    for stretching mesh

--- cell_based/test/simulation/TestVertexMesh33UniaxialLoad.hpp
+++ cell_based/test/simulation/TestVertexMesh33UniaxialLoad.hpp
@@ -50,7 +50,6 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "DifferentiatedCellProliferativeType.hpp"
 #include "NoCellCycleModel.hpp"
 #include "SmartPointers.hpp"
-//#include "MisraForce.hpp"
 #include "OffLatticeSimulation.hpp"
 
 #include "FakePetscSetup.hpp"
@@ -184,7 +183,7 @@ public:
 //        const double z_height = 1;
         const double target_area = 1;
         const unsigned num_cells_x = 11;
-        const unsigned num_cells_y = 5;
+        const unsigned num_cells_y = 10;
         // There seems to be a bug somewhere in voronoiprism3dVertexMeshGenerator....
 //        VoronoiPrism3dVertexMeshGenerator generator(num_cells_x, num_cells_y, z_height, 5, target_area);
 //        MutableVertexMesh<3,3>* p_mesh = generator.GetMeshAfterReMesh();
@@ -208,28 +207,21 @@ public:
         OffLatticeSimulation<3> simulator(cell_population);
         simulator.SetOutputDirectory(tmp_name);
         simulator.SetSamplingTimestepMultiple(10);
-        const double end_time = 4;
+        const double end_time = 7;
         simulator.SetEndTime(end_time);
 
-//        MAKE_PTR(MisraForce<3>, p_force);
-//        p_force->SetLateralSurfaceEnergyParameter(0);
-//        p_force->SetApicalLineTensionParameter(1);
-//        p_force->SetBasalSurfaceEnergyParameter(1);
-//        p_force->SetTargetVolume(z_height*target_area*2);
-//        p_force->SetVolumeCompressibilityParameter(30);
-//        simulator.AddForce(p_force);
         MAKE_PTR(GeneralMonolayerVertexMeshForce<3>, p_force3);
-        p_force3->SetApicalParameter(10, 10, 1);
-        p_force3->SetBasalParameter(10, 10, 1);
-        p_force3->SetLateralParameter(4);
-        p_force3->SetVolumeParameter(200, 1);
+        p_force3->SetApicalParameter(20, 20, 0.7);
+        p_force3->SetBasalParameter(20, 20, 0.7);
+        p_force3->SetLateralParameter(8);
+        p_force3->SetVolumeParameter(350, 1);
         simulator.AddForce(p_force3);
-        MAKE_PTR_ARGS(HorizontalStretchForce<3>, p_force2, (1));
+        MAKE_PTR_ARGS(HorizontalStretchForce<3>, p_force2, (1, 0.15));
         simulator.AddForce(p_force2);
 
             simulator.Solve();
 
-        TS_ASSERT_EQUALS(cell_population.GetNumRealCells(), 25u);
+        TS_ASSERT_EQUALS(cell_population.GetNumRealCells(), num_cells_x*num_cells_y);
         TS_ASSERT_DELTA(SimulationTime::Instance()->GetTime(), end_time, 1e-10);
     }
 };
