888604b4e78facd5c93751da3d616f520bc101ea
Alexander Fletcher <a.g.fletcher@sheffield.ac.uk>
2016-11-13 20:36:09 +0000

    Bit of coverage

--- crypt/test/mechanics/TestLinearSpringWithVariableSpringConstantsForce.hpp
+++ crypt/test/mechanics/TestLinearSpringWithVariableSpringConstantsForce.hpp
@@ -185,6 +185,7 @@ public:
         TS_ASSERT_DELTA(norm_2(linear_force.CalculateForceBetweenNodes(20, 21, crypt)), 1.5*8.59312/18.14, 1e-5);
 
         linear_force.SetBetaCatSpringScaler(20/6.0);
+        TS_ASSERT_DELTA(linear_force.GetBetaCatSpringScaler(), 20/6.0, 1e-6);
         TS_ASSERT_DELTA(norm_2(linear_force.CalculateForceBetweenNodes(20, 21, crypt)), 1.5*8.59312/20.0, 1e-5);
 
         // Tidy up
