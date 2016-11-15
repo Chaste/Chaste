45d53c9d3be64b80fb404cef37d41079cb0f61d1
Martin Robinson <martinjrobins@gmail.com>
2016-11-15 13:10:16 +0000

    Revert "#2858: test for buildbot coverage testing"
    This reverts commit 91591fc3ad678bcd05e9ebb939d95dbff52812c1.

--- cmake/Modules/ChasteRunCoverageTests.cmake
+++ cmake/Modules/ChasteRunCoverageTests.cmake
@@ -1,6 +1,5 @@
 execute_process(
-        COMMAND ${CTEST_COMMAND} "-L" "Continuous" "--output-on-failure" 
-        COMMAND ${CTEST_COMMAND} "-L" "Parallel" "--output-on-failure" 
+        COMMAND ${CTEST_COMMAND} "-L" "Continuous|Parallel" "--output-on-failure" 
         WORKING_DIRECTORY ${Chaste_BINARY_DIR}
         ERROR_QUIET
         )
