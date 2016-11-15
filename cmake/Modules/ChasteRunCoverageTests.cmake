91591fc3ad678bcd05e9ebb939d95dbff52812c1
Martin Robinson <martinjrobins@gmail.com>
2016-11-15 13:10:16 +0000

    #2858: test for buildbot coverage testing

--- cmake/Modules/ChasteRunCoverageTests.cmake
+++ cmake/Modules/ChasteRunCoverageTests.cmake
@@ -1,5 +1,6 @@
 execute_process(
-        COMMAND ${CTEST_COMMAND} "-L" "Continuous|Parallel" "--output-on-failure" 
+        COMMAND ${CTEST_COMMAND} "-L" "Continuous" "--output-on-failure" 
+        COMMAND ${CTEST_COMMAND} "-L" "Parallel" "--output-on-failure" 
         WORKING_DIRECTORY ${Chaste_BINARY_DIR}
         ERROR_QUIET
         )
