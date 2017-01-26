Chaste/Chaste

commit a14ea9b4c0cbf9cd54dc569b4d25096df044dbf4

mesh coverage guards

mesh/src/common/TetrahedralMesh.cpp
@@ -419,7 +419,7 @@ unsigned TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetContainingElementIndexWithI
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 unsigned TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetNearestElementIndex(const ChastePoint<SPACE_DIM>& rTestPoint)
 {
-    EXCEPT_IF_NOT(ELEMENT_DIM == SPACE_DIM); // CalculateInterpolationWeights hits an assertion otherwise
+    EXCEPT_IF_NOT(ELEMENT_DIM == SPACE_DIM);  // LCOV_EXCL_LINE // CalculateInterpolationWeights hits an assertion otherwise
     double max_min_weight = -std::numeric_limits<double>::infinity();
     unsigned closest_index = 0;
     for (unsigned i=0; i<this->mElements.size(); i++)
@@ -485,8 +485,8 @@ void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::Clear()
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 double TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetAngleBetweenNodes(unsigned indexA, unsigned indexB)
 {
-    assert(SPACE_DIM == 2);
-    assert(SPACE_DIM == ELEMENT_DIM);
+    assert(SPACE_DIM == 2); 			// LCOV_EXCL_LINE
+    assert(SPACE_DIM == ELEMENT_DIM); 	// LCOV_EXCL_LINE
 
     double x_difference = this->mNodes[indexB]->rGetLocation()[0] - this->mNodes[indexA]->rGetLocation()[0];
     double y_difference = this->mNodes[indexB]->rGetLocation()[1] - this->mNodes[indexA]->rGetLocation()[1];
@@ -733,7 +733,7 @@ void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetJacobianForElement(unsigned ele
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetInverseJacobianForElement(unsigned elementIndex, c_matrix<double, SPACE_DIM, ELEMENT_DIM>& rJacobian, double& rJacobianDeterminant, c_matrix<double, ELEMENT_DIM, SPACE_DIM>& rInverseJacobian) const
 {
-    assert(ELEMENT_DIM <= SPACE_DIM);
+    assert(ELEMENT_DIM <= SPACE_DIM); 	// LCOV_EXCL_LINE
     assert(elementIndex < this->mElementInverseJacobians.size());
     rInverseJacobian = this->mElementInverseJacobians[elementIndex];
     rJacobian = this->mElementJacobians[elementIndex];
@@ -743,7 +743,7 @@ void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetInverseJacobianForElement(unsig
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetWeightedDirectionForElement(unsigned elementIndex, c_vector<double, SPACE_DIM>& rWeightedDirection, double& rJacobianDeterminant) const
 {
-    assert(ELEMENT_DIM < SPACE_DIM);
+    assert(ELEMENT_DIM < SPACE_DIM); 	// LCOV_EXCL_LINE
     assert(elementIndex < this->mElementWeightedDirections.size());
     rWeightedDirection = this->mElementWeightedDirections[elementIndex];
     rJacobianDeterminant = this->mElementJacobianDeterminants[elementIndex];
@@ -945,11 +945,14 @@ void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ImportFromMesher(MESHER_IO& mesher
                 this->mBoundaryElements.push_back(p_b_element);
                 next_boundary_element_index++;
             }
+            // LCOV_EXCL_START
             catch (Exception &)
             {
                 // Tetgen is feeding us lies  //Watch this space for coverage
+            	NEVER_REACHED;
                 assert(SPACE_DIM == 3);
             }
+            // LCOV_EXCL_STOP
         }
     }
 

mesh/src/common/AbstractMesh.cpp
@@ -115,12 +115,13 @@ Node<SPACE_DIM>* AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNodeFromPrePermutation
     }
 }
 
+// LCOV_EXCL_START
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::ReadNodesPerProcessorFile(const std::string& rNodesPerProcessorFile)
 {
     NEVER_REACHED;
 }
-
+// LCOV_EXCL_STOP
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::SetElementOwnerships()
@@ -156,11 +157,13 @@ void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::SetDistributedVectorFactory(Distribut
     mpDistributedVectorFactory = pFactory;
 }
 
+// LCOV_EXCL_START
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::PermuteNodes()
 {
     NEVER_REACHED;
 }
+// LCOV_EXCL_STOP
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::BoundaryNodeIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetBoundaryNodeIteratorBegin() const
@@ -386,7 +389,7 @@ void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::Rotate(c_matrix<double , SPACE_DIM, S
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::Rotate(c_vector<double,3> axis, double angle)
 {
-    assert(SPACE_DIM == 3);
+    assert(SPACE_DIM == 3); // LCOV_EXCL_LINE
     double norm = norm_2(axis);
     c_vector<double,3> unit_axis=axis/norm;
 

mesh/src/common/AbstractTetrahedralElement.cpp
@@ -228,7 +228,7 @@ c_vector<double, SPACE_DIM> AbstractTetrahedralElement<ELEMENT_DIM, SPACE_DIM>::
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractTetrahedralElement<ELEMENT_DIM, SPACE_DIM>::CalculateInverseJacobian(c_matrix<double, SPACE_DIM, ELEMENT_DIM>& rJacobian, double& rJacobianDeterminant, c_matrix<double, ELEMENT_DIM, SPACE_DIM>& rInverseJacobian)
 {
-    assert(ELEMENT_DIM <= SPACE_DIM);
+    assert(ELEMENT_DIM <= SPACE_DIM); 	// LCOV_EXCL_LINE
     CalculateJacobian(rJacobian, rJacobianDeterminant);
 
     // CalculateJacobian should make sure that the determinant is not close to zero (or, in fact, negative)
@@ -289,7 +289,7 @@ AbstractTetrahedralElement<0, SPACE_DIM>::AbstractTetrahedralElement(unsigned in
     // Sanity checking
     //unsigned total_nodes = 1;
     assert(this->mNodes.size() == 1);
-    assert(SPACE_DIM > 0);
+    assert(SPACE_DIM > 0); 	// LCOV_EXCL_LINE
 
     // This is so we know it's the first time of asking
     // Create Jacobian
@@ -314,7 +314,7 @@ void AbstractTetrahedralElement<0, SPACE_DIM>::CalculateWeightedDirection(
         c_vector<double, SPACE_DIM>& rWeightedDirection,
         double& rJacobianDeterminant)
 {
-    assert(SPACE_DIM > 0);
+    assert(SPACE_DIM > 0); 	// LCOV_EXCL_LINE
 
     // End point of a line
     rWeightedDirection = zero_vector<double>(SPACE_DIM);
@@ -326,7 +326,7 @@ void AbstractTetrahedralElement<0, SPACE_DIM>::CalculateWeightedDirection(
 template<unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM> AbstractTetrahedralElement<0, SPACE_DIM>::CalculateNormal()
 {
-    assert(SPACE_DIM > 0);
+    assert(SPACE_DIM > 0); 	// LCOV_EXCL_LINE
 
     // End point of a line
     c_vector<double, SPACE_DIM> normal = zero_vector<double>(SPACE_DIM);

mesh/src/common/AbstractTetrahedralMesh.cpp
@@ -244,7 +244,7 @@ void AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::CheckOutwardNormals()
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructLinearMesh(unsigned width)
 {
-    assert(ELEMENT_DIM == 1);
+    assert(ELEMENT_DIM == 1); 	// LCOV_EXCL_LINE
 
     for (unsigned node_index=0; node_index<=width; node_index++)
     {
@@ -276,8 +276,8 @@ void AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructLinearMesh(unsign
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructRectangularMesh(unsigned width, unsigned height, bool stagger)
 {
-    assert(SPACE_DIM == 2);
-    assert(ELEMENT_DIM == 2);
+    assert(SPACE_DIM == 2); 	// LCOV_EXCL_LINE
+    assert(ELEMENT_DIM == 2); 	// LCOV_EXCL_LINE
 
     //Construct the nodes
     unsigned node_index=0;
@@ -385,8 +385,8 @@ void AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructCuboid(unsigned w
         unsigned height,
         unsigned depth)
 {
-    assert(SPACE_DIM == 3);
-    assert(ELEMENT_DIM == 3);
+    assert(SPACE_DIM == 3); 	// LCOV_EXCL_LINE
+    assert(ELEMENT_DIM == 3); 	// LCOV_EXCL_LINE
     //Construct the nodes
 
     unsigned node_index = 0;
@@ -618,7 +618,7 @@ void AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructRegularSlabMeshWi
         unsigned dimension, double spaceStep,
         double width, double height, double depth)
 {
-    assert(ELEMENT_DIM == SPACE_DIM);
+    assert(ELEMENT_DIM == SPACE_DIM); 	// LCOV_EXCL_LINE
     if (dimension >= SPACE_DIM)
     {
         EXCEPTION("Cannot split on non-existent dimension");
@@ -1040,7 +1040,7 @@ unsigned AbstractTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::GetNearestElementIndex
                                                                                                  std::set<unsigned> testElements)
 {
     assert(testElements.size() > 0);
-    EXCEPT_IF_NOT(ELEMENT_DIM == SPACE_DIM); // CalculateInterpolationWeights hits an assertion otherwise
+    EXCEPT_IF_NOT(ELEMENT_DIM == SPACE_DIM); // LCOV_EXCL_LINE // CalculateInterpolationWeights hits an assertion otherwise
 
     double max_min_weight = -std::numeric_limits<double>::infinity();
     unsigned closest_index = 0;

mesh/src/common/BoundaryElement.cpp
@@ -59,7 +59,7 @@ template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 BoundaryElement<ELEMENT_DIM, SPACE_DIM>::BoundaryElement(unsigned index, Node<SPACE_DIM>* pNode)
     : AbstractTetrahedralElement<ELEMENT_DIM,SPACE_DIM>(index)
 {
-    assert(ELEMENT_DIM == 0);
+    assert(ELEMENT_DIM == 0); 	// LCOV_EXCL_LINE
 
     // Store Node pointer
     this->mNodes.push_back(pNode);

mesh/src/common/DistributedTetrahedralMesh.cpp
@@ -696,7 +696,7 @@ void DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ReorderNodes()
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructLinearMesh(unsigned width)
 {
-    assert(ELEMENT_DIM == 1);
+    assert(ELEMENT_DIM == 1); 	// LCOV_EXCL_LINE
 
      //Check that there are enough nodes to make the parallelisation worthwhile
     if (width==0)
@@ -813,8 +813,8 @@ void DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructLinearMesh(uns
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructRectangularMesh(unsigned width, unsigned height, bool stagger)
 {
-    assert(SPACE_DIM == 2);
-    assert(ELEMENT_DIM == 2);
+    assert(SPACE_DIM == 2); 	// LCOV_EXCL_LINE
+    assert(ELEMENT_DIM == 2); 	// LCOV_EXCL_LINE
     //Check that there are enough nodes to make the parallelisation worthwhile
     if (height==0)
     {
@@ -1016,8 +1016,8 @@ void DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ConstructCuboid(unsigne
         unsigned height,
         unsigned depth)
 {
-    assert(SPACE_DIM == 3);
-    assert(ELEMENT_DIM == 3);
+    assert(SPACE_DIM == 3); 	// LCOV_EXCL_LINE
+    assert(ELEMENT_DIM == 3); 	// LCOV_EXCL_LINE
     //Check that there are enough nodes to make the parallelisation worthwhile
     if (depth==0)
     {
@@ -1307,7 +1307,7 @@ void DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ParMetisLibraryNodeAndE
         std::vector<unsigned>& rProcessorsOffset)
 {
     assert(PetscTools::IsParallel());
-    assert(ELEMENT_DIM==2 || ELEMENT_DIM==3); // Metis works with triangles and tetras
+    assert(ELEMENT_DIM==2 || ELEMENT_DIM==3); // LCOV_EXCL_LINE // Metis works with triangles and tetras
 
     const unsigned num_elements = rMeshReader.GetNumElements();
     const unsigned num_procs = PetscTools::GetNumProcs();
@@ -1616,13 +1616,14 @@ ChasteCuboid<SPACE_DIM> DistributedTetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::Calc
         my_minimum_point=my_box.rGetLowerCorner();
         my_maximum_point=my_box.rGetUpperCorner();
     }
+    // LCOV_EXCL_START
     catch (Exception& e)
     {
-// LCOV_EXCL_START
         PetscTools::ReplicateException(true);
         throw e;
-// LCOV_EXCL_STOP
+
     }
+    // LCOV_EXCL_STOP
 
     PetscTools::ReplicateException(false);
 

mesh/src/common/Element.cpp
@@ -126,7 +126,7 @@ c_vector<double,SPACE_DIM+1> Element<ELEMENT_DIM, SPACE_DIM>::CalculateCircumsph
      *
      */
 
-    assert(ELEMENT_DIM == SPACE_DIM);
+    assert(ELEMENT_DIM == SPACE_DIM); 	// LCOV_EXCL_LINE
     c_vector<double, ELEMENT_DIM> rhs;
 
     for (unsigned j=0; j<ELEMENT_DIM; j++)
@@ -162,7 +162,7 @@ c_vector<double,SPACE_DIM+1> Element<ELEMENT_DIM, SPACE_DIM>::CalculateCircumsph
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 double Element<ELEMENT_DIM, SPACE_DIM>::CalculateQuality()
 {
-    assert(SPACE_DIM == ELEMENT_DIM);
+    assert(SPACE_DIM == ELEMENT_DIM); 	// LCOV_EXCL_LINE
     if (SPACE_DIM == 1)
     {
         return 1.0;
@@ -228,7 +228,7 @@ template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM+1> Element<ELEMENT_DIM, SPACE_DIM>::CalculateInterpolationWeights(const ChastePoint<SPACE_DIM>& rTestPoint)
 {
     // Can only test if it's a tetrahedral mesh in 3d, triangles in 2d...
-    assert(ELEMENT_DIM == SPACE_DIM);
+    assert(ELEMENT_DIM == SPACE_DIM); 	// LCOV_EXCL_LINE
 
     c_vector<double, SPACE_DIM+1> weights;
 
@@ -248,7 +248,7 @@ template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM+1> Element<ELEMENT_DIM, SPACE_DIM>::CalculateInterpolationWeightsWithProjection(const ChastePoint<SPACE_DIM>& rTestPoint)
 {
     //Can only test if it's a tetrahedral mesh in 3d, triangles in 2d...
-    assert(ELEMENT_DIM == SPACE_DIM);
+    assert(ELEMENT_DIM == SPACE_DIM); 	// LCOV_EXCL_LINE
 
     c_vector<double, SPACE_DIM+1> weights = CalculateInterpolationWeights(rTestPoint);
 
@@ -290,7 +290,7 @@ template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM> Element<ELEMENT_DIM, SPACE_DIM>::CalculateXi(const ChastePoint<SPACE_DIM>& rTestPoint)
 {
     //Can only test if it's a tetrahedral mesh in 3d, triangles in 2d...
-    assert(ELEMENT_DIM == SPACE_DIM);
+    assert(ELEMENT_DIM == SPACE_DIM); 	// LCOV_EXCL_LINE
 
     // Find the location with respect to node 0
 ///\todo: #1361 ComputeContainingElements and related methods, and methods called by that down to
@@ -313,7 +313,7 @@ template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 bool Element<ELEMENT_DIM, SPACE_DIM>::IncludesPoint(const ChastePoint<SPACE_DIM>& rTestPoint, bool strict)
 {
     // Can only test if it's a tetrahedral mesh in 3d, triangles in 2d...
-    assert(ELEMENT_DIM == SPACE_DIM);
+    assert(ELEMENT_DIM == SPACE_DIM); 	// LCOV_EXCL_LINE
 
     c_vector<double, SPACE_DIM+1> weights=CalculateInterpolationWeights(rTestPoint);
 

mesh/src/common/MixedDimensionMesh.cpp
@@ -105,10 +105,12 @@ void MixedDimensionMesh<ELEMENT_DIM, SPACE_DIM>::ConstructFromMeshReader(Abstrac
                 {
                     nodes.push_back(this->GetNodeOrHaloNode(element_data.NodeIndices[j]) );
                 }
+                // LCOV_EXCL_START
                 catch (Exception&)
                 {
                     NEVER_REACHED;
                 }
+                // LCOV_EXCL_STOP
             }
 
             Element<1u, SPACE_DIM>* p_element = new Element<1u,SPACE_DIM>(element_index, nodes, false);

commit 222f84feafda1b56466103c32f79ac18a346aeee

#2858 Fix profiling error introduced in changeset:a14ea9b/git_repo

mesh/src/common/TetrahedralMesh.cpp
@@ -945,14 +945,15 @@ void TetrahedralMesh<ELEMENT_DIM, SPACE_DIM>::ImportFromMesher(MESHER_IO& mesher
                 this->mBoundaryElements.push_back(p_b_element);
                 next_boundary_element_index++;
             }
-            // LCOV_EXCL_START
             catch (Exception &)
             {
-                // Tetgen is feeding us lies  //Watch this space for coverage
-            	NEVER_REACHED;
-                assert(SPACE_DIM == 3);
+                // Tetgen is feeding us lies
+                /*
+                 *  Note: this code is covered in profiling (Test3dOffLatticeRepresentativeSimulation).
+                 *  It's hard to replicate Tetgen's behaviour with a unit test.
+                 */
+                assert(SPACE_DIM == 3); // LCOV_EXCL_LINE
             }
-            // LCOV_EXCL_STOP
         }
     }
 