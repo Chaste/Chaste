4e84b1721ac96ca608ca925fb84cc94b9decea7b
bartosz.bartmanski <bartosz.bartmanski@dtc.ox.ac.uk>
2017-02-09 20:23:44 +0000

    Added a mesh generator and a test file for an array of cells that will model AVE migration.

---
+++ cell_based/test/simulation/TestIBMAVEMigrationSimulation.hpp
@@ -0,0 +1,151 @@
+/*
+
+Copyright (c) 2005-2017, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+// Created by bartosz on 07/02/17.
+
+#ifndef CHASTE_TESTIBMAVEMIGRATIONSIMULATION_HPP
+#define CHASTE_TESTIBMAVEMIGRATIONSIMULATION_HPP
+
+/* The following are used in every Chaste test */
+#include <cxxtest/TestSuite.h>
+#include "AbstractCellBasedTestSuite.hpp"
+
+/* Required for the core elements of the Chaste simulation environment */
+#include "CellsGenerator.hpp"
+#include "DifferentiatedCellProliferativeType.hpp"
+#include "OffLatticeSimulation.hpp"
+#include "SmartPointers.hpp"
+#include "UniformCellCycleModel.hpp"
+
+/* Required for the Immersed Boundary functionality */
+#include "ImmersedBoundaryLinearInteractionForce.hpp"
+#include "ImmersedBoundaryCellPopulation.hpp"
+#include "ImmersedBoundaryLinearMembraneForce.hpp"
+#include "ImmersedBoundaryMesh.hpp"
+#include "ImmersedBoundarySimulationModifier.hpp"
+#include "ImmersedBoundaryPalisadeMeshGenerator.hpp"
+
+/* Required for setting up the numerical method */
+#include "ForwardEulerNumericalMethod.hpp"
+#include <boost/make_shared.hpp>
+
+/* To make sure that the test is not run in parallel */
+#include "FakePetscSetup.hpp"
+
+/* Not sure whether these still work */
+#include "ImmersedBoundaryAVEMigrationMeshGenerator.hpp"
+
+class TestIBMAVEMigrationSimulation : public AbstractCellBasedTestSuite
+{
+public:
+    /*
+     * == Mouse Embryo simulation - simulation of the Distal half of the Visceral Endoderm cells ==
+     *
+     * We will model the VE cells of a mouse embryo using the Immersed Boundary method,
+     * which will be placed on a membrane to keep these ordered and having the required shape.
+     * As a first approximation, the VE cells will be on a circular basement membrane, with the
+     * lower half representing the VE cells around the epiblast
+     */
+    void TestIBMMouseEmbryoCrossSectionSimulation() throw (Exception)
+    {
+        /*
+         * First use the mesh generator to set up the immersed boundary elements.
+         *
+         * @param numCellsVertically - number of cells vertically
+         * @param numCellsHorizontally - number of cells horizontally
+         * @param numNodesPerCell - the numebr of nodes in a cell
+         * @param ellipseExponent - how round the cells are
+         * @param spacing - the spacing between the cells in the array of cells
+         * @param shift - a factor by which to shift the cells in every other row
+         */
+        ImmersedBoundaryAVEMigrationMeshGenerator generator(5, 5, 100, 0.01, 0.03, 0.5);
+        ImmersedBoundaryMesh<2,2>* p_mesh = generator.GetMesh();
+
+        /*
+         * In the next three lines, we generate cells that will be used in the simulation
+         * We can choose the the proliferative type and Cell Cycle model.
+         * Apparently {{{UniformlyDistributedCellCycleModel}}} doesn't allow proliferation, which doesn't matter for this simulation, as on the timescales we're looking at,
+         * there is very little proliferation
+         */
+        std::vector<CellPtr> cells;
+        MAKE_PTR(DifferentiatedCellProliferativeType, p_diff_type);
+        CellsGenerator<UniformCellCycleModel, 2> cells_generator;
+        cells_generator.GenerateBasicRandom(cells, p_mesh->GetNumElements(), p_diff_type);
+
+        /* We now create a {{{CellPopulation}}} object (passing in the mesh and cells) to connect the mesh and the cells
+         * together. Here we use an {{{ImmersedBoundaryCellPopulation}}} and the dimension is <2>.*/
+        ImmersedBoundaryCellPopulation<2> cell_population(*p_mesh, cells);
+
+        /* We now create an {{{OffLatticeSimulation}}} object and pass in the {{{CellPopulation}}}. We also set some
+         * options for the simulation like output directory, output multiple (so we don't visualize every timestep),
+         * and end time.
+         * Additionally, we tell the numerical method that we want the cell population to update node locations.*/
+        OffLatticeSimulation<2> simulator(cell_population);
+        simulator.SetNumericalMethod(boost::make_shared<ForwardEulerNumericalMethod<2,2> >());
+        simulator.GetNumericalMethod()->SetUseUpdateNodeLocation(true);
+
+        double dt = 0.01;
+        simulator.SetOutputDirectory("TestIBMAVEMigrationSimulation");
+        simulator.SetDt(dt);
+        simulator.SetSamplingTimestepMultiple(10);
+        simulator.SetEndTime(100.0 * dt);
+
+        /* All of the machinery for the immersed boundary method is handled in the following {{{SimulationModifier}}}.
+         * Here, we create a 'shared pointer' to an {{{ImmersedBoundarySimulationModifier}}} object and pass it to the
+         * {{{OffLatticeSimulation}}}.*/
+        MAKE_PTR(ImmersedBoundarySimulationModifier<2>, p_main_modifier);
+        simulator.AddSimulationModifier(p_main_modifier);
+
+        /* We now associate an {{{ImmersedBoundaryLinearMembraneForce}}} and
+         * {{{ImmersedBoundaryLinearInteractionForce}}} to the {{{SimulationModifier}}} which
+         * handles the membrane elasticity forces.  These are created in a similar manner as above.*/
+        MAKE_PTR(ImmersedBoundaryLinearMembraneForce<2>, p_boundary_force);
+        p_main_modifier->AddImmersedBoundaryForce(p_boundary_force);
+        p_boundary_force->SetElementSpringConst(0.5 * 1e8);
+        p_boundary_force->SetLaminaSpringConst(1.0 * 1e8);
+
+        MAKE_PTR(ImmersedBoundaryLinearInteractionForce<2>, p_cell_cell_force);
+        p_main_modifier->AddImmersedBoundaryForce(p_cell_cell_force);
+        p_cell_cell_force->SetSpringConst(1.0 * 1e6);
+
+        /* Finally we call the {{{Solve}}} method on the simulation to run the simulation.*/
+        simulator.Solve();
+
+
+    }
+
+};
+
+#endif //CHASTE_TESTIBMAVEMIGRATIONSIMULATION_HPP
---
+++ mesh/src/immersed_boundary/ImmersedBoundaryAVEMigrationMeshGenerator.cpp
@@ -0,0 +1,112 @@
+/*
+
+Copyright (c) 2005-2017, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+// Created by bartosz on 07/02/17.
+
+#include "ImmersedBoundaryAVEMigrationMeshGenerator.hpp"
+#include "ImmersedBoundaryEnumerations.hpp"
+
+ImmersedBoundaryAVEMigrationMeshGenerator::ImmersedBoundaryAVEMigrationMeshGenerator(unsigned numCellsHorizontally,
+                                                                                     unsigned numCellsVertically,
+                                                                                     unsigned numNodesPerCell,
+                                                                                     double ellipseExponent,
+                                                                                     double spacing,
+                                                                                     double shift)
+{
+    // Check for sensible input
+    assert(numCellsHorizontally > 0);
+    assert(numCellsVertically > 0);
+    assert(numNodesPerCell > 3);
+    assert(ellipseExponent > 0);
+    assert(spacing > 0);
+
+    // Helper vectors
+    unit_vector<double> x_unit(2, 0);
+    unit_vector<double> y_unit(2, 1);
+
+    // Calculate the size of cells
+    double cell_width =  (1.0 - (numCellsHorizontally) * spacing)/numCellsHorizontally;
+    double cell_height =  (1.0 - (numCellsVertically) * spacing)/numCellsVertically;
+
+    // Set up a reference cell
+    SuperellipseGenerator* p_gen = new SuperellipseGenerator(numNodesPerCell, ellipseExponent, cell_width, cell_height, 0.0, 0.5*spacing);
+    std::vector<c_vector<double, 2> > locations =  p_gen->GetPointsAsVectors();
+
+    // Set up the containers for holding the nodes and ib_elements
+    std::vector<Node<2>*> nodes;
+    std::vector<ImmersedBoundaryElement<2, 2>*> ib_elements;
+    std::vector<ImmersedBoundaryElement<1, 2>*> ib_laminas;
+
+    for (unsigned vert_index = 0; vert_index < numCellsVertically; vert_index++)
+    {
+        for (unsigned horz_index = 0; horz_index < numCellsHorizontally; horz_index++)
+        {
+            std::vector<Node<2>*> nodes_this_elem;
+            // Shift the reference cell to an appropriate place
+            for ( unsigned location_index = 0; location_index < locations.size(); location_index++)
+            {
+//                c_vector<double, 2> position = locations[location_index] + horz_index * (cell_width + spacing) * x_unit + vert_index * (cell_height + spacing) * y_unit;
+
+                c_vector<double, 2> position;
+                if (vert_index % 2 != 0)
+                {
+                    position = locations[location_index] + horz_index * (cell_width + spacing) * x_unit + vert_index * (cell_height + spacing) * y_unit;
+                }
+                else
+                {
+                    position = locations[location_index] + horz_index * (cell_width + spacing) * x_unit + vert_index * (cell_height + spacing) * y_unit;
+                    position[0] = fmod(position[0] + shift * (cell_width + spacing), 1.0);
+                }
+
+                Node<2>* p_node = new Node<2>(nodes.size(), position, true);
+                nodes.push_back(p_node);
+                nodes_this_elem.push_back(nodes.back());
+            }
+            ib_elements.push_back(new ImmersedBoundaryElement<2, 2>(ib_elements.size(), nodes_this_elem));
+        }
+    }
+
+    mpMesh = new ImmersedBoundaryMesh<2,2>(nodes, ib_elements, ib_laminas);
+}
+
+ImmersedBoundaryAVEMigrationMeshGenerator::~ImmersedBoundaryAVEMigrationMeshGenerator()
+{
+    delete mpMesh;
+}
+
+ImmersedBoundaryMesh<2, 2>* ImmersedBoundaryAVEMigrationMeshGenerator::GetMesh()
+{
+    return mpMesh;
+}
