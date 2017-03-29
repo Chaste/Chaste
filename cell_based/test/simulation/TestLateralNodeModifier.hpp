510dd89674faa18697a723fdc8b2c97e8deda698
WeijieT <tanweijie_94@hotmail.com>
2017-03-29 12:06:38 +0100

    minor edits on TestLateralNodeModifier

--- cell_based/test/simulation/TestLateralNodeModifier.hpp
+++ cell_based/test/simulation/TestLateralNodeModifier.hpp
@@ -26,7 +26,6 @@
 #include "VertexMeshWriter.hpp"
 #include "FakePetscSetup.hpp"
 
-#include "Debug.hpp"
 class TestLateralNodeModifier : public AbstractCellBasedTestSuite
 {
 private:
@@ -35,8 +34,6 @@ private:
         VertexMeshWriter<3, 3> mesh_writer("TestAsynchronousT1", "mesh", false);
         mesh_writer.WriteVtkUsingMesh(rMesh, additionalTag);
     }
-    static const double target_area = 1;
-    static const double z_height = 1;
 
 public:
     void TestUpdateCellData()
@@ -49,9 +46,6 @@ public:
         MutableVertexMesh<3, 3>* p_mesh = generator.GetMesh();
         WriteMesh(*p_mesh, "ori0");
 
-        PRINT_CONTAINER(p_mesh->GetNode(4)->rGetModifiableLocation());
-        PRINT_CONTAINER(p_mesh->GetNode(7)->rGetModifiableLocation());
-
         p_mesh->GetNode(3)->rGetModifiableLocation()[1] = 0.95;
         p_mesh->GetNode(6)->rGetModifiableLocation()[1] = 1.05;
         p_mesh->SetCellRearrangementThreshold(0.2);
@@ -59,8 +53,8 @@ public:
         WriteMesh(*p_mesh, "ori1");
 
         p_mesh->ReMesh();
-        TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 33u);
         TS_ASSERT_EQUALS(p_mesh->GetNumFaces(), 28u);
+        TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 33u);
         WriteMesh(*p_mesh, "ori2");
 
         std::vector<CellPtr> cells;
@@ -73,16 +67,16 @@ public:
         node_modifier.UpdateCellData(cell_population);
         WriteMesh(*p_mesh, "ori3");
 
-        p_mesh->GetNode(3 + 32)->rGetModifiableLocation()[1] = 0.9;
-        p_mesh->GetNode(6 + 32)->rGetModifiableLocation()[1] = 1.1;
+        p_mesh->GetNode(3 + 16)->rGetModifiableLocation()[1] = 0.9;
+        p_mesh->GetNode(6 + 16)->rGetModifiableLocation()[1] = 1.1;
         p_mesh->SetCellRearrangementThreshold(0.3);
         WriteMesh(*p_mesh, "ori4");
 
         node_modifier.UpdateCellData(cell_population);
         WriteMesh(*p_mesh, "ori5");
 
-        TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 33u);
-        TS_ASSERT_EQUALS(p_mesh->GetNumFaces(), 28u);
+        TS_ASSERT_EQUALS(p_mesh->GetNumFaces(), 27u);
+        TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 32u);
     }
 
     void TestAsynchronousT1()
@@ -92,15 +86,19 @@ public:
          */
         std::string output_filename = "TestAsynchronousT1/SecondTest";
         const double end_time = 1;
+        const double target_volume = 3 * sqrt(3) / 2;
 
-        HexagonalPrism3dVertexMeshGenerator generator(4, 3, 3 * sqrt(3) / 2, 1);
+        HexagonalPrism3dVertexMeshGenerator generator(5, 3, target_volume, 1);
         MutableVertexMesh<3, 3>* p_mesh = generator.GetMesh();
 
-        p_mesh->GetNode(16)->rGetModifiableLocation()[1] = 1.4;
-        p_mesh->GetNode(21)->rGetModifiableLocation()[1] = 1.7;
-        p_mesh->SetCellRearrangementThreshold(0.40);
+        p_mesh->GetNode(19)->rGetModifiableLocation()[1] = 2.45;
+        p_mesh->GetNode(25)->rGetModifiableLocation()[1] = 2.55;
+        p_mesh->SetCellRearrangementThreshold(0.30);
+        p_mesh->DeleteElementPriorToReMesh(9u);
 
         p_mesh->ReMesh();
+        TS_ASSERT_EQUALS(p_mesh->GetNumFaces(), 86u);
+        TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 89u);
         p_mesh->SetCellRearrangementThreshold(0.01);
         p_mesh->SetCellRearrangementRatio(1.5);
 
@@ -118,22 +116,23 @@ public:
         p_force3->SetApicalParameters(2.5, 2.5, 0.7);
         p_force3->SetBasalParameters(2.5, 2.5, 0.7);
         p_force3->SetLateralParameter(0, 2.5);
-        p_force3->SetVolumeParameters(400, target_area * z_height);
+        p_force3->SetVolumeParameters(400, target_volume);
         simulator.AddForce(p_force3);
 
         MAKE_PTR(LateralNodeModifier, p_node_modifier);
         simulator.AddSimulationModifier(p_node_modifier);
 
-        MAKE_PTR(HorizontalStretchForce<3>, p_force2);
-        p_force2->SetForceMagnitude(1);
-        p_force2->SetRelativeWidth(0.15);
+        MAKE_PTR_ARGS(HorizontalStretchForce<3>, p_force2, (1, 0.25));
+        p_force2->SetUpPinnedElements(cell_population);
         simulator.AddForce(p_force2);
 
         simulator.Solve();
 
         CellBasedSimulationArchiver<3, OffLatticeSimulation<3> >::Save(&simulator);
 
-        TS_ASSERT_EQUALS(cell_population.GetNumRealCells(), 12u);
+        TS_ASSERT_EQUALS(cell_population.GetNumRealCells(), 14u);
+        TS_ASSERT_EQUALS(p_mesh->GetNumFaces(), 85u);
+        TS_ASSERT_EQUALS(p_mesh->GetNumNodes(), 88u);
         TS_ASSERT_DELTA(SimulationTime::Instance()->GetTime(), end_time, 1e-10);
     }
 };
