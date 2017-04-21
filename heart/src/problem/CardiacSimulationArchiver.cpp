78ada81208d94e59ec72431b2bbda43179bbcf78
Gary Mirams <gary.mirams@gmail.com>
2017-04-21 11:29:03 -0400

    Fix memory on Exception throwing

--- heart/src/problem/CardiacSimulationArchiver.cpp
+++ heart/src/problem/CardiacSimulationArchiver.cpp
@@ -175,6 +175,10 @@ PROBLEM_CLASS* CardiacSimulationArchiver<PROBLEM_CLASS>::Migrate(const FileFinde
     catch (Exception &e)
     {
         DistributedVectorFactory::SetCheckNumberOfProcessesOnLoad(true);
+        if (p_unarchived_simulation)
+        {
+            delete p_unarchived_simulation;
+        }
         throw e;
     }
 
