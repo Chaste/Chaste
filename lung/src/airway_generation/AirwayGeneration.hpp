17a77baee19c50a6d4f5d6886420784d00501272
Fergus Cooper <cooper@maths.ox.ac.uk>
2017-04-22 19:09:58 +0100

    #2883 Potential fix for HZ definition problem

--- lung/src/airway_generation/AirwayGeneration.hpp
+++ lung/src/airway_generation/AirwayGeneration.hpp
@@ -46,6 +46,12 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define _BACKWARD_BACKWARD_WARNING_H 1 //Cut out the strstream deprecated warning for now (gcc4.3)
 #include "vtkVersion.h"
 
+// This pragma is here to prevent a define conflict for HZ in vtkPointLocator (#def'd in linux header asm-generic/param.h)
+#pragma push_macro("HZ")
+#undef HZ
+#include "vtkPointLocator.h"
+#pragma pop_macro("HZ")
+
 #if ((VTK_MAJOR_VERSION >= 5 && VTK_MINOR_VERSION >= 6) || VTK_MAJOR_VERSION >= 6)
 
 #include "vtkSmartPointer.h"
