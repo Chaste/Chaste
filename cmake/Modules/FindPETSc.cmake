ed6794fbc4362eaaa3ce3aed73d251a40877feba
Fergus Cooper <cooper@maths.ox.ac.uk>
2017-05-24 15:58:16 +0100

    #2909 Altered config paths for newer petsc
    Also removed VERY old PETSc, to simplify

--- cmake/Modules/FindPETSc.cmake
+++ cmake/Modules/FindPETSc.cmake
@@ -251,15 +251,12 @@ message("${PETSC_ARCH}")
 
 # Determine whether the PETSc layout is old-style (through 2.3.3) or
 # new-style (>= 3.0.0)
-if (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/lib/petsc/conf/petscvariables") # > 3.5
-  set (petsc_conf_rules "${PETSC_DIR}/lib/petsc/conf/rules")
-  set (petsc_conf_variables "${PETSC_DIR}/lib/petsc/conf/variables")
-elseif (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/include/petscconf.h")   # > 2.3.3
+if (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/lib/petsc/conf/petscvariables")             # From 3.5
+  set (petsc_conf_rules "${PETSC_DIR}/${PETSC_ARCH}/lib/petsc/conf/rules")
+  set (petsc_conf_variables "${PETSC_DIR}/${PETSC_ARCH}/lib/petsc/conf/variables")
+elseif (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/include/petscconf.h")                   # Before 3.5
   set (petsc_conf_rules "${PETSC_DIR}/conf/rules")
   set (petsc_conf_variables "${PETSC_DIR}/conf/variables")
-elseif (EXISTS "${PETSC_DIR}/bmake/${PETSC_ARCH}/petscconf.h") # <= 2.3.3
-  set (petsc_conf_rules "${PETSC_DIR}/bmake/common/rules")
-  set (petsc_conf_variables "${PETSC_DIR}/bmake/common/variables")
 elseif (PETSC_DIR)
   message (SEND_ERROR "The pair PETSC_DIR=${PETSC_DIR} PETSC_ARCH=${PETSC_ARCH} do not specify a valid PETSc installation")
 endif ()
