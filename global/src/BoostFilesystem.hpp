b82de736dffbfa53291be9d964557c80253e9f30
Fergus Cooper <cooper@maths.ox.ac.uk>
2017-06-15 15:36:46 +0100

    #2811 Better fix for boost scoped nums

--- global/src/BoostFilesystem.hpp
+++ global/src/BoostFilesystem.hpp
@@ -55,7 +55,20 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define PATH_LEAF_NAME(path) path.leaf().string()
 #endif
 
+/*
+ * There is potential binary incompatibility when linking the filesystem library (built with C++98) and Chaste (built
+ * with C++11).  This is due to the use of scoped enums in the filesystem library, which have to be emulated pre-C++11
+ * and thus generate different decorated function names.  See #2811 and https://svn.boost.org/trac10/ticket/6779
+ * for details.
+ * This was resolved in Boost 1.57, so we special-case the older versions.
+ */
+#if BOOST_VERSION <= 105600
+#define BOOST_NO_CXX11_SCOPED_ENUMS
 #include <boost/filesystem.hpp>
+#undef BOOST_NO_CXX11_SCOPED_ENUMS
+#else
+#include <boost/filesystem.hpp>
+#endif
 #include <boost/filesystem/fstream.hpp>
 
 namespace fs = boost::filesystem;
