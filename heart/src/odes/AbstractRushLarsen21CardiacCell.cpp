03462d1972b3cf0c1b4ca5cfe89aca576281caf5
Wenxian Guo <w.guo@usask.ca>
2017-06-21 10:59:06 -0600

    next step: modify abstractrushlarsen21

---
+++ heart/src/odes/AbstractRushLarsen21CardiacCell.cpp
@@ -0,0 +1,153 @@
+/*
+
+Copyright (c) 2005-2017, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "AbstractRushLarsen21CardiacCell.hpp"
+
+#include <cassert>
+#include <cmath>
+
+#include "Exception.hpp"
+#include "OdeSolution.hpp"
+#include "TimeStepper.hpp"
+
+AbstractRushLarsen21CardiacCell::AbstractRushLarsen21CardiacCell(unsigned numberOfStateVariables,
+                                                             unsigned voltageIndex,
+                                                             boost::shared_ptr<AbstractStimulusFunction> pIntracellularStimulus)
+    : AbstractCardiacCell(boost::shared_ptr<AbstractIvpOdeSolver>(),
+                          numberOfStateVariables,
+                          voltageIndex,
+                          pIntracellularStimulus)
+{}
+
+AbstractRushLarsen21CardiacCell::~AbstractRushLarsen21CardiacCell()
+{}
+
+OdeSolution AbstractRushLarsen21CardiacCell::Compute(double tStart, double tEnd, double tSamp)
+{
+    // In this method, we iterate over timesteps, doing the following for each:
+    //   - update V using a forward Euler step
+    //   - do as in ComputeExceptVoltage(t) to update the remaining state variables
+    //     using Rush Larsen method or forward Euler as appropriate
+
+    // Check length of time interval
+    if (tSamp < mDt)
+    {
+        tSamp = mDt;
+    }
+    const unsigned n_steps = (unsigned) floor((tEnd - tStart)/tSamp + 0.5);
+    assert(fabs(tStart+n_steps*tSamp - tEnd) < 1e-12);
+    const unsigned n_small_steps = (unsigned) floor(tSamp/mDt+0.5);
+    assert(fabs(mDt*n_small_steps - tSamp) < 1e-12);
+
+    // Initialise solution store
+    OdeSolution solutions;
+    solutions.SetNumberOfTimeSteps(n_steps);
+    solutions.rGetSolutions().push_back(rGetStateVariables());
+    solutions.rGetTimes().push_back(tStart);
+    solutions.SetOdeSystemInformation(this->mpSystemInfo);
+
+    std::vector<double> dy(mNumberOfStateVariables, 0);
+    std::vector<double> alpha(mNumberOfStateVariables, 0);
+    std::vector<double> beta(mNumberOfStateVariables, 0);
+
+    // Loop over time
+    for (unsigned i=0; i<n_steps; i++)
+    {
+        double curr_time = tStart;
+        for (unsigned j=0; j<n_small_steps; j++)
+        {
+            curr_time = tStart + i*tSamp + j*mDt;
+            EvaluateEquations(curr_time, dy, alpha, beta);
+            UpdateTransmembranePotential(dy);
+            ComputeOneStepExceptVoltage(dy, alpha, beta);
+            VerifyStateVariables();
+        }
+
+        // Update solutions
+        solutions.rGetSolutions().push_back(rGetStateVariables());
+        solutions.rGetTimes().push_back(curr_time+mDt);
+    }
+
+    return solutions;
+}
+
+void AbstractRushLarsen21CardiacCell::ComputeExceptVoltage(double tStart, double tEnd)
+{
+    mSetVoltageDerivativeToZero = true;
+    TimeStepper stepper(tStart, tEnd, mDt);
+
+    std::vector<double> dy(mNumberOfStateVariables, 0);
+    std::vector<double> alpha(mNumberOfStateVariables, 0);
+    std::vector<double> beta(mNumberOfStateVariables, 0);
+
+    while (!stepper.IsTimeAtEnd())
+    {
+        EvaluateEquations(stepper.GetTime(), dy, alpha, beta);
+        ComputeOneStepExceptVoltage(dy, alpha, beta);
+
+#ifndef NDEBUG
+        // Check gating variables are still in range
+        VerifyStateVariables();
+#endif // NDEBUG
+
+        stepper.AdvanceOneTimeStep();
+    }
+    mSetVoltageDerivativeToZero = false;
+}
+
+void AbstractRushLarsen21CardiacCell::SolveAndUpdateState(double tStart, double tEnd)
+{
+    TimeStepper stepper(tStart, tEnd, mDt);
+
+    std::vector<double> dy(mNumberOfStateVariables, 0);
+    std::vector<double> alpha(mNumberOfStateVariables, 0);
+    std::vector<double> beta(mNumberOfStateVariables, 0);
+
+    while (!stepper.IsTimeAtEnd())
+    {
+        EvaluateEquations(stepper.GetTime(), dy, alpha, beta);
+        UpdateTransmembranePotential(dy);
+        ComputeOneStepExceptVoltage(dy, alpha, beta);
+        VerifyStateVariables();
+
+        stepper.AdvanceOneTimeStep();
+    }
+}
+
+void AbstractRushLarsen21CardiacCell::UpdateTransmembranePotential(const std::vector<double> &rDY)
+{
+    unsigned v_index = GetVoltageIndex();
+    rGetStateVariables()[v_index] += mDt*rDY[v_index];
+}
---
+++ heart/src/odes/AbstractRushLarsen21CardiacCell.hpp
@@ -0,0 +1,188 @@
+/*
+
+Copyright (c) 2005-2017, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef ABSTRACTRUSHLARSEN21CARDIACCELL_HPP_
+#define ABSTRACTRUSHLARSEN21CARDIACCELL_HPP_
+
+#include "ChasteSerialization.hpp"
+#include <boost/serialization/base_object.hpp>
+#include "ClassIsAbstract.hpp"
+
+#include "AbstractCardiacCell.hpp"
+#include "PetscTools.hpp"
+
+/**
+ * This is the base class for cardiac cells solved using the Rush-Larsen method.
+ * It is based on code contributed by Megan Lewis, University of Saskatchewan
+ *
+ * The basic approach to solving such models is:
+ *  \li Compute alpha & beta values for gating variables, and derivatives for
+ *      other state variables.
+ *  \li Update the transmembrane potential, either from solving an external PDE,
+ *      or using a forward Euler step.
+ *  \li Update any eligible gating variables (or similar) with Rush-Larsen scheme.
+ *  \li Update the remaining state variables using a forward Euler step.
+ */
+class AbstractRushLarsen21CardiacCell : public AbstractCardiacCell
+{
+private:
+    /** Needed for serialization. */
+    friend class boost::serialization::access;
+    /**
+     * Archive the member variables.
+     *
+     * @param archive
+     * @param version
+     */
+    template<class Archive>
+    void serialize(Archive & archive, const unsigned int version)
+    {
+        // This calls serialize on the base class.
+        archive & boost::serialization::base_object<AbstractCardiacCell>(*this);
+    }
+
+public:
+    /**
+     * Standard constructor for a cell.
+     *
+     * @param numberOfStateVariables  the size of the ODE system
+     * @param voltageIndex  the index of the variable representing the transmembrane
+     *     potential within the state variable vector
+     * @param pIntracellularStimulus  the intracellular stimulus function
+     *
+     * Some notes for future reference:
+     *  \li It's a pity that inheriting from AbstractCardiacCell forces us to store a
+     *      null pointer (for the unused ODE solver) in every instance.  We may want
+     *      to revisit this design decision at a later date.
+     */
+    AbstractRushLarsen21CardiacCell(
+        unsigned numberOfStateVariables,
+        unsigned voltageIndex,
+        boost::shared_ptr<AbstractStimulusFunction> pIntracellularStimulus);
+
+    /** Virtual destructor */
+    virtual ~AbstractRushLarsen21CardiacCell();
+
+    /**
+     * Simulates this cell's behaviour between the time interval [tStart, tEnd],
+     * with timestep #mDt.  Uses a forward Euler step to update the transmembrane
+     * potential at each timestep.
+     *
+     * The length of the time interval must be a multiple of the timestep.
+     *
+     * @param tStart  beginning of the time interval to simulate
+     * @param tEnd  end of the time interval to simulate
+     * @param tSamp  sampling interval for returned results (defaults to #mDt)
+     * @return  the values of each state variable, at intervals of tSamp.
+     */
+    OdeSolution Compute(double tStart, double tEnd, double tSamp=0.0);
+
+    /**
+     * Simulates this cell's behaviour between the time interval [tStart, tEnd],
+     * with timestep #mDt.  The transmembrane potential is kept fixed throughout,
+     * but the other state variables are updated.
+     *
+     * The length of the time interval must be a multiple of the timestep.
+     *
+     * @param tStart  beginning of the time interval to simulate
+     * @param tEnd  end of the time interval to simulate
+     */
+    void ComputeExceptVoltage(double tStart, double tEnd);
+
+    /**
+     * Simulate this cell's behaviour between the time interval [tStart, tEnd],
+     * with timestemp #mDt, updating the internal state variable values.
+     *
+     * @param tStart  beginning of the time interval to simulate
+     * @param tEnd  end of the time interval to simulate
+     */
+    void SolveAndUpdateState(double tStart, double tEnd);
+
+private:
+// LCOV_EXCL_START
+    /**
+     * This function should never be called - the cell class incorporates its own solver.
+     *
+     * @param time
+     * @param rY
+     * @param rDY
+     */
+    void EvaluateYDerivatives(double time, const std::vector<double> &rY, std::vector<double> &rDY)
+    {
+        NEVER_REACHED;
+    }
+// LCOV_EXCL_STOP
+
+protected:
+    /**
+     * Update the values of elligible gating variables using the Rush-Larsen method,
+     * and of other non-V variables using forward Euler, for a single timestep.
+     *
+     * \note This method must be provided by subclasses.
+     *
+     * @param rDY  vector containing dy/dt values
+     * @param rAlphaOrTau  vector containing alpha or tau values, depending on the formulation
+     * @param rBetaOrInf  vector containing beta or inf values, depending on the formulation
+     */
+    virtual void ComputeOneStepExceptVoltage(const std::vector<double> &rDY,
+                                             const std::vector<double> &rAlphaOrTau,
+                                             const std::vector<double> &rBetaOrInf)=0;
+
+    /**
+     * Perform a forward Euler step to update the transmembrane potential.
+     *
+     * @param rDY  vector containing dy/dt values
+     */
+    void UpdateTransmembranePotential(const std::vector<double> &rDY);
+
+     /**
+     * Compute dy/dt and alpha and beta values.
+     *
+     * \note This method must be provided by subclasses.
+     *
+     * @param time  start of this timestep
+     * @param rDY  vector to fill in with dy/dt values
+     * @param rAlphaOrTau  vector to fill in with alpha or tau values, depending on the formulation
+     * @param rBetaOrInf  vector to fill in with beta or inf values, depending on the formulation
+     */
+    virtual void EvaluateEquations(double time,
+                                   std::vector<double> &rDY,
+                                   std::vector<double> &rAlphaOrTau,
+                                   std::vector<double> &rBetaOrInf)=0;
+};
+
+CLASS_IS_ABSTRACT(AbstractRushLarsen21CardiacCell)
+
+#endif // ABSTRACTRUSHLARSEN21CARDIACCELL_HPP_
