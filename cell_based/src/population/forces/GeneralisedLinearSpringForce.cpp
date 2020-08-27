3b4468d2237802b8173067743fdc6d894b9ab325
James Osborne <james.m.osborne@gmail.com>
2020-08-27 17:15:20 +1000

    Starting the ticket branch

--- cell_based/src/population/forces/GeneralisedLinearSpringForce.cpp
+++ cell_based/src/population/forces/GeneralisedLinearSpringForce.cpp
@@ -33,6 +33,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
+
 #include "GeneralisedLinearSpringForce.hpp"
 
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
