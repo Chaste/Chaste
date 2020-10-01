d54bd0de83707916de7b2ed7ca78a75002fe6f35
MauriceHendrix <maurice.hendrix@nottingham.ac.uk>
2020-10-01 15:03:07 +0100

    getting hostconfig to work with appredict

--- python/hostconfig/default.py
+++ python/hostconfig/default.py
@@ -53,7 +53,8 @@ chaste_libs_path =  '/Users/administrator1/Documents/chaste-libs'
 #EDIT HERE
 
 if not os.path.exists(chaste_libs_path) or not os.path.isdir(chaste_libs_path):
-    print("Chaste dependencies folder %s not found; please edit python/hostconfig/default.py" % chaste_libs_path, file=sys.stderr)
+    print >>sys.stderr, "Chaste dependencies folder", chaste_libs_path, \
+        "not found; please edit python/hostconfig/default.py"
     sys.exit(1)
 
 petsc_3_0_path = chaste_libs_path+'petsc-3.0.0-p8/'
