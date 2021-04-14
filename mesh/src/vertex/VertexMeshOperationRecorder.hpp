d2fcb66ca507a8aecd8b22512dc4ef02d663c991
Aydar <uatay.sheffield@gmail.com>
2021-04-14 13:34:13 +0100

    copyrights edited

--- mesh/src/vertex/VertexMeshOperationRecorder.hpp
+++ mesh/src/vertex/VertexMeshOperationRecorder.hpp
@@ -1,9 +1,37 @@
 /*
- * VertexMeshOperationRecorder.hpp
- *
- *  Created on: 10 Dec 2019
- *      Author: aydar
- */
+
+Copyright (c) 2005-2021, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 
 #ifndef VERTEXMESHOPERATIONRECORDER_HPP_
 #define VERTEXMESHOPERATIONRECORDER_HPP_
@@ -164,6 +192,12 @@ struct CellDivisionInfo
     c_vector<double, SPACE_DIM> mDivisionAxis;
 };
 
+/**
+ * This class records operations performed on the mesh.
+ * In particular, this class records operations on edges and nodes during e.g. T1 transition or cell division
+ * The sequence of operations as well as their nature are needed for remapping of old (prior to an operation) edge based quantities
+ * into new state. For example, when an edge is split into two or shrinked, the edge quantities may change according to the kind of operation
+ */
 template<unsigned int ELEMENT_DIM, unsigned int SPACE_DIM>
 class VertexMeshOperationRecorder
 {
