d9fe32c6b2b5ad0e59c6797366d3afb39857ded6
James Osborne <james.m.osborne@gmail.com>
2021-07-08 12:36:02 +1000

    Ticket #3069 fixing coverage

--- cell_based/test/population/TestT2SwapCellKiller.hpp
+++ cell_based/test/population/TestT2SwapCellKiller.hpp
@@ -42,6 +42,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "CellsGenerator.hpp"
 #include "FixedG1GenerationalCellCycleModel.hpp"
 #include "VertexMeshWriter.hpp"
+#include "CellRemovalLocationsWriter.hpp"
 #include "VertexBasedCellPopulation.hpp"
 #include "T2SwapCellKiller.hpp"
 #include "OffLatticeSimulation.hpp"
@@ -201,7 +202,7 @@ public:
     {
         /**
          * This is performs a single T2 swap in a simulation and tests that the cells and vertex elements are
-         * deleted correctly.
+         * deleted correctly. Also check the cell removal is output correctly.
          */
         // Make 6 nodes to assign to four elements
         std::vector<Node<2>*> nodes;
@@ -251,6 +252,9 @@ public:
         std::vector<CellPtr> cells;
         cells_generator.GenerateBasic(cells, vertex_mesh.GetNumElements(), std::vector<unsigned>());
         VertexBasedCellPopulation<2> cell_population(vertex_mesh, cells);
+
+        // Add a writer to store when cells are removed from simulation
+        cell_population.AddPopulationWriter<CellRemovalLocationsWriter>();
 
         // make a simulator
         OffLatticeSimulation<2> simulator(cell_population);
@@ -315,6 +319,12 @@ public:
 
         // We also do not have any undeleted cells
         TS_ASSERT_EQUALS(cell_population.rGetCells().size(),3u);
+
+        //Check the cell removal is recorded correctly.
+        FileFinder generated_file("TestT2SwapCellKillerInSimulation/results_from_time_0.003/removals.dat", RelativeTo::ChasteTestOutput);
+        FileFinder reference_file("cell_based/test/data/TestT2SwapCellKillerInSimulation/removals.dat",RelativeTo::ChasteSourceRoot);
+        FileComparison files(generated_file,reference_file);
+        TS_ASSERT(files.CompareFiles());
     }
 
     void TestKillerForMultipleT2Swaps()
