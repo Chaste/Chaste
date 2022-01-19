afefd1be47bfeca07483898c7c7cfc06c50f6239
Gary Mirams <gary.mirams@gmail.com>
2022-01-19 17:29:39 +0000

    Copyright Mayhem!

--- mesh/test/data/inputs/gen1Deles.py
+++ mesh/test/data/inputs/gen1Deles.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Copyright (c) 2005-2021, University of Oxford.
+"""Copyright (c) 2005-2022, University of Oxford.
 All rights reserved.
 
 University of Oxford means the Chancellor, Masters and Scholars of the
