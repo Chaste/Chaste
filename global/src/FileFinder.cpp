1c08ad4f59244b35b2569b48058f06f764302ff8
Kwabena N Amponsah <kwabenantim@yahoo.com>
2022-11-07 13:18:05 +0000

    #3094: Adding missing include in FileFinder

--- global/src/FileFinder.cpp
+++ global/src/FileFinder.cpp
@@ -35,6 +35,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "FileFinder.hpp"
 
+#include <algorithm>
 #include <cassert>
 
 #include "BoostFilesystem.hpp"
