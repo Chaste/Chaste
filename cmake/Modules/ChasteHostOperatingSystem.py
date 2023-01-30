9ccbcb0f39de3042bf25a071865041e87fcabf83
Ben Evans <ben.d.evans@gmail.com>
2023-01-30 15:00:43 +0000

    Remove Python 2 boilerplate

--- cmake/Modules/ChasteHostOperatingSystem.py
+++ cmake/Modules/ChasteHostOperatingSystem.py
@@ -30,8 +30,6 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from __future__ import print_function  # print without newline character
-
 import platform
 import distro
 
