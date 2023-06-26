56ae9b422ab3a33e5746c2f9755c502620fde529
Fergus Cooper <fcooper8472@gmail.com>
2023-06-26 11:44:49 +0000

    Tidy includes: reorder and remove unused

--- global/src/Exception.cpp
+++ global/src/Exception.cpp
@@ -33,17 +33,16 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#include <cassert>
-#include <iostream>
-#include <petsc.h>
-#include <string>     // std::char_traits
-
 #include "Exception.hpp"
+
+#include <iostream>
+#include <string>
+
+#include <petsc.h>
 #include "BoostFilesystem.hpp"
-#include "FileFinder.hpp"
-#include "PosixPathFixer.hpp"
-#include "GetCurrentWorkingDirectory.hpp"
+
 #include "ChasteBuildRoot.hpp"
+#include "PosixPathFixer.hpp"
 
 #if (PETSC_VERSION_MAJOR == 3 && PETSC_VERSION_MINOR < 2 || PETSC_VERSION_MAJOR<3 ) // Before PETSc 3.2
 typedef PetscTruth PetscBool;
