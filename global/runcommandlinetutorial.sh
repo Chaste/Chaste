6805343d877c707bff98fdfe49261df7ae75da52
Jack <44051158+BJackal@users.noreply.github.com>
2024-05-28 11:08:52 +0100

    Update runcommandlinetutorial.sh
    Improving commenting.

--- global/runcommandlinetutorial.sh
+++ global/runcommandlinetutorial.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# This bash script accompanies that TestCommandLineArgumentsTutorial
+# This bash script accompanies that TestCommandLineArgumentsTutorial.
+# First, you should build Chaste outside of the main chaste folder in a directory named "build".
+# If youre build folder has a different name such as "chaste_build" e.t.c, then replace the "build"
+# on line 19 with your folders name. Second, you should also investigate the accompanying 
+# TestCommandLineArgumentsTutorial.hpp to see how the simulations are executed
+
+# This bash script will be utilised to executed the TestCommandLineArgumentsTutorial multiple times.
+# Outputting the sum of the command lines arguments at each iteration to the terminal.
+
+# To execute this bash script, after first building Chaste, open a new terminal and navigate to Chaste/global/ .
+# Now enter into the terminal "bash runcommandlinetutorial.sh" and press enter. You should see multiple tests being executed
+# in succession.
+
 # Here we will declare some values we wish to later pass to a for loop.  
 N=2
 L=3
