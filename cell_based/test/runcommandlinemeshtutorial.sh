88cf294133dc7c26f8f03d2f757faba42cd4c576
Jack <44051158+BJackal@users.noreply.github.com>
2024-05-28 10:34:58 +0100

    Update runcommandlinemeshtutorial.sh
    Adding some extra comments.

--- cell_based/test/runcommandlinemeshtutorial.sh
+++ cell_based/test/runcommandlinemeshtutorial.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # This bash script is utilised alongside the TestCommandLineVertexMeshTutorial.hpp file.
 # First, you should build Chaste outside of the main chaste folder in a directory named "build".
-# If youre build folder has a different name such as "chaste_build" e.t.c, then replace the "build"
-# on line XXX with your folders name. Second, you should also investigate the accompanying 
+# If your build folder has a different name such as "chaste_build" e.t.c, then replace the "build"
+# on line 21 with your folders name. Second, you should also investigate the accompanying 
 # TestCommandLineVertexMeshTutorial.hpp to see how the simulations are executed
 
 # This bash script will be utilised to executed the TestCommandLineVertexMeshTutorial multiple times.
 # Creating 9 total simulations with a tissue height and width from 2->10.
+
+# To execute this bash script, after first building Chaste, open a new terminal and navigate to Chaste/cell_based/test/ .
+# Now enter into the terminal "bash runcommandlinemeshtutorial.sh" and press enter. You should see multiple tests being executed
+# in succession.
 
 # N is the max number of cells we wish to have output. 
 N=10
