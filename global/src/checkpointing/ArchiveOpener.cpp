bf1e8de9a5a07a370b787e176c7228a4f807d1b3
Kevin Hoellring <kevin.hoellring@fau.de>
2025-06-19 16:07:36 +0200

    Try inheriting the general class template

--- global/src/checkpointing/ArchiveOpener.cpp
+++ global/src/checkpointing/ArchiveOpener.cpp
@@ -54,7 +54,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * @tparam InputArchive Type of the input archive type, which can vary between text and binary input archives from boost::archive
  */
 template <class InputArchive>
-ArchiveOpener<InputArchive, std::ifstream>
+ArchiveOpener: public ArchiveOpener<InputArchive, std::ifstream>
 {
     /**
      * Specialization for input archives.
@@ -133,7 +133,7 @@ ArchiveOpener<InputArchive, std::ifstream>
  * @tparam OutputArchive Type of the output archive type, which can vary between text and binary output archives from boost::archive
  */
 template <class OutputArchive>
-ArchiveOpener<OutputArchive, std::ofstream>
+ArchiveOpener: public ArchiveOpener<OutputArchive, std::ofstream>
 {
     /**
      * Specialization for output archives.
