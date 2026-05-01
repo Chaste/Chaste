ec59773ec7a2a905644a82f7e6d4904177a5dd5f
Matthew Leach <matt-leach@hotmail.co.uk>
2026-05-01 16:57:20 +0100

    add openmp acceleration to more forces

--- cell_based/src/population/forces/NagaiHondaForce.cpp
+++ cell_based/src/population/forces/NagaiHondaForce.cpp
@@ -98,6 +98,7 @@ void NagaiHondaForce<DIM>::AddForceContribution(AbstractCellPopulation<DIM>& rCe
     }
 
     // Iterate over vertices in the cell population
+    #pragma omp parallel for
     for (unsigned node_index=0; node_index<num_nodes; node_index++)
     {
         Node<DIM>* p_this_node = p_cell_population->GetNode(node_index);
