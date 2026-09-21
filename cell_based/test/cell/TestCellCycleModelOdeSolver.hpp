3de997563a7972a3b0082fa0268176bd0096ea3c
Fergus Cooper <fcooper8472@gmail.com>
2026-09-21 17:14:15 +0000

    Remove CVODE ifdef guards from cell_based test files

--- cell_based/test/cell/TestCellCycleModelOdeSolver.hpp
+++ cell_based/test/cell/TestCellCycleModelOdeSolver.hpp
@@ -218,7 +218,6 @@ public:
 
     void TestWithCvodeAdaptor()
     {
-#ifdef CHASTE_CVODE
         typedef CellCycleModelOdeSolver<TysonNovakCellCycleModel, CvodeAdaptor> CvodeSolver;
 
         // Check we can create an instance
@@ -234,10 +233,6 @@ public:
         TS_ASSERT_THROWS_NOTHING(p_solver->CheckForStoppingEvents());
         TS_ASSERT_THROWS_NOTHING(p_solver->SetMaxSteps(1000));
         TS_ASSERT_THROWS_NOTHING(p_solver->SetTolerances(1e-5, 1e-5));
-#else
-        std::cout << "CVODE is not enabled. " << std::endl;
-        std::cout << "If required please install and alter your hostconfig settings to switch on chaste support." << std::endl;
-#endif //CHASTE_CVODE
     }
 
     void TestArchiving()
